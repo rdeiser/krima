1871d2ffcb6123466224aa89dd32538c32443a44
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2019-12-30 09:34:26 -0600

    Display Item

---
+++ grimas/DisplayItem/splats/rightBody.php
@@ -0,0 +1,11 @@
+              <table class="table">
+                <tr><th>Title:</th><td><?=$e($item['title'])?></td></tr>
+				<tr><th>Call Number:</th><td><?=$e($item['call_number'])?></td></tr>
+				<tr><th>Description:</th><td><?=$e($item['description'])?></td></tr>
+				<tr><th>Barcode:</th><td><?=$e($item['barcode'])?></td></tr>
+				<tr><th>Location:</th><td><?=$e($item['location'])?></td></tr>
+				<tr><th>Process Type:</th><td><?=$e($item['process_type'])?></td></tr>
+				<tr><th>Fulfillment Note</th><td><?=$e($item['fulfillment_note'])?></td></tr>
+				<tr><th>Internal Note 3</th><td><?=$e($item['internal_note_3'])?></td></tr>
+				<tr><th>Statistics Note 3</th><td><?=$e($item['statistics_note_3'])?></td></tr>
+              </table>
