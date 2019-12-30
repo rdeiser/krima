b0361bb48b83b166bda35b0be855d8cc92872211
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2019-12-30 09:53:47 -0600

    update splats

--- grimas/DisplayItem/splats/success.php
+++ grimas/DisplayItem/splats/success.php
@@ -1,9 +1,9 @@
 <?= $t('side-by-side', 
 		array(
 			'item' => $item,
-			'title' => 'Add items in a series',
-			'leftTitle' => 'New record',
-			'rightTitle' => 'Item based on:',
+			'title' => 'Item Display',
+			'leftTitle' => 'DELETE',
+			'rightTitle' => 'Item Information:',
 		 )
 	)
 ?>
