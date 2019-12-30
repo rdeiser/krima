a458e26137f3e5050e021e617fa3ce0ccf00604c
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2019-12-30 09:47:36 -0600

    modify

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -7,28 +7,7 @@ class DisplayItem extends GrimaTask {
 	function do_task() {
 
 		$this->item = new Item();
-		$this->item->loadFromAlmaBarcode($this['barcode_model']);
-
-		if (isset($this['adding']) and ($this['adding'] == "true")) {
-			$newItem = clone $this->item;
-			unset($newItem['item_pid']);
-			$itemdata_elements = array(
-				'barcode',
-				'enumeration_a',
-				'enumeration_b',
-				'chronology_i',
-				'chronology_j',
-				'description'
-			);
-			foreach ($itemdata_elements as $element) {
-				if (isset($this[$element])) {
-					$newItem[$element] = $this[$element];
-				}
-			}
-			$ret = $newItem->addToAlmaHolding($this->item['mms_id'],$this->item['holding_id']);
-			$this->item = new Item();
-			$this->item->xml = $ret;
-		}
+		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		$this->splatVars['item'] = $this->item;
 	}
 
