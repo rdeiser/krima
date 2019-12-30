9d03f05b1f355c5f245d284b78f6ccb4e01738e8
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2019-12-30 10:16:52 -0600

    update

--- grimas/UnboxingWorkflow/UnboxingWorkflow.php
+++ grimas/UnboxingWorkflow/UnboxingWorkflow.php
@@ -6,12 +6,12 @@ class UnboxingWorkflow extends GrimaTask {
 
 	function do_task() {
 		$item = new Item();
-		$item->loadFromAlmaBarcode($this['barcode']);
+		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		$item['internal_note_3'] = date("m/d/Y");
 		$item->updateAlma();
 }
 	function print_success(){
-			GrimaTask::call('DisplayItem', array('barcode' => $this['barcode']));
+			GrimaTask::call('DisplayItem', array('unboxed_barcode' => $this['unboxed_barcode']));
 	}
 }
 
