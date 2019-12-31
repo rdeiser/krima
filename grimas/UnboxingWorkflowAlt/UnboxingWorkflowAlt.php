6a3214dc3939a1cbb166c9cb6eed1990173d296b
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2019-12-31 11:43:17 -0600

    test

--- grimas/UnboxingWorkflowAlt/UnboxingWorkflowAlt.php
+++ grimas/UnboxingWorkflowAlt/UnboxingWorkflowAlt.php
@@ -6,13 +6,13 @@ class UnboxingWorkflowAlt extends GrimaTask {
 
 	function do_task() {
 		$item = new Item();
-		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$item->loadFromAlmaBarcode($this['barcode']);
 		/*$item['inventory_date'] = date("Y-m-d H:00:00");*/
 		/*$item['internal_note_3'] = date("m/d/Y");*/
 		$item->updateAlma();
 }
 	function print_success(){
-			GrimaTask::call('DisplayItem', array('unboxed_barcode' => $this['unboxed_barcode']));
+			GrimaTask::call('DisplayItem', array('barcode' => $this['barcode']));
 	}
 }
 
