07d1e50d398b45cf32eaad91a0ae7d3d8630fa4e
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 15:10:59 -0600

    test

--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -1,8 +1,8 @@
-              <form method="post" action="DisplayItem.php">
-			  <input required="yes" type="hidden" name="next_barcode" value="<?=$e($item['barcode'])?>">
+              <form method="post" action="NextUnboxingWorkflow.php">
+			  <input required="yes" type="hidden" name="unboxed_barcode" value="<?=$e($item['barcode'])?>">
 			  <div class="form-row col-12 pb-4">
 					<label class="col-3 form-check-label" autofocus="autofocus" for="barcode">Barcode:</label>
-					<input class="col-9 form-control znew" type="text" name="next_barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
+					<input class="col-9 form-control znew" type="text" name="unboxed_barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
 				  </div>
 				</div>
                 <input class="btn btn-primary btn-lg active" type="submit" value="Submit">
