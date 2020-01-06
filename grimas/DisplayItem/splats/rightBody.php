13e5e10e876756800b0f20606818daa4b6fd0cb6
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 15:02:11 -0600

    Update rightBody.php

--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -5,5 +5,5 @@
 					<input class="col-9 form-control znew" type="text" name="barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
 				  </div>
 				</div>
-                <input class="btn btn-primary btn-lg active" type="submit" autofocus="autofocus" value="Submit">
+                <input class="btn btn-primary btn-lg active" type="submit" value="Submit">
               </form>
