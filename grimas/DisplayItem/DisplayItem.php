981ecd8278e6e848182bd323b3a268e7a687dce6
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 12:06:09 -0600

    Update DisplayItem.php

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -5,10 +5,12 @@ require_once("../grima-lib.php");
 class DisplayItem extends GrimaTask {
 
 	function do_task() {
-		$item = new Item();
-		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
+
+		$this->item = new Item();
+		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		$this->splatVars['item'] = $this->item;
 	}
+
 }
 
 DisplayItem::RunIt();
