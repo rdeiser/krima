7627b9f0cb1e4910035c23847f1cd101febbf329
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 15:18:37 -0600

    All In ONe

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -5,12 +5,19 @@ require_once("../grima-lib.php");
 class DisplayItem extends GrimaTask {
 
 	function do_task() {
-
-		$this->item = new Item();
-		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$item = new Item();
+		$item->loadFromAlmaBarcode($this['next_barcode']);
+		$item['internal_note_3'] = ('Inventory Date:').date("m/d/Y");
+		$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
+		$item['inventory_date'] = date("Y-m-d");
+		$item->updateAlma();
+		
 		$this->splatVars['item'] = $this->item;
+}
+	function print_success(){
+			GrimaTask::call('DisplayItem', array('unboxed_barcode' => $this['unboxed_barcode']));
 	}
-
 }
 
 DisplayItem::RunIt();
+
--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -1,8 +1,8 @@
-              <form method="post" action="NextUnboxingWorkflow.php">
-			  <input required="yes" type="hidden" name="unboxed_barcode" value="<?=$e($item['barcode'])?>">
+              <form method="post" action="DisplayItem.php">
+			  <input type="hidden" required="yes" autofocus="autofocus" name="next_barcode" value="<?=$e(item['next_barcode'])?>">
 			  <div class="form-row col-12 pb-4">
-					<label class="col-3 form-check-label" autofocus="autofocus" for="barcode">Barcode:</label>
-					<input class="col-9 form-control znew" type="text" name="unboxed_barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
+					<label class="col-3 form-check-label" for="barcode">Barcode:</label>
+					<input class="col-9 form-control znew" type="text" name="barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
 				  </div>
 				</div>
                 <input class="btn btn-primary btn-lg active" type="submit" value="Submit">
