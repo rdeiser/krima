7593d3cbedcd8fe55448f9a2d090cefd1ffecd9e
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 15:02:47 -0600

    Update rightBody.php

--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -2,7 +2,7 @@
 			  <input required="no" type="hidden" name="next_barcode" value="<?=$e($item['barcode'])?>">
 			  <div class="form-row col-12 pb-4">
 					<label class="col-3 form-check-label" autofocus="autofocus" for="barcode">Barcode:</label>
-					<input class="col-9 form-control znew" type="text" name="barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
+					<input class="col-9 form-control znew" type="text" name="next_barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
 				  </div>
 				</div>
                 <input class="btn btn-primary btn-lg active" type="submit" value="Submit">
