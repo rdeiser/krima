f6a12f665a9de427f75b974c5e22b84044b87039
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 09:32:45 -0600

    Update UnboxingWorkflow.php

--- grimas/UnboxingWorkflow/UnboxingWorkflow.php
+++ grimas/UnboxingWorkflow/UnboxingWorkflow.php
@@ -7,7 +7,7 @@ class UnboxingWorkflow extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
-		$item['internal_note_3'] = "Inventory Date:"date("m/d/Y");
+		$item['internal_note_3'] = date("m/d/Y");
 		$item['inventory_number'] = date("\I\n\v\e\n\t\o\r\y \D\a\t\e\: m/d/Y");
 		/*$item['inventory_date'] = new date("Y-m-d");*/
 		$item->updateAlma();
