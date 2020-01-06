dfc90aaf62e8fcb7be0c7ef58dcec16c3131a577
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 15:37:28 -0600

    test

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -7,15 +7,13 @@ class DisplayItem extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
-		$item['internal_note_3'] = ('Inventory Date:').date("m/d/Y");
-		$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
-		$item['inventory_date'] = date("Y-m-d");
-		$item->updateAlma();
-		
+		if (isset($this['adding']) and $this['adding'] == "true")) {
+			$item['internal_note_3'] = ('Inventory Date:').date("m/d/Y");
+			$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
+			$item['inventory_date'] = date("Y-m-d");
+			$item->updateAlma();
+		}
 		$this->splatVars['item'] = $this->item;
-}
-	function print_success(){
-			GrimaTask::call('DisplayItem', array('unboxed_barcode' => $this['unboxed_barcode']));
 	}
 }
 
