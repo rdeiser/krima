94f1d1bd514d97a0bf533fd6b33fcd17860286ce
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 12:17:39 -0600

    Update leftBody.php

--- grimas/DisplayItem/splats/leftBody.php
+++ grimas/DisplayItem/splats/leftBody.php
@@ -1,4 +1,4 @@
-              <form method="post" action="..UnboxingWorkflow/UnboxingWorkflow.php">
+              <form method="post" action="redirect('../UnboxingWorkflow/UnboxingWorkflow.php')">
 			  <div class="form-row col-12 pb-4">
 					<label class="col-3 form-check-label" for="barcode">Barcode:</label>
 					<input class="col-9 form-control znew" type="text" name="barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
