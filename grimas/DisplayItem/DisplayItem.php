46c17393a0a6672b019ce7bf9e937a7c0bf8eebf
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 11:55:05 -0600

    sidebyside

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -7,12 +7,12 @@ class DisplayItem extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		foreach ($item as $item){
 		$item['internal_note_3'] = ('Inventory Date:').date("m/d/Y");
 		$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
 		$item['inventory_date'] = date("Y-m-d");
 		$item->updateAlma();
-	}
-	function do_task() {
+		}
 		$this->splatVars['item'] = $this->item;
 	}
 }
