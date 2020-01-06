4f0dc64351f2abc8b35c5217b34db3c184a60951
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-06 12:23:20 -0600

    reverse

--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -1,13 +1,8 @@
-              <table class="table">
-                <tr><th>Title:</th><td><?=$e($item['title'])?></td></tr>
-				<tr><th>Call Number:</th><td><?=$e($item['call_number'])?></td></tr>
-				<tr><th>Description:</th><td><?=$e($item['description'])?></td></tr>
-				<tr><th>Barcode:</th><td><?=$e($item['barcode'])?></td></tr>
-				<tr><th>Location:</th><td><?=$e($item['location'])?></td></tr>
-				<tr><th>Process Type:</th><td><?=$e($item['process_type'])?></td></tr>
-				<tr><th>Fulfillment Note</th><td><?=$e($item['fulfillment_note'])?></td></tr>
-				<tr><th>Inventory Date</th><td><?=$e($item['inventory_date'])?></td></tr>
-				<tr><th>Inventory Number</th><td><?=$e($item['inventory_number'])?></td></tr>
-				<tr><th>Internal Note 3</th><td><?=$e($item['internal_note_3'])?></td></tr>
-				<tr><th>Statistics Note 3</th><td><?=$e($item['statistics_note_3'])?></td></tr>
-              </table>
+              <form method="post" action="UnboxingWorkflow.php">
+			  <div class="form-row col-12 pb-4">
+					<label class="col-3 form-check-label" for="barcode">Barcode:</label>
+					<input class="col-9 form-control znew" type="text" name="barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
+				  </div>
+				</div>
+                <input class="btn btn-primary btn-lg active" type="submit" value="Submit">
+              </form>
