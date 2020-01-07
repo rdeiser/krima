cc4841313197c586abeeb21c8c9c61d0079dfff9
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-07 08:57:20 -0600

    Update leftBody.php

--- grimas/DisplayItem/splats/leftBody.php
+++ grimas/DisplayItem/splats/leftBody.php
@@ -1,7 +1,8 @@
               <form method="post" action="UnboxingWorfklow.php">
 			  <input type="hidden" name="unboxed_barcode" value="<?=$e(item['unboxed_barcode'])?>">
-              </form>
-			  <div class="form-row col-12 pb-4">
+				<div class="form-row col-12 pb-4">
 					<label class="col-3 form-check-label" for="unboxed_barcode">Barcode:</label>
 					<input class="col-9 form-control znew" type="text" name="barcode" id="barcode" size="20" placeholder="SCAN NEW BARCODE"/>
-				  </div>
+				</div>
+				<input class="btn btn-primary btn-sm active" type="submit" value="Submit">
+              </form>
