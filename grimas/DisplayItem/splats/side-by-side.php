c8c479dc9f76cdf56ef2b9d32a8aece87acb8497
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-07 08:14:32 -0600

    Update side-by-side.php

--- grimas/DisplayItem/splats/side-by-side.php
+++ grimas/DisplayItem/splats/side-by-side.php
@@ -1,6 +1,6 @@
         <h1 class="page-header text-center"><?=$e($title)?></h1>
         <div class="row">
-          <div class="col">
+          <!--<div class="col">
             <div class="card">
               <div class="card-header">
                 <h2 class="card-title"><?=$e($leftTitle)?></h2>
@@ -9,7 +9,7 @@
 <?= $t('leftBody', array('item' => $item)) ?>
               </div>
             </div>
-          </div>
+          </div>-->
           <div class="col">
             <div class="card">
               <div class="card-header">
