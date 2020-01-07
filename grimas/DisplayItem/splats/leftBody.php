669c1f064b7c013b51006cfef47b77e1f509d649
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-07 09:35:31 -0600

    Update leftBody.php

--- grimas/DisplayItem/splats/leftBody.php
+++ grimas/DisplayItem/splats/leftBody.php
@@ -1,8 +1,8 @@
               <form method="post" action="DisplayItem.php">
-			  <input type="hidden" name="unboxed_barcode" value="<?=$e(item['barcode'])?>">
+			  <input type="hidden" name="unboxed_barcode" value="<?=$e(this['barcode'])?>">
 				<div class="form-row col-12 pb-4">
 					<label class="col-3 form-check-label" for="unboxed_barcode">Barcode:</label>
-					<input class="col-9 form-control znew" type="text" name="barcode" id="barcode" size="20" placeholder="SCAN NEW BARCODE"/>
+					<input class="col-9 form-control znew" type="text" name="barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
 				</div>
 				<input type="hidden" name="adding" value="true"> <!-- override button -->
 				<input class="btn btn-primary btn-sm active" type="submit" value="Submit">
