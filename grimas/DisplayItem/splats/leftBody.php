7728d44561ab085cec74e9940a50adbcba4411af
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-07 08:46:53 -0600

    Update leftBody.php

--- grimas/DisplayItem/splats/leftBody.php
+++ grimas/DisplayItem/splats/leftBody.php
@@ -1,11 +1,11 @@
 <?php
 
-require("rdeiser17/git_environment/krima/grimas/UnboxingWorkflow/UnboxingWorkflow.php")
+include('/home/rdeiser17/git_environment/krima/grimas/UnboxingWorkflow/UnboxingWorkflow.php');
  ?>
               <form method="post" action="UnboxingWorfklow.php">
 			  <input type="hidden" name="unboxed_barcode" value="<?=$e(item['barcode'])?>">
               </form>
 			  <div class="form-row col-12 pb-4">
 					<label class="col-3 form-check-label" for="unboxed_barcode">Barcode:</label>
-					<input class="col-9 form-control znew" type="text" name="unboxed_barcode" id="unboxed_barcode" size="20" placeholder="SCAN NEW BARCODE"/>
+					<input class="col-9 form-control znew" type="text" name="unboxed_barcode" id="barcode" size="20" placeholder="SCAN NEW BARCODE"/>
 				  </div>
