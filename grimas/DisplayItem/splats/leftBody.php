5ae66fc9a30927b6853a75a30aa87d35d4db5532
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-07 12:51:24 -0600

    Update leftBody.php

--- grimas/DisplayItem/splats/leftBody.php
+++ grimas/DisplayItem/splats/leftBody.php
@@ -7,4 +7,4 @@
 				<input type="hidden" name="adding" value="true">
 				<input class="btn btn-primary btn-sm active" type="submit" value="Submit">
               </form>-->
-			  <input class="btn btn-primary btn-sm active" onclick="history.go(-1);" type="submit" value="Back">
+			  
