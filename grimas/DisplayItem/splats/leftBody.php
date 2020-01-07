24b42fda7c4f6d1fd1ec5ee318367b9e26c2a8fd
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-07 11:29:28 -0600

    Update leftBody.php

--- grimas/DisplayItem/splats/leftBody.php
+++ grimas/DisplayItem/splats/leftBody.php
@@ -1,4 +1,5 @@
               <form method="post" action="DisplayItem.php">
+			  <input type="hidden" name="adding" value="true">
 			  <input type="hidden" name="next_barcode" value="<?=$e($item['barcode'])?>">
 				<div class="form-row col-12 pb-4">
 					<label class="col-3 form-check-label" for="unboxed_barcode">Barcode:</label>
