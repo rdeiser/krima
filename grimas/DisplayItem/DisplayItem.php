9cb56de9bad358fb2a3f2558a9475984637e8294
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-07 12:03:27 -0600

    next

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -12,7 +12,7 @@ class DisplayItem extends GrimaTask {
 		if (isset($this['adding']) and ($this['adding'] == "true")) {
 			foreach ($this->item as $barcode) {
 			$this->item = new Item();
-			$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
+			$this->item->loadFromAlmaBarcode($this['next_barcode']);
 			$item['internal_note_3'] = ('Inventory Date:').date("m/d/Y");
 			$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
 			$item['inventory_date'] = date("Y-m-d");
--- grimas/DisplayItem/splats/leftBody.php
+++ grimas/DisplayItem/splats/leftBody.php
@@ -1,7 +1,7 @@
               <form method="post" action="DisplayItem.php">
-			  <input type="hidden" name="unboxed_barcode" value="<?=$e($item['barcode'])?>">
+			  <input type="hidden" name="next_barcode" value="<?=$e($item['barcode'])?>">
 				<div class="form-row col-12 pb-4">
-					<label class="col-3 form-check-label" for="unboxed_barcode">Barcode:</label>
+					<label class="col-3 form-check-label" for="next_barcode">Barcode:</label>
 					<input class="col-9 form-control znew" type="text" name="barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
 				</div>
 				<input type="hidden" name="adding" value="true"> <!-- override button -->
