d389f3d46dc4bfd6b4a146894ba834ced8b521d6
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-09 11:05:30 -0600

    moved to bottom

--- grimas/DisplayItemB/splats/rightBody.php
+++ grimas/DisplayItemB/splats/rightBody.php
@@ -11,4 +11,11 @@
 				<tr><th>Internal Note 3</th><td><?=$e($item['internal_note_3'])?></td></tr>
 				<tr><th>Statistics Note 3</th><td><?=$e($item['statistics_note_3'])?></td></tr>
               </table>
-			  <input class="btn btn-primary btn-sm active" onclick="history.go(-1);" type="submit" value="Back"/>
+			  <!--<input class="btn btn-primary btn-sm active" onclick="history.go(-1);" autofocus="autofocus" type="submit" value="Back"/>-->
+			  <form method="post" action="../UnboxingWorkflowB/UnboxingWorkflowB.php">
+				<div class="form-row col-12 pb-4">
+					<label class="col-3 form-check-label" for="barcode">Barcode:</label>
+					<input class="col-9 form-control znew" type="text" name="unboxed_barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
+				</div>
+				<input class="btn btn-primary btn-sm active" type="submit" value="Submit">
+              </form>
--- grimas/DisplayItemB/splats/success.php
+++ grimas/DisplayItemB/splats/success.php
@@ -2,7 +2,7 @@
 		array(
 			'item' => $item,
 			'title' => 'Item Display',
-			'leftTitle' => 'Scan Next Barcode',
+			/*'leftTitle' => 'Scan Next Barcode',*/
 			'rightTitle' => 'Item Information:',
 		 )
 	)
