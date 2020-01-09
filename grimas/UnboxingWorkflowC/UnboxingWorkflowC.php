39f7196d06ef9e54c25aa52fd8d7865514c4ef22
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-09 15:24:20 -0600

    adding C

---
+++ grimas/UnboxingWorkflowC/UnboxingWorkflowC.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("../grima-lib.php");
+
+class UnboxingWorkflowC extends GrimaTask {
+
+	function do_task() {
+		$item = new Item();
+		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$item['internal_note_3'] = ('Inventory Date:').date("m/d/Y");
+		$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
+		$item['inventory_date'] = date("Y-m-d");
+		$item->updateAlma();
+}
+	function print_success(){
+			GrimaTask::call('DisplayItemB', array('unboxed_barcode' => $this['unboxed_barcode']));
+	}
+}
+
+UnboxingWorkflowC::RunIt();
