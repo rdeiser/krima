8b0cab10f9a93ca8071df11f55a32e3c57b84873
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-09 10:43:36 -0600

    alternative

--- grimas/DisplayItemB/splats/leftBody.php
+++ grimas/DisplayItemB/splats/leftBody.php
@@ -1,9 +1,8 @@
               <form method="post" action="../UnboxingWorkflow/UnboxingWorkflow.php">
 				<div class="form-row col-12 pb-4">
 					<label class="col-3 form-check-label" for="barcode">Barcode:</label>
-					<input class="col-9 form-control znew" type="text" name="next_barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
+					<input class="col-9 form-control znew" type="text" name="unboxed_barcode" id="barcode" size="20" placeholder="SCAN NEXT BARCODE"/>
 				</div>
-				<input type="hidden" name="adding" value="true">
 				<input class="btn btn-primary btn-sm active" type="submit" value="Submit">
               </form>
 			  
