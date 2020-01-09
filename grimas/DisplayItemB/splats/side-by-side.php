d5c6da60f826278155821caa4524f09a9329e5ba
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-09 11:14:27 -0600

    Update side-by-side.php

--- grimas/DisplayItemB/splats/side-by-side.php
+++ grimas/DisplayItemB/splats/side-by-side.php
@@ -1,15 +1,5 @@
         <h1 class="page-header text-center"><?=$e($title)?></h1>
         <div class="row">
-          <!--<div class="col">
-            <div class="card">
-              <div class="card-header">
-                <h2 class="card-title"><?/*=$e($leftTitle)?></h2>
-              </div>
-              <div class="card-body">
-<?= $t('leftBody', array('item' => $item)) */?>
-              </div>
-            </div>
-          </div>-->
           <div class="col">
             <div class="card">
               <div class="card-header">
