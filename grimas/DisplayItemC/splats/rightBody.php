118bbc321b0d204f7a6f879f81291c54e6bbddb7
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-13 15:14:03 -0600

    Update rightBody.php

--- grimas/DisplayItemC/splats/rightBody.php
+++ grimas/DisplayItemC/splats/rightBody.php
@@ -1,15 +1,15 @@
               <table class="table">
                 <tr><th class="flip"><span>Title:</span><span>Título:</span></th><td><?=$e($item['title'])?></td></tr>
 				<tr><th class="flip"><span>Call Number:</span><span>Número de clasificación:</span></th><td><?=$e($item['call_number'])?></td></tr>
-				<tr><th class="flip"><span>Description:</span><span>Descripción</span></th><td><?=$e($item['description'])?></td></tr>
-				<tr><th class="flip"><span>Barcode:</span><span>códigos de barras</span></th><td><?=$e($item['barcode'])?></td></tr>
-				<tr><th class="flip"><span>Location:</span><span>Ubicación</span></th><td><?=$e($item['location'])?></td></tr>
-				<tr><th class="flip"><span>Process Type:</span><span>Tipo de Proceso</span></th><td><?=$e($item['process_type'])?></td></tr>
-				<tr><th class="flip"><span>Fulfillment Note</span><span>Nota de cumplimiento</span></th><td><?=$e($item['fulfillment_note'])?></td></tr>
-				<tr><th class="flip"><span>Inventory Date</span><span>Fecha de inventario</span></th><td><?=$e($item['inventory_date'])?></td></tr>
-				<tr><th>Inventory Number></th><td><?=$e($item['inventory_number'])?></td></tr>
-				<tr><th>Internal Note 3</th><td><?=$e($item['internal_note_3'])?></td></tr>
-				<tr><th>Statistics Note 3</th><td><?=$e($item['statistics_note_3'])?>
+				<tr><th class="flip"><span>Description:</span><span>Descripción:</span></th><td><?=$e($item['description'])?></td></tr>
+				<tr><th class="flip"><span>Barcode:</span><span>códigos de barras:</span></th><td><?=$e($item['barcode'])?></td></tr>
+				<tr><th class="flip"><span>Location:</span><span>Ubicación:</span></th><td><?=$e($item['location'])?></td></tr>
+				<tr><th class="flip"><span>Process Type:</span><span>Tipo de Proceso:</span></th><td><?=$e($item['process_type'])?></td></tr>
+				<tr><th class="flip"><span>Fulfillment Note:</span><span>Nota de cumplimiento:</span></th><td><?=$e($item['fulfillment_note'])?></td></tr>
+				<tr><th class="flip"><span>Inventory Date</span><span>Fecha de inventario:</span></th><td><?=$e($item['inventory_date'])?></td></tr>
+				<tr><th>Inventory Number:</th><td><?=$e($item['inventory_number'])?></td></tr>
+				<tr><th>Internal Note 3:</th><td><?=$e($item['internal_note_3'])?></td></tr>
+				<tr><th>Statistics Note 3:</th><td><?=$e($item['statistics_note_3'])?>
 				</td></tr>
               </table>
 			  <!--<input class="btn btn-primary btn-sm active" onclick="history.go(-1);" autofocus="autofocus" type="submit" value="Back"/>-->
