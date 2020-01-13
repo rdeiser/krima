9c7148da165fc81c4e98cff1fc6708352a6855c8
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-13 13:22:43 -0600

    center

--- grimas/DisplayItemC/splats/success.php
+++ grimas/DisplayItemC/splats/success.php
@@ -1,7 +1,7 @@
 <?= $t('side-by-side', 
 		array(
 			'item' => $item,
-			'title' => 'Unboxing WorkflowC',
+			'title' => 'Item Information:',
 			/*'leftTitle' => 'Scan Next Barcode',*/
 			'rightTitle' => 'Item Information:',
 		 )
