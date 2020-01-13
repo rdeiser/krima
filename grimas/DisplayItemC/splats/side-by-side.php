7a4effeaabc2bd75434b50cc63bad89f107fbc75
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-13 12:34:06 -0600

    Update side-by-side.php

--- grimas/DisplayItemC/splats/side-by-side.php
+++ grimas/DisplayItemC/splats/side-by-side.php
@@ -1,5 +1,5 @@
 <?php if ($item['statistics_note_3']=='ANNEX ingest');
-					$style = 'class="statnote"';
+					$style = 'style="color: green;"';
 					?>
 		<h1 class="page-header text-center"><?=$e($title)?></h1>
         <div class="row">
