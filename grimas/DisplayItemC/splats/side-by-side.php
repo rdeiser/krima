bfe4c1b5c7e5e82737513baab399d5c04be197e8
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-13 12:46:14 -0600

    Update side-by-side.php

--- grimas/DisplayItemC/splats/side-by-side.php
+++ grimas/DisplayItemC/splats/side-by-side.php
@@ -1,10 +1,14 @@
-<?php if ($item['statistics_note_3']=='ANNEX ingest');
-					$style = 'class="statnote;"';
+<?php if ($item['statistics_note_3']=='To be WITHDRAWN');
+			$style = 'style="background-color: #cd5555;"';
+	else if ($item['statistics_note_3']=='ANNEX ingest');
+			$style = 'style="background-color: #6495ed;"';
+	else if ($item['statistics_note_3']=='HALE return');
+			$style = 'style="background-color: #4f2684;"';
 					?>
 		<h1 class="page-header text-center"><?=$e($title)?></h1>
         <div class="row">
           <div class="col">
-            <div <?=$style?>>
+            <div class="card" <?=$style?>>
               <div class="card-header">
                 <h2 class="card-title"><?=$e($rightTitle)?></h2>
               </div>
