29398cd654fa72818e7fca7a32f31008f1608abf
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-14 12:07:25 -0600

    Update rightBody.php

--- grimas/DisplayItemC/splats/rightBody.php
+++ grimas/DisplayItemC/splats/rightBody.php
@@ -19,7 +19,7 @@ if ($item['process_type']=='') {
 				<tr><th>Internal Note 3:</th><td><?=$e($item['internal_note_3'])?></td></tr>
 				<tr><th>Statistics Note 3:</th><td class="statnote"><?=$e($item['statistics_note_3'])?>
 				</td></tr>
-				<tr></tr>
+				<th></th>
               </table>
 			  <!--<input class="btn btn-primary btn-sm active" onclick="history.go(-1);" autofocus="autofocus" type="submit" value="Back"/>-->
 			  <form method="post" action="../UnboxingWorkflowC/UnboxingWorkflowC.php">
