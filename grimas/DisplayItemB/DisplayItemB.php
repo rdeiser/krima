d7346425c627b2b1fb90c3353daf6a2169ab1e4a
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-23 14:56:46 -0600

    Update DisplayItemB.php

--- grimas/DisplayItemB/DisplayItemB.php
+++ grimas/DisplayItemB/DisplayItemB.php
@@ -5,20 +5,17 @@ require_once("../grima-lib.php");
 class DisplayItemB extends GrimaTask {
 
 	function do_task() {
+		$item = new Item();
+		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$item['internal_note_3'] = ('Inventory Date:').date("m/d/Y");
+		$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
+		$item['inventory_date'] = date("Y-m-d");
+		$item->updateAlma();
+}
+	function do_task() {
 
 		$this->item = new Item();
 		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
-		//For future testing to combine both grimas into one file-red 01/2020
-		/*if (isset($this['adding']) and ($this['adding'] == "true")) {
-			foreach ($this->item as $barcode) {
-			$this->item = new Item();
-			$this->item->loadFromAlmaBarcode($this['next_barcode']);
-			$item['internal_note_3'] = ('Inventory Date:').date("m/d/Y");
-			$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
-			$item['inventory_date'] = date("Y-m-d");
-			$item->updateAlma();
-	}
-		}*/
 		$this->splatVars['item'] = $this->item;
 }
 }
