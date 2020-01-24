5c05e1c4a21701b930c1ba44d6c2aeb0e617ec4d
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-01-24 14:21:40 -0600

    Update rightBody.php

--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -55,7 +55,7 @@ if (preg_match("/[sS]end/", $item['fulfillment_note'])) {
 else if (preg_match("/[dD]bm/", $item['fulfillment_note'])) {
 		$style2 = 'style="background-color:#cd3700;"';
 }
-else if (preg_match("/[dD]DM/", $item['fulfillment_note'])) {
+else if (preg_match("/[dD]BM/", $item['fulfillment_note'])) {
 		$style2 = 'style="background-color:#cd3700;"';
 }
 	else if ($item['fulfillment_note']) {
