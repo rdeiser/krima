72d5d3a756cb9a5715a064de9e7f1b101448069c
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-02-03 15:40:43 -0600

    Update rightBody.php

--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -63,17 +63,17 @@ else if (preg_match("/[dD]BM/", $item['fulfillment_note'])) {
 }
 ?>
               <table class="table">
-                <tr><th>Título:</th><td><?=$e($item['title'])?></td></tr>
-				<tr><th>Número de clasificación:</th><td><?=$e($item['call_number'])?></td></tr>
-				<tr><th>Descripción:</th><td><?=$e($item['description'])?></td></tr>
-				<tr><th>Código de barras:<!--Código de procedencia:--></th><td><?=$e($item['barcode'])?></td></tr>
+                <tr><th class="flip"><span>Title:</span><span>Título:</span></th><td><?=$e($item['title'])?></td></tr>
+				<tr><th class="flip"><span>Call Number:</span><span>Número de clasificación:</span></th><td><?=$e($item['call_number'])?></td></tr>
+				<tr><th class="flip"><span>Description:</span><span>Descripción:</span></th><td><?=$e($item['description'])?></td></tr>
+				<tr><th class="flip"><span>Barcode:</span><span>Código de barras:<!--Código de procedencia:--></span></th><td><?=$e($item['barcode'])?></td></tr>
 				<!--<tr><th class="flip"><span>Location:</span><span>Lugar:</span></th><td><?=$e($item['location'])?></td></tr>-->
-				<tr <?=$style?>><th>Tipo de Proceso:</th><td><?=$e($item['process_type'])?></td></tr>
-				<tr <?=$style2?>><th>Nota de Procesamiento:<!--Nota de servicios al usuario:--></th><td><?=$e($item['fulfillment_note'])?></td></tr>
+				<tr <?=$style?>><th class="flip"><span>Process Type:</span><span>Tipo de Proceso:</span></th><td><?=$e($item['process_type'])?></td></tr>
+				<tr <?=$style2?>><th class="flip"><span>Fulfillment Note:</span><span>Nota de Procesamiento:<!--Nota de servicios al usuario:--></span></th><td><?=$e($item['fulfillment_note'])?></td></tr>
 				<!--<tr><th class="flip"><span>Inventory Date:</span><span>Fecha de inventario:</span></th><td><?=$e($item['inventory_date'])?></td></tr>
 				<tr><th>Inventory Number:</th><td><?=$e($item['inventory_number'])?></td></tr>
 				<tr><th>Internal Note 3:</th><td><?=$e($item['internal_note_3'])?></td></tr>-->
-				<tr><th>Destino:</th><td class="statnote"><?=$e($item['statistics_note_3'])?>
+				<tr><th class="flip"><span>Destination:</span><span>Destino:</span></th><td class="statnote"><?=$e($item['statistics_note_3'])?>
 				</td></tr>
 				<tr><th></th><td></td></tr>
               </table>
