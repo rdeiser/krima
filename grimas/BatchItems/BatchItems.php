e71d81937a77523d173a9202d037ce45205d3a74
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-23 10:24:44 -0500

    Update BatchItems.php

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -3,8 +3,24 @@
 require_once("../grima-lib.php");
 
 class BatchItems extends GrimaTask {
-	public $biblist = array();
+	public $holdinglist = array();
 	
 	function do task() {
-		$this->
+		$this->holding = preg_split('/\r\n|\r|\n/',$this['holding_id']);
+
+		#pull holdings
+		foreach (this->holding as $holding_id) {
+			$holding = new Holding();
+			$holding->loadFromAlma($holding_id);
+			$this->holdinglist[] = $holding;
+		}
+		foreach ($itemdata_elements as $element) {
+				if (isset($this[$element])) {
+					$newItem[$element] = $this[$element];
+				}
+			}
+			$ret = $newItem->addToAlmaHolding($this->item['mms_id'],$this->item['holding_id']);
+			$this->item = new Item();
+			$this->item->xml = $ret;
+		}
 BatchItems::RunIt();
