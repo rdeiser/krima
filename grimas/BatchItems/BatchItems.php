f63750e46b75a78ef7733fd1cf7f8793b3f3bd4d
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-24 08:37:03 -0500

    Update BatchItems.php

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -20,7 +20,7 @@ class BatchItems extends GrimaTask {
 		$mfhd = $this->biblist[0]->holdings[0];
 
 		foreach ($this->holdings as $mfhd) {
-			$this->item = new Item();
+			$mfhd->item = new Item();
 			$newItem->addToAlmaHolding($this->item['mms_id'],$this->item['holding_id']);
 		}
 	}
