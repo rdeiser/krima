e9159bbbe129035ba3379a7561d00bba097739d6
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-27 09:10:22 -0500

    continued testing

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -11,6 +11,13 @@ class BatchItems extends GrimaTask {
 		foreach ($this->holding as $holdingid) {
 			$this->holding = new Holding();
 			$this['mms_id']=Holding::getMmsFromHoldingID($this['holding_id']);
+			if ($this['mms_id']){
+				$this->holding->loadFromAlma(this['mms_id'],$this['holding_id']);
+				$this->holding->getItems();
+			$this->splatVars['holding']=$this->holding;
+			}else{
+				$this->splatVars['message']=$this->message;
+			}
 	}
 }
 }
