db4d3c3c215b98234c11d0982ccf02c5f4b41276
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-27 10:51:12 -0500

    Update BatchItems.php

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -13,6 +13,7 @@ class BatchItems extends GrimaTask {
 			$this->holdinglist[] = $holding;
 		}
 		
+		$this->splatVars['width'] = 12;
 		$this->splatVars['holdinglist'] = $this->holdinglist;
 		$this->splatVars['body'] = array( 'list', 'messages' );
 	}
