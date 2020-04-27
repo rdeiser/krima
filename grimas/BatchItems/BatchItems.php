9a5bfbd1446a6dc9d4f82712fce011e99ac0de07
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-27 08:23:48 -0500

    continurd testing

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -3,37 +3,18 @@
 require_once("../grima-lib.php");
 
 class BatchItems extends GrimaTask {
-	public $biblist = array();
+	public $holdinglist = array();
 	
 	function do_task() {
-		$this->bibs = preg_split('/\r\n|\r|\n/',$this['mms']);
+		$this->mfhd = preg_split('/\r\n|\r|\n/',$this['mfhd']);
 
 		# BIBS
-		foreach ($this->bibs as $mmsid) {
-			$bib = new Bib();
-			$bib->loadFromAlma($mmsid);
-			$this->biblist[] = $bib;
+		foreach ($this->holdings as $mfhd) {
+			$holding = new Holding();
+			$holding->loadFromAlma($this['holding_id']);
+			$this->holdinglist[] = $holding;
 		}
 
-		## HOLDING
-		$this->biblist[0]->getHoldings();
-		$mfhd = $this->biblist[0]->holdings[0];
-
-		foreach ($this->biblist as $k => $bib) {
-			if ($holding['library_code']=='MAIN') {
-				function postItem($mms_id,$holding_id,$item{
-					$ret = $this->post('/almaws/v1/bibs/{mms_id}/holdings/{holding_id}/items',
-			array('mms_id' => $mms_id, 'holding_id' => $holding_id),
-			array(),
-			$item
-			);
-		$this->checkForErrorMessage($ret);
-		return $ret;
-	
-			else if ($holding['library_code']==''){
-			}
-			}
-		}
 		$this->splatVars['width'] = 12;
 		$this->splatVars['biblist'] = $this->biblist;
 		$this->splatVars['body'] = array( 'list', 'messages' );
