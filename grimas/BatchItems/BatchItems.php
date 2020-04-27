4379b6280af37c2b378e4990b0319351b2f82695
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-27 08:36:12 -0500

    Update BatchItems.php

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -13,11 +13,9 @@ class BatchItems extends GrimaTask {
 			$holding = new Holding();
 			$holding->loadFromAlma($this['holding_id']);
 			$this->holdinglist[] = $holding;
-		}/*
+		}
 
-		$this->splatVars['width'] = 12;
-		$this->splatVars['holdinglist'] = $this->holdinglist;
-		$this->splatVars['body'] = array( 'list', 'messages' );*/
+		$this->splatVars['holding'] = $this->holding;
 
 
 	}
