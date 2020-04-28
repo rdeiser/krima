9b0a83cebdb77f74b393ee526d02aa7f0b39694e
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-28 08:29:57 -0500

    Update BatchItems.php

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -6,15 +6,15 @@ class BatchItems extends GrimaTask {
 
 	function do_task() {
 		$this->holdings = preg_split('/\r\n|\r|\n/',$this['holding_id']);
-		foreach ($this->holdings as $holdingid) {
+		foreach ($this->holdings as $mfhd) {
 			$holding = new Holding();
 			$holding['mms_id'] = Holding::getMmsFromHoldingID($holding['holding_id']);
-			$holding->holding->loadFromAlma($this['mms_id'],$this[$holdingid]);
+			$holding->holding->loadFromAlma($this['mms_id'],$this[$mfhd]);
 			$this->holdinglist[] = $holding;
 		}
-		$this->splatVars['width'] = 12;
+		/*$this->splatVars['width'] = 12;
 		$this->splatVars['holdinglist'] = $this->holdinglist;
-		$this->splatVars['body'] = array( 'list', 'messages' );
+		$this->splatVars['body'] = array( 'list', 'messages' );*/
 	}
 }
 BatchItems::RunIt();
