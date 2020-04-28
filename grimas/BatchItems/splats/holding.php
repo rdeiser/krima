cda9e2231d955d88e6e673f3d200bcb2b6a7b125
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-28 08:34:29 -0500

    Update holding.php

--- grimas/BatchItems/splats/holding.php
+++ grimas/BatchItems/splats/holding.php
@@ -1,23 +1,40 @@
-<div class='card ml-4 holding'>
+<div class='card mt-2 bib'>
   <div class='card-header'>
-    <h2 class='card-title'>Holding #<?=$e($holding['holding_id'])?>
+    <h1 class='card-title'>Holding #<?=$e($holding['holding_id'])?>
       <a class='d-print-none viewlink' href="../PrintHolding/PrintHolding.php?holding_id=<?=$e($holding['holding_id'])?>">(view)</a>
-  </h2>
+    </h1>
   </div>
   <div class='card-body'>
-    <dl class="row">
-      <dt class="col-md-2 text-right library">Library:</dt>
-      <dd class="col-md-10 library"><?=$e($holding['library_code'] . " : " . 
-          $holding['library'])?></dd>
-      <dt class="col-md-2 text-right location">Location:</dt>
-      <dd class="col-md-10 location"><?=$e($holding['location_code'] . " : " . 
-          $holding['location'])?></dd>
-      <dt class="col-md-2 text-right title">Call Number:</dt>
-      <dd class="col-md-10 title"><?=$e($holding['call_number'])?></dd>
-    </dl>
-    <?php foreach ($holding->itemList->items as $item): ?>
-      <?=$t('item', array('item' => $item))?>
+    <div class="table">
+      <table class="table">
+      <tr>
+        <th>Item PID</th>
+        <th>Barcode</th>
+        <th>Material Type</th>
+        <th>Item Policy</th>
+        <th>PO Line</th>
+        <th>Description</th>
+        <th>In Temp Location?</th>
+        <th>Temp Library</th>
+        <th>Temp Location</th>
+        <th>Process Type</th>
+        <th>Public Note</th>
+      </tr>
+    <?php foreach ($holding->items as $item): ?>
+      <tr>
+        <td><?=$e($item['item_pid'])?></td>
+        <td><?=$e($item['barcode'])?></td>
+        <td><?=$e($item['physical_material_type'])?></td>
+        <td><?=$e($item['item_policy'])?></td>
+        <td><?=$e($item['po_line'])?></td>
+        <td><?=$e($item['description'])?></td>
+        <td><?=$e($item['in_temp_location'])?></td>
+        <td><?=$e($item['temp_library'])?></td>
+        <td><?=$e($item['temp_location'])?></td>
+        <td><?=$e($item['process_type'])?></td>
+        <td><?=$e($item['public_note'])?></td>
+      </tr>
     <?php endforeach ?>
+    </div>
   </div>
 </div>
-           
