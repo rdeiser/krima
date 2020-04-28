c267ec9d8f56d7c1a8df4ad412efa5d7c48ff608
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-28 08:41:45 -0500

    Update BatchItems.php

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -7,10 +7,10 @@ class BatchItems extends GrimaTask {
 
 	function do_task() {
 		$this->holdings = preg_split('/\r\n|\r|\n/',$this['holding_id']);
-		foreach ($this->holdings as $mfhd) {
+		foreach ($this->holdings as $holdingid) {
 			$holding = new Holding();
 			$holding['mms_id'] = Holding::getMmsFromHoldingID($holding['holding_id']);
-			$holding->holding->loadFromAlma($this['mms_id'],$mfhd);
+			$holding->holding->loadFromAlma($this['mms_id'],$this['holdingid']);
 			$this->holding->getItems();
 			$this->splatVars['holding'] = $this->holding;
 			$this->holdinglist[] = $holding;
