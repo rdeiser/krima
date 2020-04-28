900062db45e05268dfd7b68ad8d11edddbdd69cf
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-28 10:16:13 -0500

    Update BatchItems.php

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -12,18 +12,19 @@ class BatchItems extends GrimaTask {
 			$this->holding = new Holding();
 			$this['mms_id'] = Holding::getMmsFromHoldingID($holdingid);
 			$this->holding->loadFromAlma($this['mms_id'],$holdingid);
-			$this->addToAlmaHolding($this['mms_id'],$holdingid,$item);
+			//$this->addToAlmaHolding($this['mms_id'],$holdingid,$item);
 			//$this->holding->postItem();
-			$this->holding->getItems();
+			//$this->holding->getItems();
 			$this->holdinglist[] = $holding;
 		}
 
-		/*foreach ($this->holdinglist as $newitem) {
+		foreach ($this->holdinglist as $newitem) {
 			unset($newItem['item_pid']);
-			$newItem->addToAlmaHolding($this['mms_id'],$holdingid);
+			$newItem->addToAlmaHolding($this['mms_id'],$holdingid,$item);
 			$this->item = new Item();
-			$item->updateAlma();
-		}*/
+			//$item->updateAlma();
+		}
+		$this->holding->getItems();
 		$this->splatVars['holding'] = $this->holding;
 		/*$this->splatVars['width'] = 12;
 		$this->splatVars['holdinglist'] = $this->holdinglist;
