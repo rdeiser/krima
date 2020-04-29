b58b9f6755a64cd9ad4f585da4dd57472e95a163
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-29 10:10:00 -0500

    Update holding.php

--- grimas/BatchItems/splats/holding.php
+++ grimas/BatchItems/splats/holding.php
@@ -1,5 +1,6 @@
 <div class='card mt-2 bib'>
   <div class='card-header'>
+  <?php foreach($holdinglist as $holding): ?>
     <h1 class='card-title'>Holding #<?=$e($holding['holding_id'])?>
       <a class='d-print-none viewlink' href="../PrintHolding/PrintHolding.php?holding_id=<?=$e($holding['holding_id'])?>">(view)</a>
     </h1>
@@ -38,3 +39,4 @@
     </div>
   </div>
 </div>
+<?php endforeach?>
