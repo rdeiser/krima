4611bc400cd69dfa6a9bf0976892292907a18c1f
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-29 08:47:34 -0500

    Update list.php

--- grimas/BatchItems/splats/list.php
+++ grimas/BatchItems/splats/list.php
@@ -1,6 +1,7 @@
 <ul>
     <?php foreach ($holdinglist as $holding): ?>
-    <li>(<a href="../PrintHolding/PrintHolding.php?holding_id=<?= $holding['holding_id']?>">view record</a>)
+    <li><?=$e($holding['holding_id'])?>
+	(<a href="../PrintHolding/PrintHolding.php?holding_id=<?= $holding['holding_id']?>">view record</a>)
 </li>
     <?php endforeach ?>
 </ul>
