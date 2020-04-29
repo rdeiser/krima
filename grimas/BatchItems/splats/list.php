bfeb07c4018615bdef391b922476285062d32839
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-29 09:47:50 -0500

    Update list.php

--- grimas/BatchItems/splats/list.php
+++ grimas/BatchItems/splats/list.php
@@ -1,11 +1,9 @@
 <ul>
+<h1 class='card-title'>Holding #<?=$e($holding['holding_id'])?>
+      <a class='d-print-none viewlink' href="../PrintHolding/PrintHolding.php?holding_id=<?=$e($holding['holding_id'])?>">(view)</a>
+    </h1>
     <?php foreach ($holdinglist as $holding): ?>
     <li>Holding #<?=$e($holding['holding_id'])?>
 	(<a href="../PrintHolding/PrintHolding.php?holding_id=<?=$e($holding['holding_id'])?>">Print Record</a>)
 </li>
     <?php endforeach ?>
-	<?php foreach ($holdinglist->items as $item): ?>
-<li><?=$e($item['item_pid'])?>
-<?=$e($item['barcode'])?>
-<?php endforeach ?></li>
-</ul>
