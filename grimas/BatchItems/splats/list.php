e8228db40525b691803e02c33012bcc319297550
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-29 09:38:24 -0500

    Update list.php

--- grimas/BatchItems/splats/list.php
+++ grimas/BatchItems/splats/list.php
@@ -1,15 +1,6 @@
 <ul>
-<?php foreach($holdinglist as $holding): ?>
-<li><?= $holding->get_title_proper() ?>
- (<a href="../Hierarchy/Hierarchy.php?mms_id=<?= $holding['mms_id']?>">hierarchy</a>)
- (<a href="../PrintBib/PrintBib.php?mms_id=<?= $holding['mms_id']?>">view record</a>)
-</li>
-<?php endforeach ?>
-</ul>
-
-<ul>
     <?php foreach ($holdinglist as $holding): ?>
-    <li><?=$e($holding['holding_id'])?>
+    <li>Holding #<?=$e($holding['holding_id'])?>
 	(<a href="../PrintHolding/PrintHolding.php?holding_id=<?=$e($holding['holding_id'])?>">Print Record</a>)
 </li>
     <?php endforeach ?>
