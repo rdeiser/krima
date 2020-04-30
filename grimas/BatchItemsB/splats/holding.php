2074985afc7d091288a4c32d268952c5b67e529b
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-04-30 10:52:23 -0500

    Sandbox GRIMA

---
+++ grimas/BatchItemsB/splats/holding.php
@@ -0,0 +1,8 @@
+<div class='card mt-2 bib'>
+  <div class='card-header'>
+  <?php foreach($holdinglist as $holding): ?>
+    <h1 class='card-title'>Holding #<?=$e($holding['holding_id'])?>
+      <a class='d-print-none viewlink' href="../PrintHolding/PrintHolding.php?holding_id=<?=$e($holding['holding_id'])?>">(view)</a>
+    </h1>
+  </div>
+<?php endforeach?>
