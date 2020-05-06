7dfa10ca139514a7ddfb9a90b2b02174fdb19bf6
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-05-06 08:23:50 -0500

    Update BatchItems.php

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -19,6 +19,7 @@ class BatchItems extends GrimaTask {
 			$item['statistics_note_3'] = 'HALE return';
 			$item->addToAlmaHolding($this['mms_id'],$holdingid);
 			$this->addMessage('success',"Successfully added an Item Record to {$holdingid}");
+			$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
 			$this->holdinglist[] = $holding;
 		}
 		//$this->addMessage('success',"Successfully added an Item Record to {$holdingid}");
