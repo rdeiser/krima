ba5a13f88d2fe5d8231fd8ab73cd7996255705b5
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-05-06 09:21:22 -0500

    Update BatchItems.php

--- grimas/BatchItems/BatchItems.php
+++ grimas/BatchItems/BatchItems.php
@@ -11,15 +11,18 @@ class BatchItems extends GrimaTask {
 		foreach ($this->holdings as $holdingid) {
 			$holding = new Holding();
 			$this['mms_id'] = Holding::getMmsFromHoldingID($holdingid);
-			$holding->loadFromAlma($this['mms_id'],$holdingid);
-			$item = new Item();
-			$item['barcode'] = '';
-			//$item['inventory_date'] = '1976-01-01';
-			$item['statistics_note_2'] = 'FIRE 2018 OZONE';
-			$item['statistics_note_3'] = 'HALE return';
-			$item->addToAlmaHolding($this['mms_id'],$holdingid);
-			//$this->addMessage('success',"Successfully added an Item Record to {$holdingid}");
-			$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
+			if ($this['mms_id']) {
+				$holding->loadFromAlma($this['mms_id'],$holdingid);
+				$item = new Item();
+				$item['barcode'] = '';
+				//$item['inventory_date'] = '1976-01-01';
+				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
+				$item['statistics_note_3'] = 'HALE return';
+				$item->addToAlmaHolding($this['mms_id'],$holdingid);
+				$this->addMessage('success',"Successfully added an Item Record to {$holdingid}");
+			} else {
+				$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
+			}
 			$this->holdinglist[] = $holding;
 		}
 		//$this->addMessage('success',"Successfully added an Item Record to {$holdingid}");
