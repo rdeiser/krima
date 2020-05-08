fbd9a9e3d3ed49e79dfaa7abce12c6e4e78628d6
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-05-08 07:25:15 -0500

    Update BatchItemsMMS.php

--- grimas/BatchItemsMMS/BatchItemsMMS.php
+++ grimas/BatchItemsMMS/BatchItemsMMS.php
@@ -10,17 +10,24 @@ class BatchItemsMMS extends GrimaTask {
 
 		foreach ($this->bibs as $mmsid) {
 			$bib = new Bib();
-			$this['holding_id'] = Bib::getHoldingIDFromMms($mmsid);
-			if ($this['holding_id']) {
-				$holding->loadFromAlma($mmsid,$this['holding_id']);
-				$item = new Item();
+			$bib->loadFromAlma($mmsid);
+			$bib->getHoldings();
+			if ($holding['library_code'] = 'MAIN') {
+				$holding = $bib->holdings[0];
+				if ($holding['holding_suppress'] = 'true') {
+					addMessage('warn', "Holdings record is suppressed for {$bib['mms_id']}");
+					continue;
+				} else {
+				$holding->getItemList();
+				if (count($holding->itemList->items) = 0) {
+					$item = new Item();
 				$item['barcode'] = '';
 				//$item['inventory_date'] = '1976-01-01';
 				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
 				$item['statistics_note_3'] = 'HALE return';
 				$item->addToAlmaHolding($mmsid,$this['holding_id']);
 				$this->addMessage('success',"Successfully added an Item Record to {$holding['holding_id']}");
-			} else {
+			}} else {
 				$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$mmsid}");
 			}
 			$this->biblist[] = $bib;
