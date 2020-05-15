7d71f02b043b550ed51dd32cd0ec5f5f14d6a053
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-05-15 07:49:22 -0500

    Update DisplayItem.php

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -9,11 +9,7 @@ class DisplayItem extends GrimaTask {
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		//$item['internal_note_3'] = ('Inventory Date:').date("m/d/Y");
 		//$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
-		if ($item['inventory_date'] == "") {
-			$item['inventory_date_new_value'] = date("Y-m-d");
-		} else {
-			$item['inventory_date'] = date("Y-m-d");
-		}
+		$item['inventory_date'] = date("Y-m-d");
 		//$item['inventory_date'] = date("Y-m-d g:i:s A");
 		$item->updateAlma();
 		}
