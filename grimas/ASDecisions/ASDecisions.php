b33b6a9a3ffacc2757f621a320760a6ae0f99123
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-05-26 11:00:55 -0500

    Update ASDecisions.php

--- grimas/ASDecisions/ASDecisions.php
+++ grimas/ASDecisions/ASDecisions.php
@@ -11,15 +11,14 @@ class ASDecisions extends GrimaTask {
 		foreach ($this->barcodes as $barcode) {
 			$item = new Item();
 			$item->loadFromAlmaBarcode($barcode);
-			if (empty($item['statistics_note_2']) and empty($item['statistics_note_3'])) {
+			if (empty($item['statistics_note_2'])) {
 				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
+			} elseif ($item['statistics_note_2'] != '') {}
+			if (empty($item['statistics_note_3'])) {
 				$item['statistics_note_3'] = $this['whichnote'];
-			} elseif ($item['statistics_note_2'] != '' and empty($item['statistics_note_3'])) {
-				$item['statistics_note_3'] = $this['whichnote'];
-			} elseif (empty($item['statistics_note_2']) and $item['statistics_note_3'] != '') {
-				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
-			}
+			} elseif ($item['statistics_note_3'] != '') {}
 			$item->updateAlma();
+			$this->addMessage('success',"Successfully updated Item Recored for:{$item['barcode']}");
 		}
 	}
 }
