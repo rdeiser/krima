002154e3dbe8ecfd7dbc5a1078adc91fdd3b0ff0
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-05-26 11:09:03 -0500

    Update ASDecisions.php

--- grimas/ASDecisions/ASDecisions.php
+++ grimas/ASDecisions/ASDecisions.php
@@ -13,7 +13,7 @@ class ASDecisions extends GrimaTask {
 			$item->loadFromAlmaBarcode($barcode);
 			if (empty($item['statistics_note_2'])) {
 				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
-			} elseif ($item['statistics_note_2'] != '') {}
+			} else {}//elseif ($item['statistics_note_2'] != '') {}
 			if (empty($item['statistics_note_3'])) {
 				$item['statistics_note_3'] = $this['whichnote'];
 			} elseif ($item['statistics_note_3'] != '') {}
