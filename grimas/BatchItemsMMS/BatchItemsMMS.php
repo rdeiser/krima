0c040fac0db330887d4737f57ecc1f74f1e14abd
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-01 13:08:51 -0500

    Update BatchItemsMMS.php

--- grimas/BatchItemsMMS/BatchItemsMMS.php
+++ grimas/BatchItemsMMS/BatchItemsMMS.php
@@ -11,30 +11,30 @@ class BatchItemsMMS extends GrimaTask {
 		foreach ($this->bibs as $mmsid) {
 			$holding = new Holding();
 			$holding->loadFromAlma($mmsid,$this['holding_id']);
+			if ($holding['location_code'] = 'main') {
+				$holding['library_code'] = 'WITHDRAW';
+				$holding['location_code'] = 'wdmain';
+			} else {}
 			if ($holding['location_code'] = 'over') {
 				$holding['library_code'] = 'WITHDRAW';
 				$holding['location_code'] = 'wdover';
-			}/* else {}
-			if ($holding['location_code'] = 'over') {
-				$holding['library_code'] = 'WITHDRAW';
-				$holding['location_code'] = 'wdover';
-			} 
+			} else {}
 			if ($holding['location_code'] = 'cmc') {
 				$holding['library_code'] = 'WITHDRAW';
 				$holding['location_code'] = 'wdcmc';
-			} 
+			} else {}
 			if ($holding['location_code'] = 'juv') {
 				$holding['library_code'] = 'WITHDRAW';
 				$holding['location_code'] = 'wdjuv';
-			} 
+			} else {}
 			if ($holding['location_code'] = 'overplus') {
 				$holding['library_code'] = 'WITHDRAW';
 				$holding['location_code'] = 'wdoverplus';
-			} 
+			} else {}
 			if ($holding['location_code'] = 'ref') {
 				$holding['library_code'] = 'WITHDRAW';
 				$holding['location_code'] = 'wdref';
-			}*/
+			} else {}
 			$holding->updateAlma();
 		}
 }
