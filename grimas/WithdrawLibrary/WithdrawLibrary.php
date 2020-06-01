95fbb13b359111ee375588791b3625d414037c83
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-01 12:48:29 -0500

    test

---
+++ grimas/WithdrawLibrary/WithdrawLibrary.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once("../grima-lib.php");
+
+class WithdrawLibrary extends GrimaTask {
+	public $holdinglist = array();
+
+	function do_task() {
+		$this->holdings = preg_split('/\r\n|\r|\n/',$this['holding_id']);
+
+		foreach ($this->holdings as $holdingid) {
+			$holding = new Holding();
+			$this['mms_id'] = Holding::getMmsFromHoldingID($holdingid);
+			if ($this['mms_id']) {
+				$holding->loadFromAlma($this['mms_id'],$holdingid);
+				$holding = new Holding();
+				$holding['library_code'] = 'WITHDRAW';
+				if ($holding['location_code'] = 'main') {
+					$holding['location_code'] = 'wdmain';
+				} else {}
+				if ($holding['location_code'] = 'over') {
+					$holding['location_code'] = 'wdover';
+				} else {}
+				if ($holding['location_code'] = 'cmc') {
+					$holding['location_code'] = 'wdcmc';
+				} else {}
+				if ($holding['location_code'] = 'juv') {
+					$holding['location_code'] = 'wdjuv';
+				} else {}
+				if ($holding['location_code'] = 'overplus') {
+					$holding['location_code'] = 'wdoverplus';
+				} else {}
+				if ($holding['location_code'] = 'ref') {
+					$holding['location_code'] = 'wdref';
+				} else {}
+	}
+}
+}
+WithdrawLibrary::RunIt();
