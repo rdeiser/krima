6c760290fb02b0826cef2f6adb436308dcbe1117
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-04 11:44:40 -0500

    Update BatchItemsMMS.php

--- grimas/BatchItemsMMS/BatchItemsMMS.php
+++ grimas/BatchItemsMMS/BatchItemsMMS.php
@@ -19,7 +19,7 @@ class BatchItemsMMS extends GrimaTask {
 			//$item['statistics_note_3'] = $this['whichnote'];
 			$item->addToAlmaHolding($mmsid,$this['holding_id']);
 			//$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with item PID: {$item['item_pid']}");
-			$this->addMessage('success',"Successfully added an Item Record to {$holding['holding_id'} with Barcode: {$item['barcode']}");
+			$this->addMessage('success',"Successfully added an Item Record to {$holding['holding_id']} with Barcode: {$item['barcode']}");
 				/*function print_success() {
     do_redirect('../WithdrawLibrary/WithdrawLibrary.php?holding_id=' . $this['holding_id']);
 }*/
