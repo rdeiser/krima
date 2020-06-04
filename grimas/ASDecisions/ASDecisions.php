eea6bd3a01a1df9e083caa9a1c9d824d68cad18f
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-04 12:44:05 -0500

    Update ASDecisions.php

--- grimas/ASDecisions/ASDecisions.php
+++ grimas/ASDecisions/ASDecisions.php
@@ -27,6 +27,11 @@ class ASDecisions extends GrimaTask {
 			
 			$item = new Item();
 			$item->loadFromAlmaBarcode($barcode);
+			if ($this=>error = true) {
+				$this->addMessage('error',"Alma did not find {$item['barcode']}");
+				continue;
+			} else {
+			
 			if ($item['statistics_note_2'] == '') {
 				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
 			} else {}/*elseif ($item['statistics_note_2'] != '') {}*/
@@ -36,6 +41,7 @@ class ASDecisions extends GrimaTask {
 			$item->updateAlma();
 			$this->addMessage('success',"Successfully updated Item Recored for:{$item['barcode']}");
 			//$this->addMessage('success',"Successfully updated Item Recored for:{$item['barcode']} and set#{$set['set_id']}");
+			}
 		}
 	}
 }
