2f1c29f8824ea36676962a39232e8ef35a744861
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-09 15:20:34 -0500

    Update DeleteItem.php

--- grimas/DeleteItem/DeleteItem.php
+++ grimas/DeleteItem/DeleteItem.php
@@ -7,6 +7,8 @@ class DeleteItem extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBCorX($this['item']);
+		$item['statistics_note_1'] = 'WITHDRAWN';
+		$item->updateAlma();
 		$item->deleteFromAlma();
 		$this->addMessage('success',"Deleted item {$this['item']}");
 	}
