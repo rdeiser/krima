5394afccb76865c3f27ae4593998025e0c4e00a7
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-10 09:13:43 -0500

    New Grima

---
+++ grimas/DeleteTreePO/DeleteTreePO.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once("../grima-lib.php");
+
+class DeleteTree extends GrimaTask {
+
+	function do_task() {
+		$bib = new Bib();
+		$bib->loadFromAlma($this['mms_id']);
+		$bib->getHoldings();
+		$holding = $bib->holdings[0];
+		$holding = new Holding();
+		$withdrawn_phrase = "withdrawn" . date("m-Y")
+		$holding->appendField("852","8","",array('h' => $withdrawn_phrase));
+		$holding->getItemList();
+		if (count($holding->itemList->items) > 1) {
+			addMessage('warn', "More than one item on holding {$holding['holding_id']}");
+			//continue;
+		}
+		$item = $holding->itemList->items[0];
+		$item['statistics_note_1'] = 'WITHDRAWN';
+		$item->updateAlma();
+		
+		$bib->deleteTreeFromAlma();
+		$this->addMessage('success',
+			"deleted bib {$this['mms_id']} and all inventory with Statistics Note 1 = {$item['statistics_note_1']}");
+	}
+}
+
+DeleteTree::RunIt();
