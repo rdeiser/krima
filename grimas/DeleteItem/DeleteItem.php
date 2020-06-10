de7c762d831c72b781d0b619b5694bfba416c0f1
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-10 10:22:28 -0500

    Update DeleteItem.php

--- grimas/DeleteItem/DeleteItem.php
+++ grimas/DeleteItem/DeleteItem.php
@@ -7,10 +7,13 @@ class DeleteItem extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBCorX($this['item']);
+		if ($item['statistics_note_3'] == 'To be WITHDRAWN'; {
 		$item['statistics_note_1'] = 'WITHDRAWN';
 		$item->updateAlma();
 		$item->deleteFromAlma();
 		$this->addMessage('success',"Deleted item {$this['item']}");
+		} else {
+			$this->addMessage('error',"Item Record for {$item['barcode']}") has Statistics Note 3 of :{$item['statistics_note_3']}");
 	}
 }
 
