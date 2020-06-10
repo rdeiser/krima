121c37a32592eb452334c478aad68d31c8941aba
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-10 08:44:08 -0500

    Update DeleteTree.php

--- grimas/DeleteTree/DeleteTree.php
+++ grimas/DeleteTree/DeleteTree.php
@@ -7,9 +7,20 @@ class DeleteTree extends GrimaTask {
 	function do_task() {
 		$bib = new Bib();
 		$bib->loadFromAlma($this['mms_id']);
+		$bib->getHoldings();
+		$holding = $bib->holdings[0];
+		$holding->getItemList();
+		if (count($holding->itemList->items) > 1) {
+			addMessage('warn', "More than one item on holding {$holding['holding_id']}");
+			continue;
+		}
+		$item = $holding->itemList->items[0];
+		$item['statistics_note_1'] = 'WITHDRAWN';
+		$item->updateAlma();
+		
 		$bib->deleteTreeFromAlma();
 		$this->addMessage('success',
-			"deleted bib {$this['mms_id']} and all inventory");
+			"deleted bib {$this['mms_id']} and all inventory with Statistics Note 1 = {$item['statistics_note_1']}");
 	}
 }
 
