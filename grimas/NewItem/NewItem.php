ea6a2aa5968a8b33dc5a62436d9277a20b6ac918
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-12 11:47:37 -0500

    Update_testing

--- grimas/NewItem/NewItem.php
+++ grimas/NewItem/NewItem.php
@@ -3,12 +3,15 @@
 require_once("../grima-lib.php");
 
 class NewItem extends GrimaTask {
+	public $biblist = array();
 
 	function do_task() {
+		$this->bibs = preg_split('/\r\n|\r|\n/',$this['mms']);
+		foreach ($this->bibs as $mmsid) {
 			$holding = new Holding();
-			$holding->loadFromAlma($this['mmsid'],$this['holding_id']);
+			$holding->loadFromAlma($mmsid,$this['holding_id']);
 			/*$this['mms_id'] = Holding::getMmsFromHoldingID($holdingid);*/
-			if ($this['mmsid']) {
+			//if ($this['mmsid']) {
 				//$holding->loadFromAlma($this['mms_id'],$holdingid);
 				$item = new Item();
 				$item['copy_id'] = $this['copyid'];
@@ -21,7 +24,8 @@ class NewItem extends GrimaTask {
 				$this->addMessage('success',"Successfully added an Item Record to {$this['holding_id']} with Barcode: {$item['barcode']}");
 			} else {
 				$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$this['holding_id']}");
-			}
+			//}
+		}
 	}
 }
 NewItem::RunIt();
