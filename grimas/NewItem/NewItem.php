7f5a046baa2343ef6e55d4cad40bba5212db6137
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-12 13:07:03 -0500

    Update NewItem.php

--- grimas/NewItem/NewItem.php
+++ grimas/NewItem/NewItem.php
@@ -15,7 +15,6 @@ class NewItem extends GrimaTask {
 				//$holding->loadFromAlma($this['mms_id'],$holdingid);
 				$item = new Item();
 				//$item['copy_id'] = $this['copyid'];
-				//$item['copy_id'] = $this['copyid'];
 				$item['item_policy'] = $this['itempolicy'];
 				$item['pieces'] = $this['pieces'];
 				$item['public_note'] = $this ['pubnote'];
