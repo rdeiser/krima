7a5f34abcc9d042f30e8f6346055003d4d16bb28
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-15 09:39:22 -0500

    Update NewItem.php

--- grimas/NewItem/NewItem.php
+++ grimas/NewItem/NewItem.php
@@ -3,15 +3,15 @@
 require_once("../grima-lib.php");
 
 class NewItem extends GrimaTask {
+	public $holdinglist = array();
 
 	function do_task() {
 		$this->holdings = preg_split('/\r\n|\r|\n/',$this['holding_id']);
+
 		foreach ($this->holdings as $holdingid) {
 			$holding = new Holding();
-			$holding->loadFromAlma($holdingid,$this['mms_id']);
 			$this['mms_id'] = Holding::getMmsFromHoldingID($holdingid);
-			if ($this['mmsid']) {
-				$holding->loadFromAlma($this['mms_id'],$holdingid);
+			if ($this['mms_id']) {
 				$holding->loadFromAlma($this['mms_id'],$holdingid);
 				$item = new Item();
 				//$item['copy_id'] = $this['copyid'];
