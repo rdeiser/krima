aeb4e44fcf16f767a9ac632c7903e6443bbd1264
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-15 09:41:20 -0500

    Update NewItem.php

--- grimas/NewItem/NewItem.php
+++ grimas/NewItem/NewItem.php
@@ -22,7 +22,7 @@ class NewItem extends GrimaTask {
 				$item['statistics_note_2'] = $this['statnote2'];
 				$item['statistics_note_3'] = $this['statnote3'];
 				$item['barcode'] = $this['barcode'];
-				$item->addToAlmaHolding($mmsid,$this['holding_id']);
+				$item->addToAlmaHolding($this['mms_id'],$holdingid);
 				$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
 			} else {
 				$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
