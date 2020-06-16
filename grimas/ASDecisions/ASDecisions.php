6cc94a6fabc495d9a2e5844b2e75dbf310ddb648
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-16 11:07:12 -0500

    Update ASDecisions.php

--- grimas/ASDecisions/ASDecisions.php
+++ grimas/ASDecisions/ASDecisions.php
@@ -30,12 +30,10 @@ class ASDecisions extends GrimaTask {
 			if ($item['statistics_note_2'] == '') {
 				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
 			} else {}/*elseif ($item['statistics_note_2'] != '') {}*/
-			if ($item['statistics_note_3'] == 'AHD HALE retrun') {
-				$item['statistics_note_3'] = 'AHD HALE return';
-			} else {}
-			if ($item['statistics_note_3'] == '') {
+			$item['statistics_note_3'] = $this['whichnote'];
+			/*if ($item['statistics_note_3'] == '') {
 				$item['statistics_note_3'] = $this['whichnote'];
-			} else {}
+			} else {}*/
 			$item->updateAlma();
 			$this->addMessage('success',"Successfully updated Item Recored for: {$item['barcode']}");
 			//$this->addMessage('success',"Successfully updated Item Recored for:{$item['barcode']} and set#{$set['set_id']}");
