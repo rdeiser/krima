ab69bce5e993f368b834b9efe5cf7ddd1edcf6ae
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-17 12:57:06 -0500

    Update side-by-side.php

--- grimas/DisplayItem/splats/side-by-side.php
+++ grimas/DisplayItem/splats/side-by-side.php
@@ -1,4 +1,33 @@
-<?php if ($item['statistics_note_3']=='To be WITHDRAWN') {
+<?php 
+if ($item['statistics_note_3'] == 'HALE return') {
+	$pattern = '/(HALE return)/';
+	$replace = 'HALE return';
+} 
+if ($item['statistics_note_3'] == 'ANNEX ingest') {
+	$pattern = '/(ANNEX ingest)/';
+	$replace = 'ANNEX ingest';
+}
+if ($item['statistics_note_3'] == 'To be WITHDRAWN') {
+	$pattern = '/(To be WITHDRAWN)/';
+	$replace = 'To be WITHDRAWN';
+}
+if ($item['statistics_note_3'] == 'AHD HALE return') {
+	$pattern = '/(AHD HALE return)/';
+	$replace = 'HALE return';
+} 
+if ($item['statistics_note_3'] == 'AHD ANNEX ingest') {
+	$pattern = '/(AHD ANNEX ingest)/';
+	$replace = 'ANNEX ingest';
+}
+if ($item['statistics_note_3'] == 'AHD To be WITHDRAWN') {
+	$pattern = '/(AHD To be WITHDRAWN)/';
+	$replace = 'To be WITHDRAWN';
+}
+if ($item['statistics_note_3'] == '') {
+	$pattern = '//';
+	$replace = 'Send to Problem Shelf';
+}
+if ($item['statistics_note_3']=='To be WITHDRAWN') {
 			$style = 'style="background-color: #cd5555;"';
 			$text = '';
 }
@@ -32,7 +61,7 @@
           <div class="col">
             <div class="card" <?=$style?>>
               <div class="card-header">
-                <h2 class="card-title"><?=$e($item['statistics_note_3'])?><?=$text?></h2>
+                <h2 class="card-title"><?=preg_replace($pattern, $replace, $item['statistics_note_3'])?><?=$text?></h2>
               </div>
               <div class="card-body">
 <?= $t('rightBody', array('item' => $item)) ?>
