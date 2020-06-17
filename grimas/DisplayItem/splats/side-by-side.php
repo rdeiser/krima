231a51a46a1b24d4697828520ad3dd9c9b2862d8
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-17 13:04:45 -0500

    Update side-by-side.php

--- grimas/DisplayItem/splats/side-by-side.php
+++ grimas/DisplayItem/splats/side-by-side.php
@@ -62,7 +62,7 @@ if ($item['statistics_note_3']=='To be WITHDRAWN') {
           <div class="col">
             <div class="card" <?=$style?>>
               <div class="card-header">
-                <h2 class="card-title"><?=preg_replace($pattern, $replace, $item['statistics_note_3'])?><?=$text?></h2>
+                <h2 class="card-title"><?=preg_replace($pattern, $replace, $item['statistics_note_3'])?></h2>
               </div>
               <div class="card-body">
 <?= $t('rightBody', array('item' => $item)) ?>
