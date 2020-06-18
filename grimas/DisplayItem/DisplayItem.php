d587476a16f9df637437f3b073668b82c74703fd
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-18 13:42:19 -0500

    Update DisplayItem.php

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -11,10 +11,8 @@ class DisplayItem extends GrimaTask {
 		//$item['inventory_number'] = ('Inventory Date:').date("m/d/Y");
 		$item['inventory_date'] = date("Y-m-d");
 		//$item['inventory_date'] = date("Y-m-d g:i:s A");
-		if (isset($item['statistics_note_3'])) {
-			if ($item['in_temp_location'] == 'true') {
-				$item['in_temp_location'] = 'false';
-			}
+		if ($item['in_temp_location'] == 'true') {
+			$item['in_temp_location'] = 'false';
 		}
 		$item->updateAlma();
 		}
