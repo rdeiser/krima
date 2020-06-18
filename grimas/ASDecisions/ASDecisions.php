a24d9c77b94491b69c40a6e85e01678303b2cb93
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-18 11:00:58 -0500

    Update ASDecisions.php

--- grimas/ASDecisions/ASDecisions.php
+++ grimas/ASDecisions/ASDecisions.php
@@ -27,6 +27,15 @@ class ASDecisions extends GrimaTask {
 			
 			$item = new Item();
 			$item->loadFromAlmaBarcode($barcode);
+			if ($this['whichnote']=='AHD HALE return'){
+				$item['statsitcs_note_3'] = $this['whichnote']
+			}
+			if ($this['whichnote']=='AHD ANNEX ingest'){
+				$item['statsitcs_note_3'] = $this['whichnote']
+			}
+			if ($this['whichnote']=='AHD To be WITHDRAWN'){
+				$item['statsitcs_note_3'] = $this['whichnote']
+			}
 			if ($item['statistics_note_2'] == '') {
 				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
 			} else {}/*elseif ($item['statistics_note_2'] != '') {}*/
