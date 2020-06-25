d88be5c52b383c34d9a564396c94ae7bb9ca653a
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-25 09:33:06 -0500

    New Grima
    Removes Barcode Hopefully.

---
+++ grimas/RemoveBarcode/RemoveBarcode.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("../grima-lib.php");
+
+class RemoveBarcode extends GrimaTask {
+	
+	function do_task() {
+		$item->item = new Item();
+		$item->loadFromAlmaBarcode($this['barcode']);
+		
+		$item->deleteBarcode();
+		$item->updateAlma();
+
+	}
+
+	function print_success() {
+		GrimaTask::call('ViewXmlItem', array('item_pid' => {$item['item_pid']}));
+	}
+		
+}
+
+RemoveBarcode::RunIt();
