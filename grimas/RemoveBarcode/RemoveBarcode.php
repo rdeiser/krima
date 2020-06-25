81062cc8ae866909e8efbdaca400603b31a28856
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-06-25 10:27:10 -0500

    Update RemoveBarcode.php

--- grimas/RemoveBarcode/RemoveBarcode.php
+++ grimas/RemoveBarcode/RemoveBarcode.php
@@ -5,7 +5,7 @@ require_once("../grima-lib.php");
 class RemoveBarcode extends GrimaTask {
 	
 	function do_task() {
-		foreach($this['barcode_remove'] as $barcode) {
+		foreach($this->barcode_remove as $barcode) {
 			$item = new Item();
 			$item->loadFromAlmaBarcode($barcode);
 			//$item['statistics_note_2'] = 'FIRE 2018 OZONE';
