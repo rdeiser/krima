7e06cc62fdf5fc334efa35c5cfb463ab417dd483
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-07-01 13:38:40 -0500

    Update NewItem.php

--- grimas/NewItem/NewItem.php
+++ grimas/NewItem/NewItem.php
@@ -13,18 +13,32 @@ class NewItem extends GrimaTask {
 			$holding->loadFromAlma($this['mms'],$holdingid);
 			if ($this['mms']) {
 				$holding->loadFromAlma($this['mms'],$holdingid);
-				$item = new Item();
-				$item['copy_id'] = $this['copyid'];
-				$item['item_policy'] = $this['itempolicy'];
-				$item['pieces'] = $this['pieces'];
-				$item['public_note'] = $this ['pubnote'];
-				$item['fulfillment_note'] = $this['fulnote'];
-				$item['statistics_note_1'] = $this['statnote1'];
-				$item['statistics_note_2'] = $this['statnote2'];
-				$item['statistics_note_3'] = $this['statnote3'];
-				$item['barcode'] = $this['barcode'];
-				$item->addToAlmaHolding($this['mms'],$holdingid);
-				$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
+				if(isset($this['barcode']) && null !== $this['barcode']) {
+					$item = new Item();
+					$item['copy_id'] = $this['copyid'];
+					$item['item_policy'] = $this['itempolicy'];
+					$item['pieces'] = $this['pieces'];
+					$item['public_note'] = $this ['pubnote'];
+					$item['fulfillment_note'] = $this['fulnote'];
+					$item['statistics_note_1'] = $this['statnote1'];
+					$item['statistics_note_2'] = $this['statnote2'];
+					$item['statistics_note_3'] = $this['statnote3'];
+					$item['barcode'] = $this['barcode'];
+					$item->addToAlmaHolding($this['mms'],$holdingid);
+					$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
+				} else {
+					$item = new Itemnbc();
+					$item['copy_id'] = $this['copyid'];
+					$item['item_policy'] = $this['itempolicy'];
+					$item['pieces'] = $this['pieces'];
+					$item['public_note'] = $this ['pubnote'];
+					$item['fulfillment_note'] = $this['fulnote'];
+					$item['statistics_note_1'] = $this['statnote1'];
+					$item['statistics_note_2'] = $this['statnote2'];
+					$item['statistics_note_3'] = $this['statnote3'];
+					$item->addToAlmaHolding($this['mms'],$holdingid);
+					$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
+				}
 			} else {
 				$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
 			}
