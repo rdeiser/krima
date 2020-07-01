f13416235f6295ade1ffc8b808939a3cba11809b
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-07-01 11:40:51 -0500

    Update NewItem.php

--- grimas/NewItem/NewItem.php
+++ grimas/NewItem/NewItem.php
@@ -14,18 +14,8 @@ class NewItem extends GrimaTask {
 			if ($this['mms']) {
 				$holding->loadFromAlma($this['mms'],$holdingid);
 				$item = new Item();
-				//$item['copy_id'] = $this['copyid'];
-				$item['item_policy'] = $this['itempolicy'];
-				$item['pieces'] = $this['pieces'];
-				$item['public_note'] = $this ['pubnote'];
-				$item['fulfillment_note'] = $this['fulnote'];
-				$item['statistics_note_1'] = $this['statnote1'];
-				$item['statistics_note_2'] = $this['statnote2'];
-				$item['statistics_note_3'] = $this['statnote3'];
-				//$item['barcode'] = $this['barcode'];
-				$item['barcode'] != $item['barcode'];
-				$item->addToAlmaHolding($this['mms'],$holdingid);
-				$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode:");
+				$item->addToAlmaHoldingNBC($this['mms'],$holdingid);
+				$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
 			} else {
 				$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
 			}
