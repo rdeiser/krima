03c2e5083d152b70bc1899067e94a0d4596d3a31
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-07-01 14:03:25 -0500

    Update NewItem.php

--- grimas/NewItem/NewItem.php
+++ grimas/NewItem/NewItem.php
@@ -22,7 +22,6 @@ class NewItem extends GrimaTask {
 					$item['statistics_note_1'] = $this['statnote1'];
 					$item['statistics_note_2'] = $this['statnote2'];
 					$item['statistics_note_3'] = $this['statnote3'];
-					$item['barcode'] = $this['barcode'];
 					$item->addToAlmaHolding($this['mms'],$holdingid);
 					$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
 				/*if(isset($this['barcode']) && null !== $this['barcode']) {
