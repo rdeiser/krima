013bb77823791a8fd0670d8f0b57cc7cd931a157
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-07-01 14:18:15 -0500

    Update NewItem.php

--- grimas/NewItem/NewItem.php
+++ grimas/NewItem/NewItem.php
@@ -14,6 +14,18 @@ class NewItem extends GrimaTask {
 			if ($this['mms']) {
 				$holding->loadFromAlma($this['mms'],$holdingid);
 				if(empty($this['barcode'])) {
+					$item = new Itemnbc();
+					$item['copy_id'] = $this['copyid'];
+					$item['item_policy'] = $this['itempolicy'];
+					$item['pieces'] = $this['pieces'];
+					$item['public_note'] = $this ['pubnote'];
+					$item['fulfillment_note'] = $this['fulnote'];
+					$item['statistics_note_1'] = $this['statnote1'];
+					$item['statistics_note_2'] = $this['statnote2'];
+					$item['statistics_note_3'] = $this['statnote3'];
+					$item->addToAlmaHolding($this['mms'],$holdingid);
+					$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with PID: {$item['item_pid']}");
+				} else {
 					$item = new Item();
 					$item['copy_id'] = $this['copyid'];
 					$item['item_policy'] = $this['itempolicy'];
@@ -26,18 +38,6 @@ class NewItem extends GrimaTask {
 					$item['barcode'] = $this['barcode'];
 					$item->addToAlmaHolding($this['mms'],$holdingid);
 					$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
-				} else {
-					$item = new Itemnbc();
-					$item['copy_id'] = $this['copyid'];
-					$item['item_policy'] = $this['itempolicy'];
-					$item['pieces'] = $this['pieces'];
-					$item['public_note'] = $this ['pubnote'];
-					$item['fulfillment_note'] = $this['fulnote'];
-					$item['statistics_note_1'] = $this['statnote1'];
-					$item['statistics_note_2'] = $this['statnote2'];
-					$item['statistics_note_3'] = $this['statnote3'];
-					$item->addToAlmaHolding($this['mms'],$holdingid);
-					$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with PID: {$item['item_pid']}");
 				}
 			} else {
 				$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
