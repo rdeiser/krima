1402b87a6711c31ba1b48beca8e111699d55a425
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-07-02 09:55:17 -0500

    Update ASDecisions.php

--- grimas/ASDecisions/ASDecisions.php
+++ grimas/ASDecisions/ASDecisions.php
@@ -28,7 +28,7 @@ class ASDecisions extends GrimaTask {
 			$item = new Item();
 			$item->loadFromAlmaBarcode($barcode);
 			$item['barcode']=='';
-			if ($this['whichnote']=='AHD HALE return'){
+			/*if ($this['whichnote']=='AHD HALE return'){
 				$item['statsitcs_note_3'] = $this['whichnote'];
 			}
 			if ($this['whichnote']=='AHD ANNEX ingest'){
@@ -41,9 +41,9 @@ class ASDecisions extends GrimaTask {
 				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
 			} else {}/*elseif ($item['statistics_note_2'] != '') {}*/
 			//$item['statistics_note_3'] = $this['whichnote'];
-			if ($item['statistics_note_3'] == '') {
+			/*if ($item['statistics_note_3'] == '') {
 				$item['statistics_note_3'] = $this['whichnote'];
-			} else {}
+			} else {}*/
 			$item->updateAlma();
 			$this->addMessage('success',"Successfully updated Item Recored for: {$item['barcode']}");
 			//$this->addMessage('success',"Successfully updated Item Recored for:{$item['barcode']} and set#{$set['set_id']}");
