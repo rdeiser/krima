3a212e8d57b38368c82cc520f607bb045e0ade4d
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-07-02 09:01:40 -0500

    Update item.php

--- grimas/Hierarchy/splats/item.php
+++ grimas/Hierarchy/splats/item.php
@@ -1,7 +1,7 @@
 <div class='card ml-4 item'>
   <div class='card-header'>
     <h3 class='card-title'>Item #<?=$e($item['item_pid'])?>
-      <a class='d-print-none viewlink' href="../PrintItem/PrintItem.php?item_pid=<?=$e($item['item_pid'])?>">(view)</a>
+      <!--<a class='d-print-none viewlink' href="../PrintItem/PrintItem.php?item_pid=<?=$e($item['item_pid'])?>">(view)</a>-->
     </h3>
   </div>
   <div class='card-body'>
