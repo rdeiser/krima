5ef4fa0e21f0393c80ccbe73a4a4f00ddcb5503d
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-07-02 08:11:00 -0500

    Update item.php

--- grimas/Hierarchy/splats/item.php
+++ grimas/Hierarchy/splats/item.php
@@ -10,8 +10,8 @@
       <dd class="col-md-10 barcode"><?=$e($item['barcode'])?>
       <dt class="col-md-2 text-right description">Description:</dt>
       <dd class="col-md-10 description"><?=$e($item['description'])?>
-	  <dt class="col-md-2 text-left description">Statistics Note 3:</dt>
-      <dd class="col-md-10 description"><?=$e($item['statistics_note_3'])?>
+	  <dt class="col-md-2 text-left ">Statistics Note 3:</dt>
+      <dd class="col-md-10"><?=$e($item['statistics_note_3'])?>
     </dl>
   </div>
 </div>
