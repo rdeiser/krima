54b8cb8ba015f75afeaf9485931d83d2294a73d7
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-07-17 12:16:11 -0500

    Update ASDecisions.php

--- grimas/ASDecisions/ASDecisions.php
+++ grimas/ASDecisions/ASDecisions.php
@@ -18,7 +18,15 @@ class ASDecisions extends GrimaTask {
 			$item->loadFromAlmaBarcode($barcode);
 			$item->addInventoryDate("1976-01-01");
 			//unset($item['barcode']);
-			
+			if ($this['whichnote']=='AHD HALE return'){
+				$item['statistics_note_3'] = $this['whichnote'];
+			}
+			if ($this['whichnote']=='AHD ANNEX ingest'){
+				$item['statistics_note_3'] = $this['whichnote'];
+			}
+			if ($this['whichnote']=='AHD To be WITHDRAWN'){
+				$item['statistics_note_3'] = $this['whichnote'];
+			}
 			if ($item['statistics_note_2'] == '') {
 				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
 			} else {}/*elseif ($item['statistics_note_2'] != '') {}*/
@@ -26,16 +34,6 @@ class ASDecisions extends GrimaTask {
 			if ($item['statistics_note_3'] == '') {
 				$item['statistics_note_3'] = $this['whichnote'];
 			} else {}
-			
-			if ($this['whichnote']=='AHD HALE return'){
-				$item['statsitcs_note_3'] = $this['whichnote'];
-			}
-			if ($this['whichnote']=='AHD ANNEX ingest'){
-				$item['statsitcs_note_3'] = $this['whichnote'];
-			}
-			if ($this['whichnote']=='AHD To be WITHDRAWN'){
-				$item['statsitcs_note_3'] = $this['whichnote'];
-			}
 			$item->updateAlma();
 			$this->addMessage('success',"Successfully updated Item Recored for: {$item['barcode']}");
 			//$this->addMessage('success',"Successfully updated Item Recored for:{$item['barcode']} and set#{$set['set_id']}");
