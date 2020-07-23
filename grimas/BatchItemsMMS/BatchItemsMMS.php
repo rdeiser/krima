553bba3bb31b108ee48785565d806ff688f28f95
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-07-23 10:43:24 -0500

    Update BatchItemsMMS.php

--- grimas/BatchItemsMMS/BatchItemsMMS.php
+++ grimas/BatchItemsMMS/BatchItemsMMS.php
@@ -11,15 +11,98 @@ class BatchItemsMMS extends GrimaTask {
 		foreach ($this->bibs as $mmsid) {
 			$holding = new Holding();
 			$holding->loadFromAlma($mmsid,$this['holding_id']);
-			$item = new Item();
-			//$item['fulfillment_note'] = $this['fulnote'];
-			//$item['inventory_date'] = '1976-01-01';
-			//$item['inventory_date'] = date("Y-m-d");
-			$item['statistics_note_2'] = 'FIRE 2018 OZONE';
-			//$item['statistics_note_3'] = $this['whichnote'];
-			$item->addToAlmaHolding($mmsid,$this['holding_id']);
-			//$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with item PID: {$item['item_pid']}");
-			$this->addMessage('success',"Successfully added an Item Record to {$holding['holding_id']} with Barcode: {$item['barcode']}");
+			if ($this['whichnote'] == 'AHD To be WITHDRAWN'){
+					if ($holding['location_code'] == 'main') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdmain';
+					} 
+					if ($holding['location_code'] == 'over') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdover';
+					} 
+					if ($holding['location_code'] == 'cmc') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdcmc';
+					} 
+					if ($holding['location_code'] == 'juv') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdjuv';
+					} 
+					if ($holding['location_code'] == 'overplus') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdoverplus';
+					} 
+					if ($holding['location_code'] == 'ref') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdref';
+					}
+					$holding->updateAlma();
+				}
+				if ($this['whichnote'] == 'To be WITHDRAWN'){
+					if ($holding['location_code'] == 'main') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdmain';
+					} 
+					if ($holding['location_code'] == 'over') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdover';
+					} 
+					if ($holding['location_code'] == 'cmc') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdcmc';
+					} 
+					if ($holding['location_code'] == 'juv') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdjuv';
+					} 
+					if ($holding['location_code'] == 'overplus') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdoverplus';
+					} 
+					if ($holding['location_code'] == 'ref') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdref';
+					}
+					$holding->updateAlma();
+				}
+				if ($this['whichnote'] == 'PHYSICAL CONDITION REVIEW For Possible Withdraw'){
+					if ($holding['location_code'] == 'main') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdmain';
+					} 
+					if ($holding['location_code'] == 'over') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdover';
+					} 
+					if ($holding['location_code'] == 'cmc') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdcmc';
+					} 
+					if ($holding['location_code'] == 'juv') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdjuv';
+					} 
+					if ($holding['location_code'] == 'overplus') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdoverplus';
+					} 
+					if ($holding['location_code'] == 'ref') {
+						$holding['library_code'] = 'WITHDRAW';
+						$holding['location_code'] = 'wdref';
+					}
+					$holding->updateAlma();
+				}
+			$item = new Itemnbc();
+				//$item['fulfillment_note'] = $this['fulnote'];
+				$item['item_policy'] = 'book/ser';
+				$item['pieces'] = '1';
+				$item['inventory_date'] = '1976-01-01';
+				$item['receiving_operator'] = 'Grima';
+				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
+				$item['statistics_note_3'] = $this['whichnote'];
+				$item->addToAlmaHolding($mmsid,$this['holding_id']);
+				
+			$this->addMessage('success',"Successfully added an Item Record to {$holding['holding_id']} with Barcode: {$item['item_pid']}");
 				/*function print_success() {
     do_redirect('../WithdrawLibrary/WithdrawLibrary.php?holding_id=' . $this['holding_id']);
 }*/
