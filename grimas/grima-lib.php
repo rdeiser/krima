42055af3d5c286743e6e35b9ffef1339024044e9
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-03 10:41:54 -0500

    Update grima-lib.php

--- grimas/grima-lib.php
+++ grimas/grima-lib.php
@@ -1073,14 +1073,19 @@ class Grima {
 	}*/
 
 	function ScanIn($mms_id,$holding_id,$item_pid) {
+		global $grima;
+		$this->mms_id = $mms_id;
+		$this->holding_id = $holding_id;
+		$this->item_pid = $item_pid;
+		scan_in($mms_id,$holding_id,$item_pid);
 		/*$library = '';
 		$circ_desk = '';
 		
 		$ret = $this->post('/almaws/v1/bibs/{mms_id}/holdings/{holding_id}/items',
 			array('mms_id' => $mms_id, 'holding_id' => $holding_id, 'item_pid' => $item_pid)
-			);*/
+			);
 		$this->checkForErrorMessage($ret);
-		return $ret;
+		return $ret;*/
 	}
 //}}}
 
