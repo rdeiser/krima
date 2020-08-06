970efe59f5753e9b567e8591697b2541d349ed98
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-06 15:31:01 -0500

    Update ASDecisions.php

--- grimas/ASDecisions/ASDecisions.php
+++ grimas/ASDecisions/ASDecisions.php
@@ -36,7 +36,8 @@ class ASDecisions extends GrimaTask {
 				//$item['statistics_note_2'] = $this['whichnote2'];
 				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
 			} else {}/*elseif ($item['statistics_note_2'] != '') {}*/
-			$item['statistics_note_3'] == $this['whichnote'];
+			
+			$item['statistics_note_3'] = $this['whichnote'];
 			/*if ($item['statistics_note_3'] == '') {
 				$item['statistics_note_3'] = $this['whichnote'];
 			} else {}*/
