7fc75d6aaaf6666aa712f6a78bb9676f1d20b753
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-10 13:50:13 -0500

    Update ScanInReturn.php

--- grimas/ScanInReturn/ScanInReturn.php
+++ grimas/ScanInReturn/ScanInReturn.php
@@ -5,10 +5,10 @@ class ScanInReturn extends GrimaTask {
 
 	function do_task() {
 		$item = new Item();
-		$item->fulfillmentscan($this['barcode']);
-		//$item->loadFromAlmaBarcode($this['barcode']);
+		//$item->fulfillmentscan($this['barcode']);
+		$item->loadFromAlmaBarcode($this['barcode']);
 		
-		//$this['barcode'] = scan_in($item['mms_id'],$item['holding_id'],$item['item_pid']);
+		$this['barcode'] = scan_in($item['mms_id'],$item['holding_id'],$item['item_pid']);
 		//$item->updateAlma();
 		//scan_in($this->item['mms_id'],$this->item['holding_id'],$this->item['pid']);
 			$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} {$item['mms_id']} {$item['holding_id']} {$item['item_pid']} Process: {$item['process_type']}");
