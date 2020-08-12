afdfdad2a21dd375a16ac25c7fca6414857dfb04
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-12 15:24:29 -0500

    Update ScanInReturn.php

--- grimas/ScanInReturn/ScanInReturn.php
+++ grimas/ScanInReturn/ScanInReturn.php
@@ -6,13 +6,10 @@ class ScanInReturn extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['barcode']);
-		
-		//postItem2($item['mms_id'],$item['holding_id'],$item['item_pid']);
-		//scan_in($item['mms_id'],$item['holding_id'],$item['item_pid']);
 		$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid']);
 		
 		//$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} {$item['mms_id']} {$item['holding_id']} {$item['item_pid']} Process: {$item['process_type']}");
-		$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} {$item['mms_id']} {$item['holding_id']} {$item['item_pid']} Process: {$item['process_type']} Additional Information: {$item['additional_info']}");
+		$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} {$item['mms_id']} {$item['holding_id']} {$item['item_pid']} Process: {$item['process_type']} Additional Information: {$response}");
 	}
 }
 
