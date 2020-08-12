3714d6527d8094d49632057ad03072490626f343
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-12 10:29:51 -0500

    Update grima-lib.php

--- grimas/grima-lib.php
+++ grimas/grima-lib.php
@@ -300,6 +300,25 @@ class Grima {
  * @param array $URLparams - URL parameters
  * @param array $QSparams - query string parameters
  */
+function scan_in($mms_id,$holding_id,$item_pid) {
+	//require_once("grima-config.php");
+	require_once("grima-lib.php");
+	//$url = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/{mms_id}/holdings/{holding_id}/items/{item_pid}';
+	$url = $server . 'almaws/v1/bibs/{mms_id}/holdings/{holding_id}/items/{item_pid}';
+	$ch = curl_init();
+	$templateParamNames = array('{mms_id}','{holding_id}','{item_pid}');
+	$templateParamValues = array(urlencode($mms_id),urlencode($holding_id),urlencode($item_pid));
+	$url = str_replace($templateParamNames, $templateParamValues, $url);
+	$queryParams ='?'. urlencode('op') . '=' . urlencode('scan') . '&' . urlencode('external_id') . '=' . urlencode('false') . '&' . urlencode('library') . '=' . urlencode('MAIN') . '&' . urlencode('circ_desk') . '=' . urlencode('DEFAULT_CIRC_DESK') . '&' . urlencode('work_order_type') . '=' . urlencode('72hr') . '&' . urlencode('status') . '=' . urlencode('72hr_Quarantine') . '&' . urlencode('done') . '=' . urlencode('false') . '&' . urlencode('auto_print_slip') . '=' . urlencode('false') . '&' . urlencode('place_on_hold_shelf') . '=' . urlencode('false') . '&' . urlencode('confirm') . '=' . urlencode('false') . '&' . urlencode('register_in_house_use') . '=' . urlencode('false') . '&apikey=' . urlencode($apikey);
+	curl_setopt($ch, CURLOPT_URL, $url . $queryParams);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_HEADER, FALSE);
+	//curl_setopt($ch, CURLOPT_HEADER, TRUE);
+	//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization:apikey***'));
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+	$response = curl_exec($ch);
+	curl_close($ch);
+	return $response;
 	/*function postIn($url,$URLparams,$QSparams) {
 		foreach ($URLparams as $k => $v) {
 			$url = str_replace('{'.$k.'}',urlencode($v),$url);
