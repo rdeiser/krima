a12dd9b2de901364a8b678d9dcde8860e37e1123
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-17 15:41:16 -0500

    Update ScanInReturn.php

--- grimas/ScanInReturn/ScanInReturn.php
+++ grimas/ScanInReturn/ScanInReturn.php
@@ -7,9 +7,9 @@ class ScanInReturn extends GrimaTask {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['barcode']);
 		$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid']);
-		//$item->addInfo();
+		$this->addMessage('success',$item->addInfo());
 		//echo $item['additional_info'];
-		function print_success() {
+		//function print_success() {
 			GrimaTask::call('ViewXmlItem', array('item_pid' => $this['item_pid']));
 		}
 		//$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} with a Process Type of: {$item['process_type']}");
