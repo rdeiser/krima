bc3a26f94639ce890537c1ba0850e986fe5c9f13
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-17 15:44:46 -0500

    Update grima-lib.php

--- grimas/grima-lib.php
+++ grimas/grima-lib.php
@@ -3267,8 +3267,8 @@ xmlns:xsd="http://www.w3.org/2001/XMLSchema">
 		$xpath = new DomXpath($this->xml);
 		$addinfos = $xpath->query("//additional_info");
 		
-		if($addinfos->lenght > 0) {
-			$note = $addinfos->item(0);
+		foreach ($addinfos as $addinfo) {
+			$note = $addinfso->item(0);
 			echo "{$node->nodeName} - {$node->nodeValue}";
 		}
 		//echo $addinfos;
