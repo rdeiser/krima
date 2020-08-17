dd2a738f4631ad8ac08d6982691df9e880820b0f
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-17 12:46:52 -0500

    Update ScanInReturn.php

--- grimas/ScanInReturn/ScanInReturn.php
+++ grimas/ScanInReturn/ScanInReturn.php
@@ -9,7 +9,7 @@ class ScanInReturn extends GrimaTask {
 		$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid']);
 		//sleep(2);
 		//$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} {$item['mms_id']} {$item['holding_id']} {$item['item_pid']} Process: {$item['process_type']}");
-		$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} {$item['mms_id']} {$item['holding_id']} {$item['item_pid']} Process: {$item['process_type']} Additional Information: {$item['additional_info']} {$item['copy_id']");
+		$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} {$item['mms_id']} {$item['holding_id']} {$item['item_pid']} Process: {$item['process_type']} Additional Information: {$item['additional_info']} {$item['copy_id']}");
 	}
 }
 
