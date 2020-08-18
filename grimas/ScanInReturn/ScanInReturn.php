3e6cce571f6553c0bd8d5defb17cd0f80a6477b4
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-18 15:32:58 -0500

    Update ScanInReturn.php

--- grimas/ScanInReturn/ScanInReturn.php
+++ grimas/ScanInReturn/ScanInReturn.php
@@ -6,26 +6,14 @@ class ScanInReturn extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['barcode']);
-		//$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid']);
 
 		$this->item = new Item();
 		$this->item->loadFromAlmaX($item['item_pid']);
 		$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid']);
-		//$inof = $item['additional_info'];
 
 		$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} Additional Information: {$this->item['additional_info']}");
 	}
-	
-	/*function print_success() {
-		XMLtoWeb($this->item->xml);
-	}*/
-		//$xpath= new DomXpath($this->xml)
-		//var_dump($xpath->evaluate("//additional_info"));
-		
-		//$this->addMessage('success',$item->addInfo());
-		//echo $item['additional_info'];
-		//$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} with a Process Type of: {$item['process_type']}");
-		//$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} {$item['mms_id']} {$item['holding_id']} {$item['item_pid']} Process: {$item['process_type']} Additional Information: {$item['additional_info']}");
+
 
 }
 
