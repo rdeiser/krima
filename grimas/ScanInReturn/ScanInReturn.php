4c9ff42930cf1fef312ed97c1823349e6f3f21c9
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-18 14:55:56 -0500

    Update ScanInReturn.php

--- grimas/ScanInReturn/ScanInReturn.php
+++ grimas/ScanInReturn/ScanInReturn.php
@@ -6,15 +6,21 @@ class ScanInReturn extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['barcode']);
+	}
+	foreach ($this['barcode'] as $barcode) {
 		$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid']);
-		$xpath= new DomXpath($this->xml)
-		var_dump($xpath->evaluate("//additional_info"));
+		function print_success() {
+			XMLtoWeb($this->item->xml);
+		}
+	}
+		//$xpath= new DomXpath($this->xml)
+		//var_dump($xpath->evaluate("//additional_info"));
 		
 		//$this->addMessage('success',$item->addInfo());
 		//echo $item['additional_info'];
 		//$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} with a Process Type of: {$item['process_type']}");
 		//$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} {$item['mms_id']} {$item['holding_id']} {$item['item_pid']} Process: {$item['process_type']} Additional Information: {$item['additional_info']}");
-	}
+
 }
 
 ScanInReturn::RunIt();
