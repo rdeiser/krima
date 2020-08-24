0e2365093a499ab5827d91b51bfa42f10d07cbb0
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-24 10:58:33 -0500

    Update rightBody.php

--- grimas/ScanInReturn/splats/rightBody.php
+++ grimas/ScanInReturn/splats/rightBody.php
@@ -2,6 +2,9 @@
 if ($item['additional_info'] =="Item's destination is: Hale Library. Request/Process Type: Transit for reshelving. Requester: . Requester ID: . Place in Queue: 1") {
 	$pattern = "/^(Item's destination is: Hale Library. Request/Process Type: Transit for reshelving. Requester: . Requester ID: . Place in Queue: 1)/";
 	$replace = 'Hale Library';
+} else {
+	$pattern = "/^(Item's destination is: Reshelve to main. Request/Process Type: . Requester: . Requester ID: . Place in Queue: 0)/";
+	$replace = 'Hale Library';
 }
 ?>
               <table class="table">
