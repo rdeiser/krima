afef3d28a3af5a1fcad4a0820cbc8f62d98eea06
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-24 11:55:35 -0500

    Update side-by-side.php

--- grimas/ScanInReturn/splats/side-by-side.php
+++ grimas/ScanInReturn/splats/side-by-side.php
@@ -1,3 +1,70 @@
+<?php 
+if ($item['statistics_note_3'] == 'HALE return') {
+	$pattern = '/(HALE return)/';
+	$replace = 'Send to Hale Library';
+} 
+if ($item['statistics_note_3'] == 'ANNEX ingest') {
+	$pattern = '/(ANNEX ingest)/';
+	$replace = 'Send to Annex';
+}
+if ($item['statistics_note_3'] == 'To be WITHDRAWN') {
+	$pattern = '/(To be WITHDRAWN)/';
+	$replace = 'Send to DBM';
+}
+if ($item['statistics_note_3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdraw') {
+	$pattern = '/(PHYSICAL CONDITION REVIEW For Possible Withdraw)/';
+	$replace = 'Send to DBM';
+}
+if ($item['statistics_note_3'] == 'AHD HALE return') {
+	$pattern = '/(AHD HALE return)/';
+	$replace = 'Send to Hale Library';
+} 
+if ($item['statistics_note_3'] == 'AHD ANNEX ingest') {
+	$pattern = '/(AHD ANNEX ingest)/';
+	$replace = 'Send to Annex';
+}
+if ($item['statistics_note_3'] == 'AHD To be WITHDRAWN') {
+	$pattern = '/(AHD To be WITHDRAWN)/';
+	$replace = 'Send to DBM';
+}
+if ($item['statistics_note_3'] == '') {
+	$pattern = '/^/';
+	$replace = 'Send to DBM';
+}
+
+if ($item['statistics_note_3']=='To be WITHDRAWN') {
+			$style = 'style="background-color: #cd5555;"';
+			$text = '';
+}
+	else if ($item['statistics_note_3']=='ANNEX ingest') {
+			$style = 'style="background-color: #6495ed;"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='HALE return') {
+			$style = 'style="background-color: #ab82ff;"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='AHD HALE return') {
+			$style = 'style="background-color: #ab82ff;"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='AHD To be WITHDRAWN') {
+			$style = 'style="background-color: #cd5555;"';
+			$text = '';
+}
+	else if ($item['statistics_note_3']=='AHD ANNEX ingest') {
+			$style = 'style="background-color: #6495ed;"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='PHYSICAL CONDITION REVIEW For Possible Withdraw') {
+			$style = 'style="#cd5555;"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='') {
+			$style = 'style=";"';
+			//$text = 'Send to Problem Shelf';
+	}
+					?>
 <!--<h1 class="page-header text-center"><?=$e($title)?></h1>-->
         <div class="row">
           <div class="col">
