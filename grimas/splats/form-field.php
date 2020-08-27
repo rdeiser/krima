50b2bb8f38f96670d1689b88c69b65c42b98d9e3
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-08-27 13:53:37 -0500

    Update form-field.php

--- grimas/splats/form-field.php
+++ grimas/splats/form-field.php
@@ -47,14 +47,6 @@
 	  value="<?=$e($value)?>"
 	>
 	<label for="<?=$e($id)?>"><?=$e($label)?></label>
-	
-	<input
-	  name="<?=$e($name)?>"
-      id="<?=$e($name)?>-no"
-	  type="<?=$e($type)?>"
-	  value="<?=$e($value)?>"
-	>
-	<label for="<?=$e($name)?>-no"><?=$e($label)?></label>
 	</fieldset>
 <?php else: ?>
     <input
