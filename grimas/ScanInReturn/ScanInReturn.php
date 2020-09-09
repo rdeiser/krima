3ab8778ee92ab975b20952f81442d57eed2b3fd4
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-09-09 15:15:28 -0500

    Update ScanInReturn.php

--- grimas/ScanInReturn/ScanInReturn.php
+++ grimas/ScanInReturn/ScanInReturn.php
@@ -12,6 +12,9 @@ class ScanInReturn extends GrimaTask {
 		if ($this['order'] == 'true') {
 			$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = $this['library'],$circ_desk = 'DEFAULT_CIRC_DESK',$work_order_type = 'Quarantine',$status = '72%20Hour%20Quarantine',$done = $this['done'],$place_on_hold_shelf = $this['hold'],$register_in_house_use = 'false');
 		}
+		if ($this['order'] == 'false') {
+			$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = $this['library'],$circ_desk = 'DEFAULT_CIRC_DESK',$work_order_type,$status,$done = $this['done'],$place_on_hold_shelf = $this['hold'],$register_in_house_use = 'false');
+		}
 		//$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = $this['library'],$circ_desk = 'DEFAULT_CIRC_DESK',$work_order_type = $this['order'],$status = $this['status'],$done = $this['done'],$place_on_hold_shelf = $this['hold'],$register_in_house_use = $this['house']);
 
 		//$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = $this['library'],$circ_desk = 'DEFAULT_CIRC_DESK');
