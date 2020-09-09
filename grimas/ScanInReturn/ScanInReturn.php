655878b5318e8cfc48b0c5dd4a69c5135f8b6a01
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-09-09 14:06:55 -0500

    Update ScanInReturn.php

--- grimas/ScanInReturn/ScanInReturn.php
+++ grimas/ScanInReturn/ScanInReturn.php
@@ -11,7 +11,8 @@ class ScanInReturn extends GrimaTask {
 		$this->item->loadFromAlmaX($item['item_pid']);
 		if ($this['order'] == 'true') {
 			$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = $this['library'],$circ_desk = 'DEFAULT_CIRC_DESK',$work_order_type = 'Quarantine',$status = '72 Hour Quarantine',$done = $this['done'],$place_on_hold_shelf = $this['hold'],$register_in_house_use = 'false');
-		} else {
+		}
+		if ($this['order'] == 'false') {
 			$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = $this['library'],$circ_desk = 'DEFAULT_CIRC_DESK');
 		}
 			
