993edc023ae4c77124d1768a5a4963c7b8497166
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-09-09 12:50:42 -0500

    Update ScanInReturn.php

--- grimas/ScanInReturn/ScanInReturn.php
+++ grimas/ScanInReturn/ScanInReturn.php
@@ -9,11 +9,12 @@ class ScanInReturn extends GrimaTask {
 
 		$this->item = new Item();
 		$this->item->loadFromAlmaX($item['item_pid']);
-		if $this['hold'] == 'true' {
+		$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = $this['library'],$circ_desk = 'DEFAULT_CIRC_DESK');
+		/*if $this['hold'] == 'true' {
 			$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = $this['library'],$place_on_hold_shelf = $this['hold'],$circ_desk = 'DEFAULT_CIRC_DESK');
 		} elseif {
 			$this->item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = $this['library'],$circ_desk = 'DEFAULT_CIRC_DESK');
-		}
+		}*/
 		$this->splatVars['item'] = $this->item;
 
 		//$this->addMessage('success',"Successfully Scanned In: {$item['barcode']} Additional Information: {$this->item['additional_info']}");
