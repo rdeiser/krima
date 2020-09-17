520e71fa5059ce49e316964970f0f8fc0e456c45
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-09-17 09:43:37 -0500

    New gov grima

---
+++ grimas/WDGovDocUpdate/WDGovDocUpdate.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("../grima-lib.php");
+
+class WDGovDocUpdate extends GrimaTask {
+	public $holdinglist = array();
+
+	function do_task() {
+		$item = new Item();
+		$item->loadFromAlmaBCorX($this['barcode']);
+		$item['inventory_date'] = date("Y-m-d");
+		$item['library_code'] = 'WITHDRAW';
+		$item['location_code'] = 'wdgov';
+		if (isset($item['internal_note_1'])) {
+			substr_replace("Gov unboxing review--",$item['internal_note_1'],21);
+		} else {
+			$item['internal_note_1'] = 'Gov unboxing review';
+		}
+		$item['internal_note_1'] = 'Gov unboxing review';
+		$item->updateAlma();
+				
+		$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
+		} else {
+			$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
+	}
+}
+
+WDGovDocUpdate::RunIt();
