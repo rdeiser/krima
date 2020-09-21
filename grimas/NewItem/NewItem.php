2f264d9c6d8e943237fa816bf4044b4703bce316
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-09-21 15:03:59 -0500

    Update NewItem.php

--- grimas/NewItem/NewItem.php
+++ grimas/NewItem/NewItem.php
@@ -13,91 +13,6 @@ class NewItem extends GrimaTask {
 			$this['mms_id'] = Holding::getMmsFromHoldingID($holdingid);
 			if ($this['mms_id']) {
 				$holding->loadFromAlma($this['mms_id'],$holdingid);
-				if ($this['statnote3'] == 'AHD To be WITHDRAWN'){
-					if ($holding['location_code'] == 'main') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdmain';
-					} 
-					if ($holding['location_code'] == 'over') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdover';
-					} 
-					if ($holding['location_code'] == 'cmc') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdcmc';
-					} 
-					if ($holding['location_code'] == 'juv') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdjuv';
-					} 
-					if ($holding['location_code'] == 'overplus') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdoverplus';
-					} 
-					if ($holding['location_code'] == 'ref') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdref';
-					}
-					$holding->updateAlma();
-				}
-				if ($this['statnote3'] == 'To be WITHDRAWN'){
-					if ($holding['location_code'] == 'annex') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdmain';
-					} 
-					if ($holding['location_code'] == 'main') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdmain';
-					} 
-					if ($holding['location_code'] == 'over') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdover';
-					} 
-					if ($holding['location_code'] == 'cmc') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdcmc';
-					} 
-					if ($holding['location_code'] == 'juv') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdjuv';
-					} 
-					if ($holding['location_code'] == 'overplus') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdoverplus';
-					} 
-					if ($holding['location_code'] == 'ref') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdref';
-					}
-					$holding->updateAlma();
-				}
-				if ($this['statnote3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdraw'){
-					if ($holding['location_code'] == 'main') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdmain';
-					} 
-					if ($holding['location_code'] == 'over') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdover';
-					} 
-					if ($holding['location_code'] == 'cmc') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdcmc';
-					} 
-					if ($holding['location_code'] == 'juv') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdjuv';
-					} 
-					if ($holding['location_code'] == 'overplus') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdoverplus';
-					} 
-					if ($holding['location_code'] == 'ref') {
-						$holding['library_code'] = 'WITHDRAW';
-						$holding['location_code'] = 'wdref';
-					}
-					$holding->updateAlma();
-				}
 				if(empty($this['barcode'])) {
 					$item = new Itemnbc();
 					if (empty($this['copyid'])) {
@@ -143,6 +58,145 @@ class NewItem extends GrimaTask {
 					$item['statistics_note_3'] = $this['statnote3'];
 					$item['barcode'] = $this['barcode'];
 					$item->addToAlmaHolding($this['mms_id'],$holdingid);
+					
+					$this->item = new Item();
+					$this->item->loadFromAlmaX($item['item_pid']);
+					if ($this['statnote3'] == 'AHD To be WITHDRAWN'){
+					if ($item['location_code'] == 'main') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdmain';
+					} 
+					if ($item['location_code'] == 'over') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdover';
+					} 
+					if ($item['location_code'] == 'cmc') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdcmc';
+					} 
+					if ($item['location_code'] == 'juv') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdjuv';
+					} 
+					if ($item['location_code'] == 'overplus') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdoverplus';
+					} 
+					if ($item['location_code'] == 'ref') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdref';
+					}
+					$this->item->updateAlma();
+				}
+				if ($this['statnote3'] == 'To be WITHDRAWN'){
+					if ($item['location_code'] == 'annex') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdmain';
+					} 
+					if ($item['location_code'] == 'main') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdmain';
+					} 
+					if ($item['location_code'] == 'over') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdover';
+					} 
+					if ($item['location_code'] == 'cmc') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdcmc';
+					} 
+					if ($item['location_code'] == 'juv') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdjuv';
+					} 
+					if ($item['location_code'] == 'overplus') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdoverplus';
+					} 
+					if ($item['location_code'] == 'ref') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdref';
+					}
+					$this->item->updateAlma();
+				}
+				if ($this['statnote3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdraw'){
+					if ($item['location_code'] == 'main') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdmain';
+					} 
+					if ($item['location_code'] == 'over') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdover';
+					} 
+					if ($item['location_code'] == 'cmc') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdcmc';
+					} 
+					if ($item['location_code'] == 'juv') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdjuv';
+					} 
+					if ($item['location_code'] == 'overplus') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdoverplus';
+					} 
+					if ($item['location_code'] == 'ref') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdref';
+					}
+					$this->item->updateAlma();
+				}
+				if ($this['statnote3'] == 'Gov unboxing review'){
+					if($item['location_code'] == 'main') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdgov';
+					}
+					if($item['location_code'] == 'gov') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdgov';
+					}
+					if($item['location_code'] == 'govcen') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdgovcen';
+					}
+					if($item['location_code'] == 'govelect') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdgovelect';
+					}
+					if($item['location_code'] == 'govmap') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdgovmap';
+					}
+					if($item['location_code'] == 'govmfile') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdgovmfile';
+					}
+					if($item['location_code'] == 'govmic') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdgovmic';
+					}
+					if($item['location_code'] == 'govover') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdgovover';
+					}
+					if($item['location_code'] == 'govref') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wdgovref';
+					}
+					if($item['location_code'] == 'govmindex') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wgovmindex';
+					}
+					if($item['location_code'] == 'govoffmap') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wgovoffmap';
+					}
+					if($item['location_code'] == 'govposter') {
+						$item['library_code'] = 'WITHDRAW';
+						$item['location_code'] = 'wgovposter';
+					}
+				$this->item->updateAlma();
+				}
 					$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
 				}
 			} else {
