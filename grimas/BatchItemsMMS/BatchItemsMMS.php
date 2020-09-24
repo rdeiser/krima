df631d419735d8168e7b3fca3248653001f063e0
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-09-24 13:37:03 -0500

    Update BatchItemsMMS.php

--- grimas/BatchItemsMMS/BatchItemsMMS.php
+++ grimas/BatchItemsMMS/BatchItemsMMS.php
@@ -92,70 +92,124 @@ class BatchItemsMMS extends GrimaTask {
 					}
 				}
 				$holding->updateAlma();
-
-			$item = new Itemnbc();
-				//$item['fulfillment_note'] = $this['fulnote'];
-				$item['item_policy'] = 'book/ser';
-				$item['pieces'] = '1';
-				$item['inventory_date'] = '1976-01-01';
-				$item['receiving_operator'] = 'Grima';
-				$item['statistics_note_2'] = 'FIRE 2018 OZONE';
-				$item['statistics_note_3'] = $this['whichnote'];
-				$item->addToAlmaHolding($mmsid,$this['holding_id']);
 				
-				$this->item = new Item();
-				$this->item->loadFromAlmaX($item['item_pid']);
-				if ($this->item['statistics_note_3'] == 'GOV UNBOXING review') {
-				if($this->item['location_code'] == 'main') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wdgov';
-				}
-				if($this->item['location_code'] == 'gov') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wdgov';
-				}
-				if($this->item['location_code'] == 'govcen') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wdgovcen';
-				}
-				if($this->item['location_code'] == 'govelect') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wdgovelect';
-				}
-				if($this->item['location_code'] == 'govmap') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wdgovmap';
-				}
-				if($this->item['location_code'] == 'govmfile') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wdgovmfile';
-				}
-				if($this->item['location_code'] == 'govmic') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wdgovmic';
-				}
-				if($this->item['location_code'] == 'govover') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wdgovover';
-				}
-				if($this->item['location_code'] == 'govref') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wdgovref';
-				}
-				if($this->item['location_code'] == 'govmindex') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wgovmindex';
-				}
-				if($this->item['location_code'] == 'govoffmap') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wgovoffmap';
-				}
-				if($this->item['location_code'] == 'govposter') {
-					$this->item['library_code'] = 'WITHDRAW';
-					$this->item['location_code'] = 'wgovposter';
-				}
-				$this->item->updateAlma();
-			}
+				if (empty($this['barcode'])) {
+					$item = new Itemnbc();
+					//$item['fulfillment_note'] = $this['fulnote'];
+					$item['item_policy'] = 'book/ser';
+					$item['pieces'] = '1';
+					$item['inventory_date'] = '1976-01-01';
+					$item['receiving_operator'] = 'Grima';
+					$item['statistics_note_2'] = 'FIRE 2018 OZONE';
+					$item['statistics_note_3'] = $this['whichnote'];
+					$item->addToAlmaHolding($mmsid,$this['holding_id']);
+					
+					if ($this->item['statistics_note_3'] == 'GOV UNBOXING review') {
+						if($this->item['location_code'] == 'main') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgov';
+						}
+						if($this->item['location_code'] == 'gov') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgov';
+						}
+						if($this->item['location_code'] == 'govcen') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovcen';
+						}
+						if($this->item['location_code'] == 'govelect') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovelect';
+						}
+						if($this->item['location_code'] == 'govmap') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovmap';
+						}
+						if($this->item['location_code'] == 'govmfile') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovmfile';
+						}
+						if($this->item['location_code'] == 'govmic') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovmic';
+						}
+						if($this->item['location_code'] == 'govover') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovover';
+						}
+						if($this->item['location_code'] == 'govref') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovref';
+						}
+						if($this->item['location_code'] == 'govmindex') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wgovmindex';
+						}
+						if($this->item['location_code'] == 'govoffmap') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wgovoffmap';
+						}
+						if($this->item['location_code'] == 'govposter') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wgovposter';
+						}
+						$this->item->updateAlma();
+					}
+				} else {
+					$this->item = new Item();
+					$this->item->loadFromAlmaX($item['item_pid']);
+					$this->item['barcode'] = $this['barcode'];
+					if ($this->item['statistics_note_3'] == 'GOV UNBOXING review') {
+						if($this->item['location_code'] == 'main') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgov';
+						}
+						if($this->item['location_code'] == 'gov') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgov';
+						}
+						if($this->item['location_code'] == 'govcen') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovcen';
+						}
+						if($this->item['location_code'] == 'govelect') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovelect';
+						}
+						if($this->item['location_code'] == 'govmap') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovmap';
+						}
+						if($this->item['location_code'] == 'govmfile') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovmfile';
+						}
+						if($this->item['location_code'] == 'govmic') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovmic';
+						}
+						if($this->item['location_code'] == 'govover') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovover';
+						}
+						if($this->item['location_code'] == 'govref') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wdgovref';
+						}
+						if($this->item['location_code'] == 'govmindex') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wgovmindex';
+						}
+						if($this->item['location_code'] == 'govoffmap') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wgovoffmap';
+						}
+						if($this->item['location_code'] == 'govposter') {
+							$this->item['library_code'] = 'WITHDRAW';
+							$this->item['location_code'] = 'wgovposter';
+						}
+						$this->item->updateAlma();
+					}
 
 			$this->addMessage('success',"Successfully added an Item Record to {$holding['holding_id']} with Barcode: {$this->item['barcode']}");
 				/*function print_success() {
