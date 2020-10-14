27095d80d23d9424ac9dc107531db2e2284d4246
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-10-14 08:59:17 -0500

    Update DisplayItem.php

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -36,6 +36,8 @@ class DisplayItem extends GrimaTask {
 {
 		$this->item = new Item();
 		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$this->holding = new Holding();
+		$this->holding->loadFromAlma($holding['holding_id'],$holding['holding_id']);
 		$this->splatVars['item'] = $this->item;
 }
 }
