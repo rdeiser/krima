0196c6dce8314962c53794523205194986939c9a
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-10-14 09:04:35 -0500

    Update rightBody.php

--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -196,7 +196,7 @@ if (preg_match("/[sS]end/", $item['fulfillment_note'])) {
 				<tr><th>Internal Note 3:</th><td><?=$e($item['internal_note_3'])?></td></tr>-->
 				<tr><th class="flip"><span>Destination:</span><span>Destino:</span></th><td class="statnote"><?= preg_replace($pattern, $replace, $item['statistics_note_3'])?>
 				</td></tr>
-				<tr><th>Holding Suppressed:</th><td><?=$e($this->holding['suppress_from_publishing'])?>
+				<tr><th>Holding Suppressed:</th><td><?=$e($item['suppress_from_publishing'])?>
 				</td></tr>
 				<tr><th>Holding ID:</th><td><?=$e($item['holding_id'])?>
 				</td></tr>
