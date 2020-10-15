97d59ea71fdd83ff5ae7420d2d341bf67fcb09bd
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-10-15 15:11:05 -0500

    Update rightBody.php

--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -192,7 +192,7 @@ if ($holding['suppress_from_publishing'] == 'true'){
 			$pattern = '/(HALE return)/';
 			$replace = 'Send to Problem Shelf';
 		}
-} else {
+} elseif ($bib['suppress_from_publishing'] == 'true'){
 	$bib = new bib();
 	$bib->loadFromAlma($item['mms_id']);
 	if ($bib['suppress_from_publishing'] == 'true'){
