90c8eefb15d77d390aa17d8319c22f4353d9098a
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-11-06 06:56:54 -0600

    Update DisplayItem.php

--- grimas/DisplayItem/DisplayItem.php
+++ grimas/DisplayItem/DisplayItem.php
@@ -17,7 +17,9 @@ class DisplayItem extends GrimaTask {
 		unset($item['temp_call_number']);
 		unset($item['temp_policy']);
 		//unset($item['alt_number_source']);
-		$item['due_back_date'] = '';
+		if (isset($item['due_back_date'])) {
+			$item['due_back_date'] = '';
+		}
 		/*if ($item['in_temp_location'] == 'true') {
 			
 		if ($item['statistics_note_3'] == 'ANNEX ingest') {
