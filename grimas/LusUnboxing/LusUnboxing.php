7cc9fd054bab62be0163068f0266f549044ef62e
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-11-16 14:43:15 -0600

    Clean up and 1 newish grima

---
+++ grimas/LusUnboxing/LusUnboxing.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once("../grima-lib.php");
+
+class LusUnboxing extends GrimaTask {
+
+	function do_task() {
+		{$item = new Item();
+		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$item->addInventoryDate(date("Y-m-d"));
+		//$item['inventory_date'] = date("Y-m-d");
+		if ($item['in_temp_location'] = 'true') {
+			$item['in_temp_location'] = 'false';
+		}
+		unset($item['temp_location']);
+		unset($item['temp_call_number_type']);
+		unset($item['temp_call_number']);
+		unset($item['temp_policy']);
+		//unset($item['alt_number_source']);
+		$item['due_back_date'] = '';
+
+		/*if ($item['in_temp_location'] == 'true') {
+			
+		if ($item['statistics_note_3'] == 'ANNEX ingest') {
+			$item['in_temp_location'] = 'false';
+		}
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$item['in_temp_location'] = 'false';
+		}
+		if ($item['statistics_note_3'] == 'To be WITHDRAWN') {
+			$item['in_temp_location'] = 'false';
+		}
+		if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$item['in_temp_location'] = 'false';
+		}
+		if ($item['statistics_note_3'] == 'AHD ANNEX ingest') {
+			$item['in_temp_location'] = 'false';
+		}
+		if ($item['statistics_note_3'] == 'AHD To be WITHDRAWN') {
+			$item['in_temp_location'] = 'false';
+		}*/
+		//$item['inventory_date'] = date("Y-m-d g:i:s A");
+		/*if ($item['in_temp_location'] == 'true') {
+			$item['in_temp_location'] = 'false';
+		}*/
+		$item->updateAlma();
+		}
+{
+		$this->item = new Item();
+		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$this->splatVars['item'] = $this->item;
+}
+}
+}
+
+LusUnboxing::RunIt();
---
+++ grimas/LusUnboxing/splats/side-by-side.php
@@ -0,0 +1,207 @@
+<?php 
+if ($item['statistics_note_3'] == 'HALE return') {
+	$pattern = '/(HALE return)/';
+	$replace = 'HALE return';
+} 
+if ($item['statistics_note_3'] == 'ANNEX ingest') {
+	$pattern = '/(ANNEX ingest)/';
+	$replace = 'ANNEX ingest';
+}
+if ($item['statistics_note_3'] == 'To be WITHDRAWN') {
+	$pattern = '/(To be WITHDRAWN)/';
+	$replace = 'To be WITHDRAWN';
+}
+if ($item['statistics_note_3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdraw') {
+	$pattern = '/(PHYSICAL CONDITION REVIEW For Possible Withdraw)/';
+	$replace = 'Send to Condition Review Shelf';
+}
+if ($item['statistics_note_3'] == 'AHD HALE return') {
+	$pattern = '/(AHD HALE return)/';
+	$replace = 'HALE return';
+} 
+if ($item['statistics_note_3'] == 'AHD ANNEX ingest') {
+	$pattern = '/(AHD ANNEX ingest)/';
+	$replace = 'ANNEX ingest';
+}
+if ($item['statistics_note_3'] == 'AHD To be WITHDRAWN') {
+	$pattern = '/(AHD To be WITHDRAWN)/';
+	$replace = 'To be WITHDRAWN';
+}
+if ($item['statistics_note_3'] == '') {
+	$pattern = '/^/';
+	$replace = 'Send to Problem Shelf';
+}
+
+if ($item['statistics_note_3']=='To be WITHDRAWN') {
+			$style = 'style="background-color: #cd5555;"';
+			$text = '';
+}
+	else if ($item['statistics_note_3']=='ANNEX ingest') {
+			$style = 'style="background-color: #6495ed;"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='HALE return') {
+			$style = 'style="background-color: #ab82ff;"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='AHD HALE return') {
+			$style = 'style="background-color: #ab82ff;"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='AHD To be WITHDRAWN') {
+			$style = 'style="background-color: #cd5555;"';
+			$text = '';
+}
+	else if ($item['statistics_note_3']=='AHD ANNEX ingest') {
+			$style = 'style="background-color: #6495ed;"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='PHYSICAL CONDITION REVIEW For Possible Withdraw') {
+			$style = 'style=";"';
+			$text = '';
+	}
+	else if ($item['statistics_note_3']=='') {
+			$style = 'style=";"';
+			//$text = 'Send to Problem Shelf';
+	}
+	/*if ($item['location'] == 'juv') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'cmc') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	/*if ($item['location_code'] == 'main') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'over') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'overplus') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'dowref') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'ref') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'juv') {
+		if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'cmc') {
+		if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	/*if ($item['location'] == 'main') {
+		if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'over') {
+		if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'overplus') {
+		if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'dowref') {
+		if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
+	if ($item['location'] == 'ref') {
+		if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}*/
+	
+	$holding = new Holding();
+	$holding->loadFromAlma($item['mms_id'],$item['holding_id']);
+	if ($holding['suppress_from_publishing'] == 'true'){
+		if ($item['statistics_note_3']== 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+			}else if ($item['statistics_note_3']== 'HALE return') {
+				$pattern = '/(HALE return)/';
+				$replace = 'Send to Problem Shelf';
+				$style = 'style=";"';
+				}
+	}else {
+		$bib = new bib();
+		$bib->loadFromAlma($item['mms_id']);
+		if ($bib['suppress_from_publishing'] == 'true'){
+			if ($item['statistics_note_3']== 'AHD HALE return') {
+				$pattern = '/(AHD HALE return)/';
+				$replace = 'Send to Problem Shelf';
+				$style = 'style=";"';
+				} else if ($item['statistics_note_3']== 'HALE return') {
+					$pattern = '/(HALE return)/';
+					$replace = 'Send to Problem Shelf';
+					$style = 'style=";"';
+				}
+		}
+	}
+					?>
+		<!--<h1 class="page-header text-center"><?=$e($title)?></h1>-->
+        <div class="row">
+          <div class="col">
+            <div class="card" <?=$style?>>
+              <div class="card-header">
+                <h2 class="card-title"><?=preg_replace($pattern, $replace, $item['statistics_note_3'])?></h2>
+              </div>
+              <div class="card-body">
+<?= $t('rightBody', array('item' => $item)) ?>
+              </div>
+            </div>
+          </div>
+        </div>
+<?= $t('messages') ?>
