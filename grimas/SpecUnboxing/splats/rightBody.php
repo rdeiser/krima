65b0ef59c2b117cea3f505779ecad57cec38dbde
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-12-04 10:32:20 -0600

    Update rightBody.php

--- grimas/SpecUnboxing/splats/rightBody.php
+++ grimas/SpecUnboxing/splats/rightBody.php
@@ -165,9 +165,9 @@ if (preg_match("/[sS]end/", $item['fulfillment_note'])) {
 	else if (preg_match("/[gG]ive/", $item['fulfillment_note'])) {
 		if (preg_match("/.[Archival box–check in and give to staff]/", $item['fulfillment_note'])) {
 			$style2 = 'style=";"';
-		}else {
+		}/*else {
 			$style2 = 'style="background-color:#cd3700;"';
-		}
+		}*/
 }
 	else if (preg_match("/[iI]LL/", $item['fulfillment_note'])) {
 		$style2 = 'style="background-color:#cd3700;"';
