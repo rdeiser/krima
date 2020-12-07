da5e9dfa41477fe08c6391db4b04c7fc2870504d
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-12-07 12:13:09 -0600

    Update Boundwith.php

--- grimas/Boundwith/Boundwith.php
+++ grimas/Boundwith/Boundwith.php
@@ -49,8 +49,9 @@ class Boundwith extends GrimaTask {
 		$this->biblist[0]->getHoldings();
 		$mfhd = $this->biblist[0]->holdings[0];
 
-		foreach ($this->biblist as $k => $bib) {
-			if ($title != $skip) {
+		foreach ($this->biblist as $bib) {
+			$maintitle = array_shift($bib);
+			foreach ($maintitle) {
 				$mfhd->appendField("014","1"," ",array('x' => 'BOUNDWITH'));
 			}
 		}
