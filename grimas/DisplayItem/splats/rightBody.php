47b7e00f324c5279a506be08cea9bd8c65f026f9
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2020-12-09 15:26:08 -0600

    Update rightBody.php

--- grimas/DisplayItem/splats/rightBody.php
+++ grimas/DisplayItem/splats/rightBody.php
@@ -215,7 +215,7 @@ if ($holding['suppress_from_publishing'] == 'true'){
 ?>
               <table class="table">
                 <tr><th class="flip"><span>Title:</span><span>Título:</span></th><td><?=$e($item['title'])?></td></tr>
-				<tr><th class="flip"><span>Call Number:</span><span>Número de clasificación:</span></th><td><?=$e($item['call_number'])?> <text <?=$style3?>>c.<?=$e($item['copy_id'])?></text></td></tr>
+				<tr><th class="flip"><span>Call Number:</span><span>Número de clasificación:</span></th><td><?=$e($item['call_number'])?><text <?=$style3?>> c.<?=$e($item['copy_id'])?></text></td></tr>
 				<tr><th class="flip"><span>Description:</span><span>Descripción:</span></th><td><?=$e($item['description'])?></td></tr>
 				<tr><th class="flip"><span>Barcode:</span><span>Código de barras:<!--Código de procedencia:--></span></th><td><?=$e($item['barcode'])?></td></tr>
 				<!--<tr><th class="flip"><span>Location:</span><span>Lugar:</span></th><td><?=$e($item['location'])?></td></tr>-->
