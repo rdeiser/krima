725629f6585c283fc24e8507f598e9025cb4f6be
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-01-22 13:20:20 -0600

    Update side-by-side.php

--- grimas/DisplayMicFilm/splats/side-by-side.php
+++ grimas/DisplayMicFilm/splats/side-by-side.php
@@ -64,6 +64,17 @@ if ($item['statistics_note_3']=='To be WITHDRAWN') {
 			$style = 'style=";"';
 			//$text = 'Send to Problem Shelf';
 	}
+	if ($item['location'] !== "microfilm") {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		} else if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}
 	/*if ($item['location'] !== "juv") {
 		if ($item['statistics_note_3'] == 'HALE return') {
 			$pattern = '/(HALE return)/';
