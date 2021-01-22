c8a3908f4adf5aac2ce17fbc9da41eab601ee2b1
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-01-22 12:46:46 -0600

    Update DisplayMicFilm.php

--- grimas/DisplayMicFilm/DisplayMicFilm.php
+++ grimas/DisplayMicFilm/DisplayMicFilm.php
@@ -31,7 +31,7 @@ class DisplayMicFilm extends GrimaTask {
 		if ($holding['location_code'] == 'mic') {
 			$holding['location_code'] = 'microfilm';
 		}
-		$subfield_k = "";
+		$subfield_k = "Raymond";
 		$holding->appendField("852","","",array('k' => $subfield_k));
 		$holding->updateAlma();
 		}
