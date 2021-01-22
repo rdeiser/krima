bfb4da1fd7026f03b91ffac8b8e10c05365bf4be
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-01-22 10:28:28 -0600

    Update LusUnboxing.php

--- grimas/LusUnboxing/LusUnboxing.php
+++ grimas/LusUnboxing/LusUnboxing.php
@@ -11,16 +11,13 @@ class LusUnboxing extends GrimaTask {
 		//$item['inventory_date'] = date("Y-m-d");
 		if ($item['in_temp_location'] = 'true') {
 			$item['in_temp_location'] = 'false';
+			$item['due_back_date'] = '';
 		}
 		unset($item['temp_location']);
 		unset($item['temp_call_number_type']);
 		unset($item['temp_call_number']);
 		unset($item['temp_policy']);
 		//unset($item['alt_number_source']);
-		$item['due_back_date'] = '';
-		/*if (isset($item['due_back_date'])) {
-			$item['due_back_date'] == '';
-		}*/
 
 		$item->updateAlma();
 		}
