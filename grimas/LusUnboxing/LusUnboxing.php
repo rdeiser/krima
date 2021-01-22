9b6f78ed191e9531cfb5f1e484df7ad34dd5de44
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-01-22 10:38:00 -0600

    Update LusUnboxing.php

--- grimas/LusUnboxing/LusUnboxing.php
+++ grimas/LusUnboxing/LusUnboxing.php
@@ -18,6 +18,10 @@ class LusUnboxing extends GrimaTask {
 		unset($item['temp_call_number']);
 		unset($item['temp_policy']);
 		//unset($item['alt_number_source']);
+		$item['due_back_date'] = '';
+		if (!empty($item['due_back_date'])) {
+			$item['due_back_date'] == '';
+		}
 
 		$item->updateAlma();
 		}
