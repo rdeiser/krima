6723e283d7aa2e3a23c1665e1e7722a5c37683a4
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-03-25 12:18:41 -0500

    Update side-by-side.php

--- grimas/DisplayKUGov/splats/side-by-side.php
+++ grimas/DisplayKUGov/splats/side-by-side.php
@@ -4,18 +4,18 @@ if ($item['statistics_note_3'] == 'KU FDLP REQUEST') {
 	$replace = 'Send to KU';
 }
 
-if ($item['statistics_note_3'] == '') {
+if ($item['library_code'] == 'WITHDRAW') {
 	$pattern = '//';
 	$replace = 'GOV WITHDRAW';
-} else if ($item['statistics_note_3'] !== 'KU FDLP REQUEST') {
+	$style = 'style="background-color: #cd5555;"';
+}else if ($item['library_code'] !== 'WITHDRAW') {
+	$pattern = '/[A-z]/';
+	$replace = 'Send to Problem Shelf';
+}else if ($item['statistics_note_3'] !== 'KU FDLP REQUEST') {
 	$pattern = '/(ANNEX ingest)|(HALE return)|(To be WITHDRAWN)|(AHD ANNEX ingest)|(AHD HALE return)|(AHD To be WITHDRAWN)|(GOV UNBOXING review)|(PHYSICAL CONDITION REVIEW)|(Needs pam binder)/';
 	$replace = 'Send to Problem Shelf';
 }
 
-if ($item['statistics_note_3']=='') {
-			$style = 'style="background-color: #cd5555;"';
-			$text = '';
-}
 	else if ($item['statistics_note_3']=='KU FDLP REQUEST') {
 			$style = 'style="background-color: #ab82ff;"';
 			$text = '';
