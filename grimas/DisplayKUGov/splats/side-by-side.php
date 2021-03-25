926abb698158ae0934221b09255306042faabe4b
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-03-25 12:31:13 -0500

    Update side-by-side.php

--- grimas/DisplayKUGov/splats/side-by-side.php
+++ grimas/DisplayKUGov/splats/side-by-side.php
@@ -17,6 +17,7 @@ if ($item['statistics_note_3'] == '') {
 }else if ($item['statistics_note_3'] !== 'KU FDLP REQUEST') {
 	$pattern = '/(ANNEX ingest)|(HALE return)|(To be WITHDRAWN)|(AHD ANNEX ingest)|(AHD HALE return)|(AHD To be WITHDRAWN)|(GOV UNBOXING review)|(PHYSICAL CONDITION REVIEW)|(Needs pam binder)/';
 	$replace = 'Send to Problem Shelf';
+	$style = 'style="background-color: #ffffff;"';
 }
 
 	else if ($item['statistics_note_3']=='KU FDLP REQUEST') {
