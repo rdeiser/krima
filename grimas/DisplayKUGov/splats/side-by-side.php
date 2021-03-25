9d2ecdfd2641dbc68bb9592eccf43b07dd740516
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-03-25 12:19:56 -0500

    Update side-by-side.php

--- grimas/DisplayKUGov/splats/side-by-side.php
+++ grimas/DisplayKUGov/splats/side-by-side.php
@@ -11,10 +11,10 @@ if ($item['library_code'] == 'WITHDRAW') {
 }else if ($item['library_code'] !== 'WITHDRAW') {
 	$pattern = '/[A-z]/';
 	$replace = 'Send to Problem Shelf';
-}else if ($item['statistics_note_3'] !== 'KU FDLP REQUEST') {
+}/*else if ($item['statistics_note_3'] !== 'KU FDLP REQUEST') {
 	$pattern = '/(ANNEX ingest)|(HALE return)|(To be WITHDRAWN)|(AHD ANNEX ingest)|(AHD HALE return)|(AHD To be WITHDRAWN)|(GOV UNBOXING review)|(PHYSICAL CONDITION REVIEW)|(Needs pam binder)/';
 	$replace = 'Send to Problem Shelf';
-}
+}*/
 
 	else if ($item['statistics_note_3']=='KU FDLP REQUEST') {
 			$style = 'style="background-color: #ab82ff;"';
