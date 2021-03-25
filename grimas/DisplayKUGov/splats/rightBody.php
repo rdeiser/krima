c9853ad09d8069e5b9123a2390ee33536968997b
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-03-25 11:05:36 -0500

    Update rightBody.php

--- grimas/DisplayKUGov/splats/rightBody.php
+++ grimas/DisplayKUGov/splats/rightBody.php
@@ -8,6 +8,9 @@ if ($item['statistics_note_3'] == '') {
 	if ($item['location_code'] == 'gov'||'govcen'||'govelect'||'govmap'||'govmfile'||'govmic'||'govover'||'govref'||'govmindex'||'govoffmap'||'govposter'||'wdgov'||'wdgovcen'||'wdgovelect'||'wdgovmap'||'wdgovmfile'||'wdgovmic'||'wdgovover'||'wgovmindex'||'wgovoffmap'||'wgovposter') {
 		$pattern = '//';
 		$replace = 'GOV WITHDRAW';
+	} else {
+		$pattern = '//';
+		$replace = 'Send to Problem Shelf';
 	}
 } else if ($item['statistics_note_3'] !== 'KU FDLP REQUEST') {
 	$pattern = '/(ANNEX ingest)|(HALE return)|(To be WITHDRAWN)|(AHD ANNEX ingest)|(AHD HALE return)|(AHD To be WITHDRAWN)|(GOV UNBOXING review)|(PHYSICAL CONDITION REVIEW)|(Needs pam binder)/';
