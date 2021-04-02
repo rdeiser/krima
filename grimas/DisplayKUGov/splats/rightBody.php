9a8d78e9efe068dfd03ef66bbd8eb9c977211334
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-04-02 07:41:45 -0500

    Update rightBody.php

--- grimas/DisplayKUGov/splats/rightBody.php
+++ grimas/DisplayKUGov/splats/rightBody.php
@@ -16,9 +16,18 @@ if ($item['statistics_note_3'] == '') {
 	$pattern = '/(ANNEX ingest)|(HALE return)|(To be WITHDRAWN)|(AHD ANNEX ingest)|(AHD HALE return)|(AHD To be WITHDRAWN)|(GOV UNBOXING review)|(PHYSICAL CONDITION REVIEW)|(Needs pam binder)/';
 	$replace = 'Send to Problem Shelf';
 }
+
+if ($item['description']=='') {
+	$style4 = 'style="opacity:0;"';
+} else {
+	$style4 = 'style="opacity:1;"';
+	$pattern2 = '/^/';
+	$replace2 = '&nbsp;';
+}
 ?>
               <table class="table">
 				<tr><th class="flip"><span>Barcode:</span><span>Código de barras:<!--Código de procedencia:--></span></th><td><?=$e($item['barcode'])?></td></tr>
+				<tr><th class="flip"><span>Call Number:</span><span>Número de clasificación:</span></th><td><?=$e ($item['call_number'])?><text <?=$style4?>><?= preg_replace($pattern2, $replace2, $item['description'])?></text><text <?=$style3?>>&nbsp;c.<?=$e($item['copy_id'])?></text></td></tr>
 				<tr><th class="flip"><span>Inventory Date:</span><span>Fecha de inventario:</span></th><td><?=$e($item['inventory_date'])?></td></tr>
 				<tr><th class="flip"><span>Destination:</span><span>Destino:</span></th><td class="statnote"><?= preg_replace($pattern, $replace, $item['statistics_note_3'])?>
 				</td></tr>
