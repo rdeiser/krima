7796f5acfa379daf1940837fe796c3c46e8c0d01
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-04-28 12:52:29 -0500

    New Grima

---
+++ grimas/DisplayGovKeep/DisplayGovKeep.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("../grima-lib.php");
+
+class DisplayGovKeep extends GrimaTask {
+
+	function do_task() {
+		{$item = new Item();
+		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$item->addInventoryDate(date("Y-m-d"));
+		$item->updateAlma();
+		}
+{
+		$this->item = new Item();
+		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$this->splatVars['item'] = $this->item;
+}
+}
+}
+
+DisplayGovKeep::RunIt();
