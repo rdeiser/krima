474aba26f1a3beb5bc1041f564c1ec29e7030413
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-04-28 12:59:30 -0500

    Update Location

--- grimas/DisplayGovKeep/splats/rightBody.php
+++ grimas/DisplayGovKeep/splats/rightBody.php
@@ -1,121 +1,13 @@
 <?php
 if ($item['statistics_note_3'] == 'HALE return') {
-	$pattern = '/(HALE return)/';
-	$replace = 'HALE return';
+	if($item['location_code'] == 'gov'||'govcen'||'govelect'||'govmap'||'govmfile'||'govmic'||'govover'||'govref'||'govmindex'||'govoffmap'||'govposter') {
+		$pattern = '/(HALE return)/';
+		$replace = 'GOV HALE return';
+	} else
+		$pattern = '/(HALE return)/';
+		$replace = 'Send to Problem Shelf';
 } 
-if ($item['statistics_note_3'] == 'ANNEX ingest') {
-	$pattern = '/(ANNEX ingest)/';
-	$replace = 'ANNEX ingest';
-}
-if ($item['statistics_note_3'] == 'To be WITHDRAWN') {
-	$pattern = '/(To be WITHDRAWN)/';
-	$replace = 'To be WITHDRAWN';
-}
-if ($item['statistics_note_3'] == 'AHD HALE return') {
-	$pattern = '/(AHD HALE return)/';
-	$replace = 'HALE return';
-} 
-if ($item['statistics_note_3'] == 'AHD ANNEX ingest') {
-	$pattern = '/(AHD ANNEX ingest)/';
-	$replace = 'ANNEX ingest';
-}
-if ($item['statistics_note_3'] == 'AHD To be WITHDRAWN') {
-	$pattern = '/(AHD To be WITHDRAWN)/';
-	$replace = 'To be WITHDRAWN';
-}
-if ($item['statistics_note_3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdraw') {
-	$pattern = '/(PHYSICAL CONDITION REVIEW For Possible Withdraw)/';
-	$replace = 'Send to Condition Review Shelf';
-}
-if ($item['statistics_note_3'] == '') {
-	$pattern = '//';
-	$replace = 'Send to Problem Shelf';
-}
 
-/*if ($item['location'] == 'juv') {
-	if ($item['statistics_note_3'] == 'HALE return') {
-		$pattern = '/(HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'cmc') {
-	if ($item['statistics_note_3'] == 'HALE return') {
-		$pattern = '/(HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location_code'] == 'main') {
-	if ($item['statistics_note_3'] == 'HALE return') {
-		$pattern = '/(HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'over') {
-	if ($item['statistics_note_3'] == 'HALE return') {
-		$pattern = '/(HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'overplus') {
-	if ($item['statistics_note_3'] == 'HALE return') {
-		$pattern = '/(HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'dowref') {
-	if ($item['statistics_note_3'] == 'HALE return') {
-		$pattern = '/(HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'ref') {
-	if ($item['statistics_note_3'] == 'HALE return') {
-		$pattern = '/(HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'juv') {
-	if ($item['statistics_note_3'] == 'AHD HALE return') {
-		$pattern = '/(AHD HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'cmc') {
-	if ($item['statistics_note_3'] == 'AHD HALE return') {
-		$pattern = '/(AHD HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'main') {
-	if ($item['statistics_note_3'] == 'AHD HALE return') {
-		$pattern = '/(AHD HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'over') {
-	if ($item['statistics_note_3'] == 'AHD HALE return') {
-		$pattern = '/(AHD HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'overplus') {
-	if ($item['statistics_note_3'] == 'AHD HALE return') {
-		$pattern = '/(AHD HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'dowref') {
-	if ($item['statistics_note_3'] == 'AHD HALE return') {
-		$pattern = '/(AHD HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}
-if ($item['location'] == 'ref') {
-	if ($item['statistics_note_3'] == 'AHD HALE return') {
-		$pattern = '/(AHD HALE return)/';
-		$replace = 'Send to Problem Shelf';
-		}
-	}*/
 //Following php color codes the Process type if it is populated
 if ($item['process_type']=='') {
 		$style = 'style=";"';
