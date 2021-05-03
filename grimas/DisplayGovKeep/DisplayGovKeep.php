1899290e420934eb84ded159adc526daa3e4f1a7
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-03 11:59:52 -0500

    Update DisplayGovKeep.php

--- grimas/DisplayGovKeep/DisplayGovKeep.php
+++ grimas/DisplayGovKeep/DisplayGovKeep.php
@@ -5,33 +5,21 @@ require_once("../grima-lib.php");
 class DisplayGovKeep extends GrimaTask {
 
 	function do_task() {
-		{
-		$item = new Item();
+		{$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		$item->addInventoryDate(date("Y-m-d"));
 		$item->updateAlma();
 		}
-		{
-		if ($item['location_code'] = 'govcen'||'govelect'||'govover'||'govref'||'govposter') {
+		{if($item['location_code'] == 'govcen'||'govelect'||'govover'||'govref'||'govposter') {
 			$item['location_code'] = 'gov';
-			$item['item_policy'] = 'book/ser';
+			$item->updateAlma();
 		}
-		$item->updateAlma();
 		}
-		/*if ($item['item_policy'] !== 'book/ser') {
-			if ($item['statistics_note_3'] == 'HALE return') {
-				$item = new Item();
-				$item->loadFromAlmaBarcode($this['unboxed_barcode']);
-				if ($item['location_code'] == 'govcen'||'govelect'||'govover'||'govref'||'govposter') {
-					$item['location_code'] = 'gov';
-					$item->updateAlma();
-				}
-			}
-		}*/
 		{
 		$this->item = new Item();
 		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		$this->splatVars['item'] = $this->item;
+		}
 		}
 	}
 }
