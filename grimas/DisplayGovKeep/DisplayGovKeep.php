3c1239f6bd76b40f3f22fc84e2580ba994f8c2d1
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-03 09:41:42 -0500

    Update DisplayGovKeep.php

--- grimas/DisplayGovKeep/DisplayGovKeep.php
+++ grimas/DisplayGovKeep/DisplayGovKeep.php
@@ -5,57 +5,40 @@ require_once("../grima-lib.php");
 class DisplayGovKeep extends GrimaTask {
 
 	function do_task() {
-		{$item = new Item();
+		$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		$item->addInventoryDate(date("Y-m-d"));
+		if ($item['location_code'] == 'govcen'||'govelect'||'govover'||'govref'||'govposter') {
+			$item['location_code'] = 'gov';
+			$item['item_policy'] = 'book/ser';
+			$item->updateAlma();
+		}
 		if ($item['item_policy'] !== 'book/ser') {
 			if ($item['statistics_note_3'] == 'HALE return') {
 				if($item['location_code'] == 'govcen'||'govelect'||'govover'||'govref'||'govposter') {
-					{
-						$holding = new Holding ();
-						$holding->getItemList();
-						if (count($holding->itemList->items) = 1 {
-							$holding->loadFromAlma($item['mms_id'],$item['holding_id']);
-							if ($holding['location_code'] == 'govcen'||'govelect'||'govover'||'govref'||'govposter') {
-								$holding['location_code'] = 'gov';
-								}
-								$holding->updateAlma();
-				} else {
-					$item = new Item();
-					$item->loadFromAlmaBarcode($this['unboxed_barcode']);
-					if ($item['location_code'] == 'govcen'||'govelect'||'govover'||'govref'||'govposter') {
-						$item['location_code'] = 'gov';
-						$item['item_policy'] = 'book/ser';
-						$item->update
-						}
+					$holding = new Holding ();
+					$holding->getItemList();
+					if (count($holding->itemList->items) = 1 {
+						$holding->loadFromAlma($item['mms_id'],$item['holding_id']);
+						$holding['location_code'] = 'gov';
+						$holding->updateAlma();
+					}
 				}
-		}
-		}
-}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			}
+		} else {
+			$item = new Item();
+			$item->loadFromAlmaBarcode($this['unboxed_barcode']);
 			if ($item['location_code'] == 'govcen'||'govelect'||'govover'||'govref'||'govposter') {
 				$item['location_code'] = 'gov';
-				$item['item_policy'] = 'book/ser';
+				$item->updateAlma();
 			}
 		}
-		$item->updateAlma();
-		}
-{
+		
 		$this->item = new Item();
 		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		$this->splatVars['item'] = $this->item;
+	}
 }
-}
-}
+
 
 DisplayGovKeep::RunIt();
