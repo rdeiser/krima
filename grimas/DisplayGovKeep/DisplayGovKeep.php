bf76b02bd45891e37b469b9acf0b0be11fe7f878
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-03 10:49:28 -0500

    Update DisplayGovKeep.php

--- grimas/DisplayGovKeep/DisplayGovKeep.php
+++ grimas/DisplayGovKeep/DisplayGovKeep.php
@@ -8,7 +8,7 @@ class DisplayGovKeep extends GrimaTask {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		$item->addInventoryDate(date("Y-m-d"));
-		if ($item['location_code'] == 'govcen'or'govelect') {
+		if ($item['location_code'] == 'govcen'||'govelect'||'govover'||'govref'||'govposter') {
 			$item['location_code'] = 'gov';
 			$item['item_policy'] = 'book/ser';
 			$item->updateAlma();
