7faf9b5e2aa682a78104777634876642b59052dc
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-04 15:13:18 -0500

    Update rightBody.php

--- grimas/DisplayGovKS/splats/rightBody.php
+++ grimas/DisplayGovKS/splats/rightBody.php
@@ -32,10 +32,31 @@ if ($item['statistics_note_3'] == '') {
 	$replace = 'Send to Problem Shelf';
 }
 
-if ($item['location'] !== 'govks' || 'govksover' || 'govks') {
-	if ($item['statistics_note_3'] == 'HALE return') {
-		$pattern = '/(HALE return)/';
-		$replace = 'Send to Problem Shelf';
+if ($item['location'] !== 'govks') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		} else if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}else if ($item['location'] !== 'govksover') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		} else if ($item['statistics_note_3'] == 'AHD HALE return') {
+			$pattern = '/(AHD HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
+		}
+	}else if ($item['location'] !== 'govksref') {
+		if ($item['statistics_note_3'] == 'HALE return') {
+			$pattern = '/(HALE return)/';
+			$replace = 'Send to Problem Shelf';
+			$style = 'style=";"';
 		} else if ($item['statistics_note_3'] == 'AHD HALE return') {
 			$pattern = '/(AHD HALE return)/';
 			$replace = 'Send to Problem Shelf';
