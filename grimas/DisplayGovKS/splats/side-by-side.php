99bf0d4a8372d78a37fa1fc971e933f4a70120a1
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-05 09:09:23 -0500

    Update side-by-side.php

--- grimas/DisplayGovKS/splats/side-by-side.php
+++ grimas/DisplayGovKS/splats/side-by-side.php
@@ -86,17 +86,7 @@ if ($item['statistics_note_3']=='To be WITHDRAWN') {
 			$style = 'style=";"';
 			//$text = 'Send to Problem Shelf';
 	}
-	if ($item['location'] !== 'govks' or 'govksover' or 'govrefks') {
-		if ($item['statistics_note_3'] == 'HALE return') {
-			$pattern = '/(HALE return)/';
-			$replace = 'Send to Problem Shelf';
-			$style = 'style=";"';
-		} else if ($item['statistics_note_3'] == 'AHD HALE return') {
-			$pattern = '/(AHD HALE return)/';
-			$replace = 'Send to Problem Shelf';
-			$style = 'style=";"';
-		}
-	}
+
 	$holding = new Holding();
 	$holding->loadFromAlma($item['mms_id'],$item['holding_id']);
 	if ($holding['suppress_from_publishing'] == 'true'){
