cb127ef6d47bf0bb716fe821289a613cf0da8580
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-05 08:46:45 -0500

    Update rightBody.php

--- grimas/DisplayGovKS/splats/rightBody.php
+++ grimas/DisplayGovKS/splats/rightBody.php
@@ -1,13 +1,22 @@
 <?php
 if ($item['statistics_note_3'] == 'HALE return') {
-	if ($item['location'] !== 'govks'||'govrefks') {
+	if ($item['location'] !== 'govks') {
 		$pattern = '/(HALE return)/';
 		$replace = 'Send to Problem Shelf';
 	} else {
 		$pattern = '/(HALE return)/';
 		$replace = 'HALE return';
 	}
-} 
+} else if ($item['statistics_note_3'] == 'HALE return') {
+	if ($item['location'] !== 'govrefks') {
+		$pattern = '/(HALE return)/';
+		$replace = 'Send to Problem Shelf';
+	} else {
+		$pattern = '/(HALE return)/';
+		$replace = 'HALE return';
+	}
+}
+
 if ($item['statistics_note_3'] == 'ANNEX ingest') {
 	$pattern = '/(ANNEX ingest)/';
 	$replace = 'ANNEX ingest';
