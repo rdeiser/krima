987596484fac3ed8e11f1ae14fe9fe3adf0a5386
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-13 13:38:30 -0500

    New Grima

---
+++ grimas/AnnexWork/AnnexWork.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("../grima-lib.php");
+
+class AnnexWork extends GrimaTask {
+
+	function do_task() {
+		{$item = new Item();
+		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$item->addInventoryDate(date("Y-m-d"));
+		$item->updateAlma();
+		}
+		{$holding = new Holding();
+		$holding->loadFromAlma($item['mms_id'],$item['holding_id']);
+		$holding['library_code'] = 'ANNEX';
+		$holding['location_code'] = 'ANNEX';
+		
+		if ($leader[5] == '') {
+			$leader[5] = 'c';
+		}
+		$holding->updateAlma();
+		/*
+		appendField($tag,$ind1,$ind2,$subfields)
+		$holding->appendField("014","","",array('a' => content));
+		deleteField($tag)
+		
+		$leader[6]
+		leader? may need to develop this within grima-lib.php similar to inventory_date?*/
+		}
+{
+		$this->item = new Item();
+		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
+		$this->splatVars['item'] = $this->item;
+}
+}
+}
+
+AnnexWork::RunIt();
