da33af00687f89fa2c70aab4511c3853da0ab5d1
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-14 12:26:52 -0500

    Update AnnexWork.php

--- grimas/AnnexWork/AnnexWork.php
+++ grimas/AnnexWork/AnnexWork.php
@@ -16,9 +16,7 @@ class AnnexWork extends GrimaTask {
 		if ($holding['location_code'] == 'main') {
 			$holding->deleteControlField("001");
 			$holding->deleteControlField("004");
-			$holding->appendField("852","0","0",array('b'));
-			//$holding->appendFieldfirstind("852","0");
-			//$holding->appendFieldsecondind("852","0");
+			$holding->setFieldindicators("852","0","0");
 			$holding['library_code'] = 'ANNEX';
 			$holding['location_code'] = 'annex';
 		}
