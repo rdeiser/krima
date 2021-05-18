c5e62bd8da3dd593e56e4a67a78c09b50d1b3b14
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-18 14:32:11 -0500

    Update grima-lib.php

--- grimas/grima-lib.php
+++ grimas/grima-lib.php
@@ -1849,13 +1849,13 @@ class AlmaObjectWithMARC extends AlmaObject {
 		$chr5s = $xpath->query("//record/leader");
 		foreach ($chr5s as $chr5) {
 			$chr5 = substr_replace($chr5,$chr,5,1)
+		}
+	}
 			//$xpath->query("//record/leader")->item(0)->setAttribute("leader",$chr5);
 			//$chr5->setAttribute($chr5);
-		}
 		/*might just be able to use the regular expression \s without a global.  Do if \s then replace with $chr.  Double check deletefield if matches coding to double check this option.
 		$chr5s = $xpath->query("//record/leader").pattern;
 		$xpath->query("//record/leader")->item(0)->setAttribute($chr);*/
-	}
 // }}}
 
 // {{{ AlmaObjectWithMARC -> setFieldindicators
