119d7537f020cd6fd37b99b6d342b4f6e2e40b51
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-05-27 11:47:42 -0500

    Update AnnexWork.php

--- grimas/AnnexWork/AnnexWork.php
+++ grimas/AnnexWork/AnnexWork.php
@@ -21,14 +21,10 @@ class AnnexWork extends GrimaTask {
 				$holding->deleteSubfieldMatching("014","9","/[0-9]?/");
 				$holding->deleteSubfieldMatching("014","a","/^[A-z]/");
 				//$holding->setHldr("c","x","2","n");
-				{$holding->setHldr5("c");
-				$holding->updateAlma();}
-				{$holding->setHldr6("x");
-				$holding->updateAlma();}
-				{$holding->setHldr17("2");
-				$holding->updateAlma();}
-				{$holding->setHldr18("n");
-				$holding->updateAlma();}
+				//$holding->setHldr5("c");
+				//$holding->setHldr6("x");
+				$holding->setHldr17("2");
+				//$holding->setHldr18("n");
 				/*$holding->setH008("2","8","4","001","a","a","   ","0");
 				$holding->set008p6("2");
 				$holding->set008p7("u");
