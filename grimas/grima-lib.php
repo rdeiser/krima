1444e8160f1b83198fc65084b87e8aad07acc804
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-06-02 13:23:07 -0500

    Update grima-lib.php

--- grimas/grima-lib.php
+++ grimas/grima-lib.php
@@ -1892,7 +1892,7 @@ class AlmaObjectWithMARC extends AlmaObject {
 		$replace6=substr_replace($replace5,$chr21,21,1);
 		$replace7=substr_replace($replace6,$chr22,22,3);
 		$replace8=substr_replace($replace7,$chr25,25,1);
-		$chr->parentNode->removeChild($chr);
+		//$chr->parentNode->removeChild($chr);
 		//appendInnerXML($chr,$replace8);
 		$add = $this->xml->createElement("controlfield");
 		$add->setAttribute("tag","008");
