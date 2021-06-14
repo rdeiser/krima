ada1b33810653af215dfc5503780f0c6aa9ac3ff
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-06-14 15:15:06 -0500

    Update rightBody.php

--- grimas/DisplayCD/splats/rightBody.php
+++ grimas/DisplayCD/splats/rightBody.php
@@ -152,7 +152,7 @@ if ($holding['suppress_from_publishing'] == 'true'){
 ?>
               <table class="table">
                 <tr><th class="flip"><span>Title:</span><span>Título:</span></th><td><?=$e($item['title'])?></td></tr>
-				<tr><th>Title Proper</th><td><?= $bib->get_title_proper() ?><?= $bib->get_title_n() ?><?= $bib->get_title_p() ?></td></tr>
+				<tr><th>Title Proper</th><td><?= $bib->get_title_proper() ?> <?= $bib->get_title_n() ?> <?= $bib->get_title_p() ?></td></tr>
 				<tr><th class="flip"><span>Call Number:</span><span>Número de clasificación:</span></th><td><?= preg_replace($pattern4, $replace4, $item['call_number'])?><text <?=$style4?>><?= preg_replace($pattern2, $replace2, $item['description'])?></text><text <?=$style3?>>&nbsp;c.<?=$e($item['copy_id'])?></text></td></tr>
 				<!--<tr><th class="flip"><span>Description:</span><span>Descripción:</span></th><td><?=$e($item['description'])?></td></tr>-->
 				<tr><th class="flip"><span>Barcode:</span><span>Código de barras:<!--Código de procedencia:--></span></th><td><?=$e($item['barcode'])?></td></tr>
