f6d0f4afa4ccc4b8ad7cca4a3efc875c79ce9611
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-06-17 11:57:53 -0500

    Update grima-lib.php

--- grimas/grima-lib.php
+++ grimas/grima-lib.php
@@ -2605,7 +2605,7 @@ class Bib extends AlmaObjectWithMARC {
 		$xpath = new DomXpath($this->xml);
 		$fields = $xpath->query("//record/datafield[@tag='245']");
 		//return ($fields[0]->nodeValue);
-		$space = preg_replace("/(\[sound recording\])\W|(\[sound recording\])|\/[A-Za-z0-9].+/","",$fields[0]->nodeValue);
+		$space = preg_replace("/(\[sound recording\])\W|(\[sound recording\])|\/[A-Za-z0-9].+$/","",$fields[0]->nodeValue);
 		return preg_replace("/\./|\:"," ",$space);
 		//return preg_replace("/[ \/=:,;\.]*$/","",$fields[0]->nodeValue);
 		/*foreach ($fields as $field) {
