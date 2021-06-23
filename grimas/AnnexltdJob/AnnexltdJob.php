79c8449db94cbda2b490832b9e0402e83d227c1b
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-06-23 08:59:27 -0500

    Update AnnexltdJob.php

--- grimas/AnnexltdJob/AnnexltdJob.php
+++ grimas/AnnexltdJob/AnnexltdJob.php
@@ -5,8 +5,9 @@ require_once("../grima-lib.php");
 class AnnexltdJob extends GrimaTask {
 
 	function do_task() {
+		//global $grima;
 		$job = new Job();
-		$job->jobScheduled591();
+		Job::jobScheduled591();
 		$this->addMessage('success',"Alma Job is running");
 	}
 }
