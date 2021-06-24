7e9ab94dfd945438eee34f1809652f87c2d9d116
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-06-24 10:21:25 -0500

    Update grima-lib.php

--- grimas/grima-lib.php
+++ grimas/grima-lib.php
@@ -1179,7 +1179,7 @@ class Grima {
 	function postJob($job_id,$op) {
 		$body = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?' .'>
 <job link="string">
-	<creator>rdeiser</creator>
+	<creator></creator>
 	<next_run>2024-05-30T09:30:10Z</next_run>
 		<parameters>
 			<parameter>
