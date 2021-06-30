33b3ca68f3e4a1e93e90acb5dba5c9b5222cd20e
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-06-30 11:00:31 -0500

    Update AnnexltdJob.php

--- grimas/AnnexltdJob/AnnexltdJob.php
+++ grimas/AnnexltdJob/AnnexltdJob.php
@@ -10,7 +10,8 @@ class AnnexltdJob extends GrimaTask {
 		$job->runAlmaJob('M17242306340002401','run');
 		$this->addMessage('success',"Alma Job is running: {$job['additional_info']}");
 		$msg = $job['additional_info'];
-		mail("rdeiser@ksu.edu","Annexlt Grima",$msg);
+		$msg = wordwrap($msg, 70, "\r\n");
+		mail('rdeiser@ksu.edu','Annexlt Grima',$msg);
 		//regex to remove data before and after the Alma Job Number ^(Job no)\.\s|(\s(triggered)\s(on)\s(\w){3}\D\s\d{2}\s(\w){3}\s\d{4}\s(\d{2}\:\d{2}\:\d{2})\s(GMT))
 	}
 }
