0ae87ae3c1dcee0e9e583d96a47a1cbe11cc3663
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-07-14 11:26:19 -0500

    Update Boundwith.php

--- grimas/Boundwith/Boundwith.php
+++ grimas/Boundwith/Boundwith.php
@@ -58,6 +58,7 @@ class Boundwith extends GrimaTask {
 		## HOLDING
 		$this->biblist[0]->getHoldings();
 		if (count($bib->holdings) = 0) {
+			continue;
 			$holding = new Holding();
 			$holding->appendField("014","1"," ",array('x' => 'BOUNDWITH'));
 			$holding['library_code'] = 'MAIN';
