92a9ab3adb43a52e64315fc9424e9a5f935e9ec1
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-08-16 11:06:39 -0500

    Update DeleteItem.php

--- grimas/DeleteItem/DeleteItem.php
+++ grimas/DeleteItem/DeleteItem.php
@@ -12,10 +12,12 @@ class DeleteItem extends GrimaTask {
 				$item['statistics_note_1'] = 'WITHDRAWN';
 				$item->updateAlma();
 			}
-		$item->deleteFromAlma();
-		$this->addMessage('success',"Deleted item {$this['item']} with Statistics Note 1 of {$item['statistics_note_1']} and Statistics Note 3 of {$item['statistics_note_3']}");
-		} else {
-			$this->addMessage('error',"Item Record for {$item['barcode']} has Statistics Note 1 of {$item['statistics_note_1']} and Statistics Note 3 of {$item['statistics_note_3']}");
+		}
+		if ($item['statistics_note_1'] = 'WITHDRAWN') {
+			$item->deleteFromAlma();
+			$this->addMessage('success',"Deleted item {$this['item']} with Statistics Note 1 of {$item['statistics_note_1']} and Statistics Note 3 of {$item['statistics_note_3']}");
+			} else {
+				$this->addMessage('error',"Item Record for {$item['barcode']} has Statistics Note 1 of {$item['statistics_note_1']} and Statistics Note 3 of {$item['statistics_note_3']}");
 		}
 	}
 }
