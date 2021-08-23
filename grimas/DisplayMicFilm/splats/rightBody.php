8a96b95179c32459da8ad0b170a558462ab2a171
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-08-23 08:54:44 -0500

    Update rightBody.php

--- grimas/DisplayMicFilm/splats/rightBody.php
+++ grimas/DisplayMicFilm/splats/rightBody.php
@@ -31,7 +31,7 @@ if ($item['statistics_note_3'] == '') {
 	$pattern = '//';
 	$replace = 'Send to Problem Shelf';
 }
-if ($item['location'] !== 'microfilm') {
+if ($item['location'] !== 'microfilm' && $item['call_number'] = 'MICROFILM NEWSPAPER') {
 	if ($item['statistics_note_3'] == 'HALE return') {
 		$pattern = '/(HALE return)/';
 		$replace = 'Send to Problem Shelf';
