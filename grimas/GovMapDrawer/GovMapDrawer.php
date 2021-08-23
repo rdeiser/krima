8c24e3ad4f9339c0ef41e85063645e6f8c4f9888
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-08-23 10:01:15 -0500

    Update GovMapDrawer.php

--- grimas/GovMapDrawer/GovMapDrawer.php
+++ grimas/GovMapDrawer/GovMapDrawer.php
@@ -21,33 +21,8 @@ class GovMapDrawer extends GrimaTask {
 					$this->addMessage('error',"Did not update map drawer number for {$holdingid}");
 					continue;
 				}
-				if ($holding['location_code'] == $this['whichnote'] && $holding->HoldingsListEntry['call_number'] = 'Drawer 104'){
-					$holding->setCallNumber('Drawer 346','','8');
-					$holding->updateAlma();
-				}
-				if ($holding['location_code'] == $this['whichnote'] && $holding->HoldingsListEntry['call_number'] = 'Drawer 105'){
-					$holding->setCallNumber('Drawer 347','','8');
-					$holding->updateAlma();
-				}
-					}
-				}
-				//$this->addMessage('success',"Successfully updated map drawer number for {$holdingid} to: STOP");
-				//$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with Barcode: {$item['barcode']}");
-				/*function print_success() {
-    do_redirect('../WithdrawLibrary/WithdrawLibrary.php?holding_id=' . $this['holding_id']);
-}*/
 			}
 		}
-		/*foreach ($this->holdinglist as $holding) {
-			$count = count($holding->itemList->items, COUNT_RECURSIVE);
-		}
-		
-		$this->addMessage('success',"Total Number of Items Added {$count}");*/
-		//$this->holding->getItems();
-		//$this->splatVars['holding'] = $this->holding;
-		//$this->splatVars['width'] = 12;
-		//$this->splatVars['holdinglist'] = $this->holdinglist;
-		//$this->splatVars['body'] = array( 'holding', 'messages' );
 	}
 }
 GovMapDrawer::RunIt();
