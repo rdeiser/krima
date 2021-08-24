a5bdd9c94e4ae5418857b90b6c1703166dfb4763
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-08-24 13:04:32 -0500

    Update GovMapDrawer.php

--- grimas/GovMapDrawer/GovMapDrawer.php
+++ grimas/GovMapDrawer/GovMapDrawer.php
@@ -12,18 +12,8 @@ class GovMapDrawer extends GrimaTask {
 			$holding = new Holding();
 			$this['mms_id'] = $holdingid;
 			if ($this['mms_id']) {
-				//$holding->loadFromAlma($this['mms_id'],$holdingid);
-				$holding->loadFromAlmaX($holdingid);
-				$xpath = new DomXpath ($this->xml);
-				$field852 = $xpath->query("//record/datafield[@tag='852']")->item(0);
-				$subfieldCs = $xpath->query("subfield[@code='c']",$field852);
-				$subfieldHs = $xpath->query("subfield[@code='h']",$field852);
-				$this['newdrawer'] = $h;
-				if ($subfieldC->nodeValue = $this['whichnote'] && $subfieldHs->nodeValue = $this['olddrawer']) {
-					$subfieldHs->parentNode->removeChild($subfieldHs);
-					
-					appendInnerXML($field852,"<subfield code=\"h\">$h</subfield>");
-				}
+				$holding->loadFromAlma($this['mms_id'],$holdingid);
+				$holding->setMapCallNumber($this['whichnote'],$this['olddrawer'],$this['newdrawer'],'8','0')
 				$this->addMessage('success',"Successfully updated map drawer number for {$holdingid}");
 			} else {
 				$this->addMessage('error',"Did not update map drawer number for {$holdingid}");
