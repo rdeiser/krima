5a7449a8633d6b89327c6a84bd4e03ed7e54a1fd
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-08-24 11:53:15 -0500

    Update GovMapDrawer.php

--- grimas/GovMapDrawer/GovMapDrawer.php
+++ grimas/GovMapDrawer/GovMapDrawer.php
@@ -17,7 +17,7 @@ class GovMapDrawer extends GrimaTask {
 					$xpath = new DomXpath ($this->xml);
 					$callnumber = $xpath->query("//record/datafield[@tag='852']/subfield[@code='h']");
 					if ($holding['location_code'] = $this['whichnote'] && $callnumber = $this['olddrawer']) {
-						$holding->setCallNumber('$this['newdrawer]','','8');
+						$holding->setCallNumber('$this['newdrawer']','','8');
 						$holding->updateAlma();
 					}
 				$this->addMessage('success',"Successfully updated map drawer number for {$holdingid}");
