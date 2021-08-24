d847201ebbca2379c8334859f6ad4978f7a7bf5a
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-08-24 12:46:22 -0500

    Update GovMapDrawer.php

--- grimas/GovMapDrawer/GovMapDrawer.php
+++ grimas/GovMapDrawer/GovMapDrawer.php
@@ -18,10 +18,11 @@ class GovMapDrawer extends GrimaTask {
 				$field852 = $xpath->query("//record/datafield[@tag='852']")->item(0);
 				$subfieldCs = $xpath->query("subfield[@code='c']",$field852);
 				$subfieldHs = $xpath->query("subfield[@code='h']",$field852);
+				$this['newdrawer'] = $h;
 				if ($subfieldC->nodeValue = $this['whichnote'] && $subfieldHs->nodeValue = $this['olddrawer']) {
 					$subfieldHs->parentNode->removeChild($subfieldHs);
-					appendInnerXML($field852,"<subfield code=\"h\">$this['newdrawer']</subfield>");
-					//appendInnerXML($field852,"<subfield code=\"h\">$h</subfield>");
+					
+					appendInnerXML($field852,"<subfield code=\"h\">$h</subfield>");
 				}
 				$this->addMessage('success',"Successfully updated map drawer number for {$holdingid}");
 			} else {
