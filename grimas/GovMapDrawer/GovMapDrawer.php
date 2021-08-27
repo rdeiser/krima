1ee58e53dea1ca5fc3d9d40522e3a7092f637c1e
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-08-27 09:50:59 -0500

    Update GovMapDrawer.php

--- grimas/GovMapDrawer/GovMapDrawer.php
+++ grimas/GovMapDrawer/GovMapDrawer.php
@@ -13,7 +13,7 @@ class GovMapDrawer extends GrimaTask {
 			$this['mms_id'] = $holdingid;
 			if ($this['mms_id']) {
 				$holding->loadFromAlma($this['mms_id'],$holdingid);
-				if ($this['whichnote'] == $holding->HoldingsListEntry['location_code']) {
+				if ($this['whichnote'] = $holding['location']) {
 					$holding->setMapCallNumber($this['olddrawer'],$this['newdrawer'],'8');
 					$holding->updateAlma();
 					$this->addMessage('success',"Successfully updated map drawer number for {$holdingid}");
