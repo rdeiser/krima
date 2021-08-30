863aa073339bd6703b608c67cd866e0b5e1ba050
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-08-30 12:09:36 -0500

    Update GovMapDrawer.php

--- grimas/GovMapDrawer/GovMapDrawer.php
+++ grimas/GovMapDrawer/GovMapDrawer.php
@@ -23,6 +23,26 @@ class GovMapDrawer extends GrimaTask {
 					$this->addMessage('error',"Did not update map drawer number for {$holdingid}");
 					continue;
 					}
+				if ($holding['call_number'] == ' Drawer 237 ') {
+					$holding->setMapCallNumber($this['whichnote'],' Drawer 237 ','Drawer 394','8');
+				
+				//$holding->setMapCallNumber($this['whichnote'],$this['olddrawer'],$this['newdrawer'],'8');
+					$holding->updateAlma();
+					$this->addMessage('success',"Successfully updated map drawer number for {$holdingid} with {$holding['call_number']}");
+				} else {
+					$this->addMessage('error',"Did not update map drawer number for {$holdingid}");
+					continue;
+					}
+				if ($holding['call_number'] == ' Drawer 236 ') {
+					$holding->setMapCallNumber($this['whichnote'],' Drawer 238 ','Drawer 395','8');
+				
+				//$holding->setMapCallNumber($this['whichnote'],$this['olddrawer'],$this['newdrawer'],'8');
+					$holding->updateAlma();
+					$this->addMessage('success',"Successfully updated map drawer number for {$holdingid} with {$holding['call_number']}");
+				} else {
+					$this->addMessage('error',"Did not update map drawer number for {$holdingid}");
+					continue;
+					}
 			}
 			}
 		}
