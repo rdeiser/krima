8d0967493279e6b52200f659f0a6af030a78daa7
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-09-29 12:08:24 -0500

    Update PrintHolding.php

--- grimas/PrintHolding/PrintHolding.php
+++ grimas/PrintHolding/PrintHolding.php
@@ -7,13 +7,11 @@ class PrintHolding extends GrimaTask {
 	function do_task() {
 		$this->holding = new Holding();
 		$this['holding_id'] = Holding::getHoldingIDFromCallnumber($this['call_number']);
-		if ($this['holding_id']) {
 		$this->holding->loadFromAlmaX($this['holding_id']);
 		$this->splatVars['width'] = 12;
 		$this->splatVars['marc'] = $this->holding;
 		$this->splatVars['body'] = array( 'marc', 'messages' );
 		$this->splatVars['title'] = "Alma Holding #${this['holding_id']}";
-		}
 	}
 
 }
