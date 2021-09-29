153613e84a5cd7d4922c045735fa4a00a7aedfa2
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-09-29 14:25:31 -0500

    Update CallnumberSearch.php

--- grimas/CallnumberSearch/CallnumberSearch.php
+++ grimas/CallnumberSearch/CallnumberSearch.php
@@ -6,11 +6,10 @@ class CallnumberSearch extends GrimaTask {
 
 	function do_task() {
 		$this->holding = new Holding();
-		//$this['holding_id'] = Holding::getHoldingIDFromCallnumber($this['callnumber']);
-		$this->holding->getHoldingIDFromCallnumber($this['callnumber']);
-		$mmsid = Holding::getHoldingIDFromCallnumber($this['callnumber']);
+		$this['holding_id'] = Holding::getHoldingIDFromCallnumber($this['callnumber']);
+		//$this->holding->getHoldingIDFromCallnumber($this['callnumber']);
 		if ($this['callnumber']) {
-			$this->holding->loadFromAlma($mmsid,$this->holding->getHoldingIDFromCallnumber($this['callnumber']));
+			$this->holding->loadFromAlma($this['holding_id'],$this['holding_id']);
 			$this->holding->getItems();
 			$this->splatVars['holding'] = $this->holding;
 		} else {
