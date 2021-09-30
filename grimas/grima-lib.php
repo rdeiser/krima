136df628150c11de6489f79b5283583d774363f2
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-09-30 09:20:56 -0500

    Update grima-lib.php

--- grimas/grima-lib.php
+++ grimas/grima-lib.php
@@ -3760,6 +3760,21 @@ xmlns:xsd="http://www.w3.org/2001/XMLSchema">
 	}
 // }}}
 
+public static function getHoldingIDFromCallnumber($callnumber) {
+		global $grima;
+
+		$report = new AnalyticsReport();
+		$report->path = "/shared/Kansas State University/Reports/In progress - Raymond/GRIMA/GRIMA_CALLNUMBER_SEARCH_HOLDING";
+		$report->filter = '<sawx:expr xsi:type="sawx:list" op="beginsWith" xmlns:saw="com.siebel.analytics.web/report/v1.1"  xmlns:sawx="com.siebel.analytics.web/expression/v1.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"  xmlns:xsd="http://www.w3.org/2001/XMLSchema"><sawx:expr xsi:type="sawx:sqlExpression">"Holding Details"."Permanent Call Number"</sawx:expr><sawx:expr xsi:type="xsd:string">{callnumber}</sawx:expr></sawx:expr>';
+	
+		$report->runReport(array('callnumber' => $callnumber), 1);
+		if (count($report->rows) == 1) {
+			return $report->rows[0][1];
+		} else {
+			return null;
+		}
+	}
+
 // {{{ loadFromAlma (get)
 /**
  * @brief populates item record from Alma
