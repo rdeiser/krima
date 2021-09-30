37ecb5c20402596c9ddb4d22b6eb3bfe3d670f4c
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-09-30 09:17:15 -0500

    Update CallnumberSearch.php

--- grimas/CallnumberSearch/CallnumberSearch.php
+++ grimas/CallnumberSearch/CallnumberSearch.php
@@ -5,16 +5,20 @@ require_once("../grima-lib.php");
 class CallnumberSearch extends GrimaTask {
 
 	function do_task() {
-		$this->holding = new Holding();
-		$this['holding_id'] = Holding::getHoldingIDFromCallnumber($this['callnumber']);
-		$this['mms_id'] = Holding::getMMSIDFromCallnumber($this['callnumber']);
-		//$this->holding->getHoldingIDFromCallnumber($this['callnumber']);
+		$this->item = new Item();
+		//$this->holding = new Holding();
+		//$this['holding_id'] = Holding::getHoldingIDFromCallnumber($this['callnumber']);
+		//$this['mms_id'] = Holding::getMMSIDFromCallnumber($this['callnumber']);
+		$itemid = Item::getHoldingIDFromCallnumber($this['callnumber']);
+		$this->item->loadFromAlmaX($itemid);
 		if ($this['callnumber']) {
-			$this->holding->loadFromAlma($this['mms_id'],$this['holding_id']);
+			$this->holding = new Holding();
+			$this->holding->loadFromAlma($item['mms_id'],$item['holding_id']);
+			//$this->holding->loadFromAlma($this['mms_id'],$this['holding_id']);
 			$this->holding->getItems();
 			$this->splatVars['holding'] = $this->holding;
 		} else {
-			GrimaTask::call('ShowItemsFromHoldingsB', array('holding_id' => ($this['holding_id'])));
+			GrimaTask::call('ShowItemsFromHoldingsB', array('holding_id' => ($item['holding_id'])));
 		}
 	}
 }
