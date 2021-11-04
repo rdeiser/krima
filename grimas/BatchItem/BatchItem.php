bb83502d4c07988b87ba145231368c3ddbaa95a2
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-11-04 11:06:44 -0500

    Update BatchItem.php

--- grimas/BatchItem/BatchItem.php
+++ grimas/BatchItem/BatchItem.php
@@ -10,9 +10,29 @@ class BatchItem extends GrimaTask {
 
 		foreach ($this->holdings as $holdingid) {
 			$holding = new Holding();
+			$holding->loadFromAlma($holdingid,$holdingid);
+
+			$item = new Item();
+			$item['fulfillment_note'] = $this['fulnote'];
+			$item['item_policy'] = 'book/ser';
+			$item['pieces'] = '1';
+			$item['inventory_date'] = date("Y-m-d");
+			$item['receiving_operator'] = 'Grima';
+			$item['statistics_note_1'] = 'FIRE 2018 OZONE';
+			$item['statistics_note_3'] = $this['whichnote'];
+			$item->addToAlmaHolding($holdingid,$holdingid);
+			
+			$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with item PID: {$item['item_pid']}");
+			} else {
+				$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
+				//$this->holdinglist[] = $holding;
+			}
+
+		/*foreach ($this->holdings as $holdingid) {
+			$holding = new Holding();
 			$this['mms_id'] = Holding::getMmsFromHoldingID($holdingid);
 			if ($this['mms_id']) {
-				$holding->loadFromAlma($holdingid,$holdingid);
+				$holding->loadFromAlma($this['mms_id'],$holdingid);
 
 				$item = new Item();
 				$item['fulfillment_note'] = $this['fulnote'];
@@ -22,14 +42,14 @@ class BatchItem extends GrimaTask {
 				$item['receiving_operator'] = 'Grima';
 				$item['statistics_note_1'] = 'FIRE 2018 OZONE';
 				$item['statistics_note_3'] = $this['whichnote'];
-				$item->addToAlmaHolding($holdingid,$holdingid);
+				$item->addToAlmaHolding($this['mms_id'],$holdingid);
 				
 				$this->addMessage('success',"Successfully added an Item Record to {$holdingid} with item PID: {$item['item_pid']}");
 			} else {
 				$this->addMessage('error',"Holding Record Suppressed or no longer active in Alma {$holdingid}");
 				//$this->holdinglist[] = $holding;
 			}
-		}
+		}*/
 	}
 }
 BatchItem::RunIt();
