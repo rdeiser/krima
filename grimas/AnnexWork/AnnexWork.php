9e3025cce54f5665109f28fabd5fe951145e5956
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-12-10 15:32:32 -0600

    Update AnnexWork.php

--- grimas/AnnexWork/AnnexWork.php
+++ grimas/AnnexWork/AnnexWork.php
@@ -7,71 +7,55 @@ class AnnexWork extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
-		
-		{if ($item['statistics_note_3'] == 'ANNEX ingest'||$item['statistics_note_3'] == 'AHD ANNEX ingest') {
+		if ($this['location'] == 'annex') {
 			$holding = new Holding();
 			$holding->loadFromAlma($item['mms_id'],$item['holding_id']);
-
-			if ($this['location'] == 'annex') {
-				if ($holding['location_code'] == 'main' || $holding['location_code'] == 'nich') {
-					$holding->deleteControlField("001");
-					$holding->deleteControlField("004");
-					$holding->setFieldindicators("852","0","0");
-					$holding->deleteSubfieldMatching("014","9","/[0-9]?/");
-					$holding->deleteSubfieldMatching("014","a","/^[A-z]/");
-					$holding->setHldr("c","x","2","n");
-					$holding->setH008("2","8","4","001","a","a","   ","0");
-					/*$holding->setHldr5("c");
-					$holding->setHldr6("x");
-					$holding->setHldr17("2");
-					$holding->setHldr18("n");
-					$holding->set008p6("2");
-					$holding->set008p7("u");
-					$holding->set008p12("8");
-					$holding->set008p16("4");
-					$holding->set008p17("001");
-					$holding->set008p20("a");
-					$holding->set008p21("a");
-					$holding->set008p22("###");
-					$holding->set008p25("0");*/
-					$holding['library_code'] = 'ANNEX';
-					$holding['location_code'] = 'annex';
-					$holding->updateAlma();
+			if ($holding['location_code'] == 'main' || $holding['location_code'] == 'nich') {
+				$holding->deleteControlField("001");
+				$holding->deleteControlField("004");
+				$holding->setFieldindicators("852","0","0");
+				$holding->deleteSubfieldMatching("014","9","/[0-9]?/");
+				$holding->deleteSubfieldMatching("014","a","/^[A-z]/");
+				$holding->setHldr("c","x","2","n");
+				$holding->setH008("2","8","4","001","a","a","   ","0");
+				$holding['library_code'] = 'ANNEX';
+				$holding['location_code'] = 'annex';
+				$holding->updateAlma();
 				
 				$item = new Item();
 				$item->loadFromAlmaBarcode($this['unboxed_barcode']);
 				if ($item['item_policy'] !== 'book/ser') {
 					$item['item_policy'] = 'book/ser';
-					//$item->updateAlma();
 				}
 				$item->addInventoryDate(date("Y-m-d"));
 				$item->updateAlma();
 				}
 			}
-			if ($this['location'] == 'govstorks') {
-				if ($holding['location_code'] == 'govks' || $holding['location_code'] == 'govksover' || $holding['location_code'] == 'govrefks') {
-					$holding->deleteControlField("001");
-					$holding->deleteControlField("004");
-					$holding->setFieldindicators("852","8","0");
-					$holding->deleteSubfieldMatching("014","9","/[0-9]?/");
-					$holding->deleteSubfieldMatching("014","a","/^[A-z]/");
-					$holding->setHldr("c","x","2","n");
-					$holding->setH008("2","8","4","001","a","a","   ","0");
-					$holding['library_code'] = 'ANNEX';
-					$holding['location_code'] = 'govstorks';
-					$holding->updateAlma();
+		if ($this['location'] == 'govstorks') {
+			if ($holding['location_code'] == 'govks' || $holding['location_code'] == 'govksover' || $holding['location_code'] == 'govrefks') {
+				$holding = new Holding();
+				$holding->loadFromAlma($item['mms_id'],$item['holding_id']);
+				$holding->deleteControlField("001");
+				$holding->deleteControlField("004");
+				$holding->setFieldindicators("852","8","0");
+				$holding->deleteSubfieldMatching("014","9","/[0-9]?/");
+				$holding->deleteSubfieldMatching("014","a","/^[A-z]/");
+				$holding->setHldr("c","x","2","n");
+				$holding->setH008("2","8","4","001","a","a","   ","0");
+				$holding['library_code'] = 'ANNEX';
+				$holding['location_code'] = 'govstorks';
+				$holding->updateAlma();
 				
 				$item = new Item();
 				$item->loadFromAlmaBarcode($this['unboxed_barcode']);
 				if ($item['item_policy'] !== 'book/ser') {
 					$item['item_policy'] = 'book/ser';
-					//$item->updateAlma();
 				}
 				$item->addInventoryDate(date("Y-m-d"));
 				$item->updateAlma();
 				}
 			}
-		} elseif ($this['location'] == 'annexltd') {
+		if ($this['location'] == 'annexltd') {
 			$holding = new Holding();
 			$holding->loadFromAlma($item['mms_id'],$item['holding_id']);
 			if ($holding['library_code'] == 'SPEC') {
@@ -95,28 +79,22 @@ class AnnexWork extends GrimaTask {
 			if ($item['statistics_note_2'] == '') {
 				$item['statistics_note_2'] = 'FIRE 2018 SPECIAL COLLECTIONS';
 			}
+		}
+		if ($this['location'] == 'KS-Extension') {
+			if ($item['process_type'] = 'MISSING') {
+				if ($item['library'] = 'ANNEX') {
+					$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = 'ANNEX',$circ_desk = 'DEFAULT_CIRC_DESK',$work_order_type = '',$status = '',$done = 'false',$place_on_hold_shelf = 'false',$register_in_house_use = 'false');
+				} elseif ($item['library'] = 'MAIN') {
+					$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = 'MAIN',$circ_desk = 'DEFAULT_CIRC_DESK',$work_order_type = '',$status = '',$done = 'false',$place_on_hold_shelf = 'false',$register_in_house_use = 'false');
+				}
+				}
+			if ($item['item_policy'] !== 'book/ser') {
+				$item['item_policy'] = 'book/ser';
+			}
 			$item->addInventoryDate(date("Y-m-d"));
 			$item->updateAlma();
-			} elseif ($this['location'] == 'KS-Extension') {
-				if ($item['process_type'] = 'MISSING') {
-					if ($item['library'] = 'ANNEX') {
-						$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = 'ANNEX',$circ_desk = 'DEFAULT_CIRC_DESK',$work_order_type = '',$status = '',$done = 'false',$place_on_hold_shelf = 'false',$register_in_house_use = 'false');
-					} elseif ($item['library'] = 'MAIN') {
-						$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = 'MAIN',$circ_desk = 'DEFAULT_CIRC_DESK',$work_order_type = '',$status = '',$done = 'false',$place_on_hold_shelf = 'false',$register_in_house_use = 'false');
-					}
-				}
-				
-				if ($item['item_policy'] !== 'book/ser') {
-					$item['item_policy'] = 'book/ser';
-				}
-				$item->addInventoryDate(date("Y-m-d"));
-				$item->updateAlma();
-				
-			} else {
-				$item->addInventoryDate(date("Y-m-d"));
-				$item->updateAlma();
-				}
-		}
+			
+			}
 {
 		$this->item = new Item();
 		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
