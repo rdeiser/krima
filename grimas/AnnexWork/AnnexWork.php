daf7e566c0a1f18b8ae5f1f37e4f406a6eca4cba
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-12-13 08:50:02 -0600

    Update AnnexWork.php

--- grimas/AnnexWork/AnnexWork.php
+++ grimas/AnnexWork/AnnexWork.php
@@ -27,6 +27,11 @@ class AnnexWork extends GrimaTask {
 					}
 					$item->addInventoryDate(date("Y-m-d"));
 					$item->updateAlma();
+				} else {
+					$item = new Item();
+					$item->loadFromAlmaBarcode($this['unboxed_barcode']);
+					$item->addInventoryDate(date("Y-m-d"));
+					$item->updateAlma();
 				}
 			} else {
 				$item = new Item();
