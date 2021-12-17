0f33f575c31e0349ccd2c14cc220f02ba920fd6a
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-12-17 12:04:59 -0600

    Update leftBody.php

--- grimas/MoreItems/splats/leftBody.php
+++ grimas/MoreItems/splats/leftBody.php
@@ -17,6 +17,10 @@
 					<input class="col-8 form-control" type="text" name="chronology_i" id="chronology_i" size="20" value="<?=$e($item['chronology_i'])?>">
 				  </div>
 				  <div class="form-row col-6 pb-4">
+					<label class="col-4 col-form-label" for="copy_id">Copy id:</label>
+					<input class="col-8 form-control" type="text" name="copy_id" id="copy_id" size="20" value="<?=$e($item['copy_id'])?>">
+				  </div>
+				  <div class="form-row col-6 pb-4">
 					<label class="col-4 col-form-label" for="chronology_j">Chron J:</label>
 					<input class="col-8 form-control" type="text" name="chronology_j" id="chronology_j" size="20" value="<?=$e($item['chronology_j'])?>">
 				  </div>
