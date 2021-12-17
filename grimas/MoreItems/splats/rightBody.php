485cb9ed550c4dbcc6bcdc6c9c3a93594fd44b03
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-12-17 12:02:32 -0600

    Update rightBody.php

--- grimas/MoreItems/splats/rightBody.php
+++ grimas/MoreItems/splats/rightBody.php
@@ -6,6 +6,9 @@
                 <tr><th>Chronology J:</th><td><?=$e($item['chronology_j'])?></td></tr>
                 <tr><th>Description:</th><td><?=$e($item['description'])?></td></tr>
 				<tr><th>Item Policy:</th><td><?=$e($item['item_policy'])?></td></tr>
+				<tr><th>Internal Note 2:</th><td><?=$e($item['internal_note_2'])?></td></tr>
+				<tr><th>Statistics Note 2:</th><td><?=$e($item['statistics_note_2'])?></td></tr>
+				<tr><th>Statistics Note 3:</th><td><?=$e($item['statistics_note_3'])?></td></tr>
                 <tr><th>Title:</th><td><?=$e($item['title'])?></td></tr>
                 <tr><th>Location:</th><td><?=$e($item['location'])?></td></tr>
                 <tr><th>Call Number:</th><td><?=$e($item['call_number'])?></td></tr>
