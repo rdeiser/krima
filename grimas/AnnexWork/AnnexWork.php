b3e3e18346e180c5144717d8a01ba136a963ade9
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2021-12-21 11:14:51 -0600

    Update AnnexWork.php

--- grimas/AnnexWork/AnnexWork.php
+++ grimas/AnnexWork/AnnexWork.php
@@ -4,7 +4,7 @@ class AnnexWork extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
-		if ($this['location'] == 'annex') {
+		if ($this['location'] === 'annex') {
 			if ($item['statistics_note_3'] == 'ANNEX ingest'||$item['statistics_note_3'] == 'AHD ANNEX ingest') {
 				$holding = new Holding();
 				$holding->loadFromAlma($item['mms_id'],$item['holding_id']);
