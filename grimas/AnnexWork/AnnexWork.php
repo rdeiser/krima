3e3ea56c45ff340eabc881945c1dc3c03b4d1975
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2022-01-10 08:12:53 -0600

    Update AnnexWork.php

--- grimas/AnnexWork/AnnexWork.php
+++ grimas/AnnexWork/AnnexWork.php
@@ -118,7 +118,7 @@ class AnnexWork extends GrimaTask {
 					$item['statistics_note_3'] = 'HALE return';
 					$item->addInventoryDate(date("Y-m-d"));
 					$item->updateAlma();
-				} else {
+				} elseif ($item['library'] = 'MAIN') {
 					$item->fulfillmentscan($item['mms_id'],$item['holding_id'],$item['item_pid'],$op = 'scan',$library = 'MAIN',$circ_desk = 'DEFAULT_CIRC_DESK',$work_order_type = '',$status = '',$done = 'false',$place_on_hold_shelf = 'false',$register_in_house_use = 'false');
 					if ($item['item_policy'] !== 'book/ser') {
 						$item['item_policy'] = 'book/ser';
