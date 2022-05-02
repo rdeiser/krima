0f41bfaba2e0b3f5541b50cd8a562e59d7c547b9
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2022-05-02 09:37:27 -0500

    Update side-by-side.php

--- grimas/ScanInReturn/splats/side-by-side.php
+++ grimas/ScanInReturn/splats/side-by-side.php
@@ -93,7 +93,7 @@ if (empty($item['statistics_note_3'])) {
 <!--<h1 class="page-header text-center"><?=$e($title)?></h1>-->
         <div class="row">
           <div class="col">
-            <div class="card" <?=$style?> >
+            <div class="card" <?=$style?>>
               <div class="card-header">
                 <h2 class="card-title"><?=preg_replace($pattern, $replace, $item['statistics_note_3'])?></h2>
               </div>
