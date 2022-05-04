6a1c0bdf80914eb1b55abb6a13bc5d516f4612ab
rdAlma2015 <50148137+rdeiser@users.noreply.github.com>
2022-05-04 12:57:28 -0500

    Update rightBody.php

--- grimas/LusUnboxing/splats/rightBody.php
+++ grimas/LusUnboxing/splats/rightBody.php
@@ -160,13 +160,7 @@ if ($holding['suppress_from_publishing'] == 'true'){
 				<tr><th class="flip"><span>Inventory Date:</span><span>Fecha de inventario:</span></th><td><?=$e($item['inventory_date'])?></td></tr>
 				<!--<tr><th>Inventory Number:</th><td><?=$e($item['inventory_number'])?></td></tr>
 				<tr><th>Internal Note 3:</th><td><?=$e($item['internal_note_3'])?></td></tr>-->
-				<tr><th class="flip"><span>Destination:</span><span>Destino:</span></th><td class="statnote"><?= preg_replace($pattern, $replace, $item['statistics_note_3'])?>
-				</td></tr>
-				<!--<tr><th>Bib Suppressed:</th><td><?=$e($bib['suppress_from_publishing'])?>
-				<tr><th>Holding Suppressed:</th><td><?=$e($holding['suppress_from_publishing'])?>
-				</td></tr>
-				<tr><th>Holding ID:</th><td><?=$e($item['holding_id'])?>
-				</td></tr>-->
+				<tr><th>Destination:</th><td class="statnote"><?=preg_replace($pattern, $replace, $item['statistics_note_3'])?></td></tr>
 				<tr><th></th><td></td></tr>
               </table>
 			  <!--<input class="btn btn-primary btn-sm active" onclick="history.go(-1);" autofocus="autofocus" type="submit" value="Back"/>-->
