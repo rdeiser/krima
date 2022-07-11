6a8f62c8c4899ba3d9ee37310976f50019bd65f0
rdeiser <50148137+rdeiser@users.noreply.github.com>
2022-07-11 12:23:07 -0500

    Update rightBody.php

--- grimas/DisplayAllitem/splats/rightBody.php
+++ grimas/DisplayAllitem/splats/rightBody.php
@@ -18,17 +18,16 @@ if ($item['description']=='') {
               <table class="table">
                 <tr><th>Title:</th><td><?=$e($item['title'])?></td></tr>
 				<tr><th>Call Number:</th><td><?= preg_replace($pattern4, $replace4, $item['call_number'])?><text <?=$style4?>><?= preg_replace($pattern2, $replace2, $item['description'])?></text><text <?=$style3?>>&nbsp;c.<?=$e($item['copy_id'])?></text></td></tr>
-				<!--<tr><th>Description:</th><td><?=$e($item['description'])?></td></tr>-->
 				<tr><th>Barcode:</th><td><?=$e($item['barcode'])?></td></tr>
 				<tr><th>Inventory Date:</th><td><?=$e($item['inventory_date'])?></td></tr>
-				<tr <?=$style?>><th>Process Type:</th><td><?=$e($item['process_type'])?></td></tr>
-				<tr <?=$style2?>><th>Fulfillment Note:</th><td><?=$e($item['fulfillment_note'])?></td></tr>
+				<tr><th>Process Type:</th><td><?=$e($item['process_type'])?></td></tr>
+				<tr><th>Fulfillment Note:</th><td><?=$e($item['fulfillment_note'])?></td></tr>
 				<tr><th>Public Note:</th><td><?=$e($item['public_note'])?></td></tr>
 				<tr><th>Internal Note 1:</th><td><?=$e($item['internal_note_1'])?></td></tr>
 				<tr><th>Internal Note 2:</th><td><?=$e($item['internal_note_2'])?></td></tr>
 				<tr><th>Statistics Note 1:</th><td><?=$e($item['statistics_note_1'])?></td></tr>
 				<tr><th>Statistics Note 2:</th><td><?=$e($item['statistics_note_2'])?></td></tr>
-				<!--<tr><th>Statistics Note 3:</th><td><?=$e($item['statistics_note_3'])?></td></tr>-->
+				<tr><th>Statistics Note 3:</th><td><?=$e($item['statistics_note_3'])?></td></tr>
 				<tr><th>Location:</th><td><?=$e($item['location'])?></td></tr>
 				<tr><th></th><td></td></tr>
               </table>
