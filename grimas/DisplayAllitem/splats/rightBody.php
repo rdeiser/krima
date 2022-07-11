f431134b0b90b7c9b542a5d4175c80f5ba8553d1
rdeiser <50148137+rdeiser@users.noreply.github.com>
2022-07-11 12:25:28 -0500

    Update rightBody.php

--- grimas/DisplayAllitem/splats/rightBody.php
+++ grimas/DisplayAllitem/splats/rightBody.php
@@ -4,8 +4,10 @@ if (preg_match("/^([2-9]|[1-9][0-9]|[1-9][0-9][0-9])$/", $item['copy_id'])) {
 } else {
 	$style3 = 'style="opacity:0;"';
 }
-if ($item['description']==='') {
+if ($item['description']=='') {
 	$style4 = 'style="opacity:0;"';
+	$pattern2 = '//';
+	$replace2 = '';
 } else {
 	$style4 = 'style="opacity:1;"';
 	$pattern2 = '/^/';
