cc8de7ec35900ed440ddf19858c91dbbb602f06c
rdeiser <50148137+rdeiser@users.noreply.github.com>
2022-09-22 15:57:42 -0500

    Update DisplayCondition.php

--- grimas/DisplayCondition/DisplayCondition.php
+++ grimas/DisplayCondition/DisplayCondition.php
@@ -4,9 +4,8 @@ class DisplayCondition extends GrimaTask {
 	function do_task() {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
-		//uncomment the following coding when moving to production.
-		/*$item->addInventoryDate(date("Y-m-d"));
-		$item->updateAlma();*/
+		$item->addInventoryDate(date("Y-m-d"));
+		$item->updateAlma();
 {
 		$this->item = new Item();
 		$this->item->loadFromAlmaBarcode($this['unboxed_barcode']);
