a3a2abfd1639867869884d81de7ed5597a8ae33a
rdeiser <50148137+rdeiser@users.noreply.github.com>
2022-09-23 11:09:39 -0500

    adding wd locations

--- grimas/DisplayCondition/DisplayCondition.php
+++ grimas/DisplayCondition/DisplayCondition.php
@@ -5,6 +5,34 @@ class DisplayCondition extends GrimaTask {
 		$item = new Item();
 		$item->loadFromAlmaBarcode($this['unboxed_barcode']);
 		$item->addInventoryDate(date("Y-m-d"));
+		if ($item['statistics_note_3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdraw') {
+			if ($item['library'] !== 'WITHDRAW') {
+				if($item['location_code'] == 'cmc') {
+					$item['library_code'] = 'WITHDRAW';
+					$item['location_code'] = 'wdcmc';
+				}
+				if($item['location_code'] == 'juv') {
+					$item['library_code'] = 'WITHDRAW';
+					$item['location_code'] = 'wdjuv';
+				}
+				if($item['location_code'] == 'main') {
+					$item['library_code'] = 'WITHDRAW';
+					$item['location_code'] = 'wdmain';
+				}
+				if($item['location_code'] == 'over') {
+					$item['library_code'] = 'WITHDRAW';
+					$item['location_code'] = 'wdover';
+				}
+				if($item['location_code'] == 'overplus') {
+					$item['library_code'] = 'WITHDRAW';
+					$item['location_code'] = 'wdoverplus';
+				}
+				if($item['location_code'] == 'ref') {
+					$item['library_code'] = 'WITHDRAW';
+					$item['location_code'] = 'wdref';
+				}
+			}
+		}			
 		$item->updateAlma();
 {
 		$this->item = new Item();
