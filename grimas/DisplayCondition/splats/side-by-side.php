4151c9ce03f9e06ab671a55307b1a87eb379a9a9
rdeiser <50148137+rdeiser@users.noreply.github.com>
2022-09-23 11:24:34 -0500

    Update side-by-side.php

--- grimas/DisplayCondition/splats/side-by-side.php
+++ grimas/DisplayCondition/splats/side-by-side.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+//Multiple Copy/Volume Withdraw Shelf -- Anther cart for description and item copy number/holdings copy number and 852 $x ser;per;anal Multiple Copy/Volume Withdraw Add Stat Note 1 WITHDRAW
 if ($item['statistics_note_3'] == 'HALE return'||$item['statistics_note_3'] == 'AHD HALE return'||$item['statistics_note_3'] == 'ANNEX ingest'||$item['statistics_note_3'] == 'AHD ANNEX ingest'||$item['statistics_note_3'] == 'To be WITHDRAWN'||$item['statistics_note_3'] == 'AHD To be WITHDRAWN'||$item['statistics_note_3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdraw'||$item['statistics_note_3'] == '') {
 	$pattern = '/(HALE return|AHD HALE return|ANNEX ingest|AHD ANNEX ingest|To be WITHDRAWN|AHDTo be WITHDRAWN)|(^)/';
 	$replace = 'Place on Reveiw Cart';
@@ -12,6 +13,8 @@ if ($item['statistics_note_3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdr
 	$style = 'style="background-color: #cd5555;"';
 	
 }
+
+if ($item['description']  !== ''||$item['copy_id'] >= '1')
 
 if ($item['statistics_note_3'] == 'Condition review--REPAIR'||$item['statistics_note_3'] == 'Condition review--CRITICAL') {
 	$pattern = '/(Condition review--REPAIR|Condition review--CRITICAL)/';
