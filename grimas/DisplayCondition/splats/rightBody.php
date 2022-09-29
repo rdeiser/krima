16f94010fa5acb935e1979a065b58e9a9a1521c4
rdeiser <50148137+rdeiser@users.noreply.github.com>
2022-09-29 09:18:58 -0500

    Update rightBody.php

--- grimas/DisplayCondition/splats/rightBody.php
+++ grimas/DisplayCondition/splats/rightBody.php
@@ -23,8 +23,8 @@ if ($item['statistics_note_3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdr
   $subfields = $holding->getSubfieldValues("852","x");
   $subfieldx = implode(" ",$subfields);
   $needle = 'per';
-  function str_contains(string $subfields, string $needle): bool {
-	if (str_contains($subfieldx,'ser')||str_contains($subfieldx,'per')||str_contains($subfieldx,'anal')) {
+  function str_contains(string $subfieldsx string $needle): bool {
+	if (strpos($subfieldx,'ser')||strpos($subfieldx,'per')||strpos($subfieldx,'anal')) {
 		$pattern = '/(PHYSICAL CONDITION REVIEW For Possible Withdraw)/';
 		$replace = 'Multiple Copy/Volume Withdraw Shelf';
 	}
