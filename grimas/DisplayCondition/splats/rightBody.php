5a5176ee3087e7aead632bdef583f2cc1a8680f4
rdeiser <50148137+rdeiser@users.noreply.github.com>
2022-09-29 08:12:29 -0500

    Update rightBody.php

--- grimas/DisplayCondition/splats/rightBody.php
+++ grimas/DisplayCondition/splats/rightBody.php
@@ -22,15 +22,34 @@ if ($item['statistics_note_3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdr
   $holding->loadFromAlma($item['mms_id'],$item['holding_id']);
   $subfields = $holding->getSubfieldValues("852","x");
   $subfieldx = implode(" ",$subfields);
-  $pos = strpos($subfieldx,'per');
-  if ($pos === true) {
+  $needle = 'per';
+  //$pos = strpos($subfieldx,'per');
+  //if ($pos === true) {
   //if (strpos($subfieldx,'ser')||strpos($subfieldx,'per')||strpos($subfieldx,'anal')) { 
   //if (str_contains($subfieldx,'ser')||str_contains($subfieldx,'per')||str_contains($subfieldx,'anal')) {
-	$pattern = '/(PHYSICAL CONDITION REVIEW For Possible Withdraw)/';
-	$replace = 'Multiple Copy/Volume Withdraw Shelf';
+	//$pattern = '/(PHYSICAL CONDITION REVIEW For Possible Withdraw)/';
+	//$replace = 'Multiple Copy/Volume Withdraw Shelf';
+  //}
+
+  if (!defined('PHP_VERSION_ID') || (defined('PHP_VERSION_ID') && PHP_VERSION_ID < 80000)) {
+	if (!function_exists('str_contains')) {
+	  /**
+	   * Checks if a string contains another
+	   *
+	   * @param string $haystack The string to search in
+	   * @param string $needle The string to search
+	   * @return boolean Returns TRUE if the needle was found in haystack, FALSE otherwise.
+	   */
+	  function str_contains(string $subfieldx, string $needle): bool {
+		if (strpos($subfieldx, $needle) !== false;) {
+			$pattern = '/(PHYSICAL CONDITION REVIEW For Possible Withdraw)/';
+			$replace = 'Multiple Copy/Volume Withdraw Shelf';
+		}
+	  }
+	  }
+	}
   }
   
-}
 
 if ($item['statistics_note_3'] == 'Condition review--REPAIR'||$item['statistics_note_3'] == 'Condition review--CRITICAL') {
 	$pattern = '/(Condition review--REPAIR|Condition review--CRITICAL)/';
