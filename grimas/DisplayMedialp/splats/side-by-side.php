718daf18b7f5b76e3be4f309bb074374fb27385c
rdeiser <rdeiser@ksu.edu>
2022-12-14 11:44:00 -0600

    update display

--- grimas/DisplayMedialp/splats/side-by-side.php
+++ grimas/DisplayMedialp/splats/side-by-side.php
@@ -2,91 +2,15 @@
 if ($item['statistics_note_3'] == 'HALE return') {
 	$pattern = '/(HALE return)/';
 	$replace = 'HALE return';
+	$style = 'style="background-color: #ab82ff;"';
+	$text = '';
 } 
-if ($item['statistics_note_3'] == 'ANNEX ingest') {
-	$pattern = '/(ANNEX ingest)/';
-	$replace = 'ANNEX ingest';
-}
-if ($item['statistics_note_3'] == 'To be WITHDRAWN') {
-	$pattern = '/(To be WITHDRAWN)/';
-	$replace = 'To be WITHDRAWN';
-}
-if ($item['statistics_note_3'] == 'PHYSICAL CONDITION REVIEW For Possible Withdraw') {
-	$pattern = '/(PHYSICAL CONDITION REVIEW For Possible Withdraw)/';
-	$replace = 'Send to Condition Review Shelf';
-}
-if ($item['statistics_note_3'] == 'AHD HALE return') {
-	$pattern = '/(AHD HALE return)/';
-	$replace = 'HALE return';
-} 
-if ($item['statistics_note_3'] == 'AHD ANNEX ingest') {
-	$pattern = '/(AHD ANNEX ingest)/';
-	$replace = 'ANNEX ingest';
-}
-if ($item['statistics_note_3'] == 'AHD To be WITHDRAWN') {
-	$pattern = '/(AHD To be WITHDRAWN)/';
-	$replace = 'To be WITHDRAWN';
-}
 
-if ($item['statistics_note_3']=='To be WITHDRAWN') {
-			$style = 'style="background-color: #cd5555;"';
-			$text = '';
-}
-	else if ($item['statistics_note_3']=='ANNEX ingest') {
-			$style = 'style="background-color: #6495ed;"';
-			$text = '';
-	}
-	else if ($item['statistics_note_3']=='HALE return') {
-			$style = 'style="background-color: #ab82ff;"';
-			$text = '';
-	}
-	else if ($item['statistics_note_3']=='AHD HALE return') {
-			$style = 'style="background-color: #ab82ff;"';
-			$text = '';
-	}
-	else if ($item['statistics_note_3']=='AHD To be WITHDRAWN') {
-			$style = 'style="background-color: #cd5555;"';
-			$text = '';
-}
-	else if ($item['statistics_note_3']=='AHD ANNEX ingest') {
-			$style = 'style="background-color: #6495ed;"';
-			$text = '';
-	}
-	else if ($item['statistics_note_3']=='PHYSICAL CONDITION REVIEW For Possible Withdraw') {
-			$style = 'style=";"';
-			$text = '';
-	}
-if ($item['statistics_note_3'] == '') {
-	if ($item['in_temp_location'] == 'false') {
-		$pattern = '/^/';
-		$replace = $item['library'];
-		if ($item['library_code'] =='ANNEX') {
-			$style = 'style="background-color: #6495ed;"';
-		} else if ($item['library_code'] =='MAIN') {
-			$style = 'style="background-color: #ab82ff;"';
-		} else if ($item['library_code'] =='SALINA') {
-			$style = 'style="background-color: #ab82ff;"';
-		} else if ($item['library_code'] =='ARCH') {
-			$style = 'style="background-color: #ab82ff;"';
-		} else if ($item['library_code'] =='MATHPHYS') {
-			$style = 'style="background-color: #ab82ff;"';
-		}
-	} else {
-		$pattern = '/^/';
-		$replace = $item['temp_location'];
-		if ($item['temp_library'] =='ANNEX') {
-			$style = 'style="background-color: #6495ed;"';
-		} else if ($item['temp_library'] =='MAIN') {
-			$style = 'style="background-color: #ab82ff;"';
-		} else if ($item['temp_library'] =='SALINA') {
-			$style = 'style="background-color: #ab82ff;"';
-		} else if ($item['temp_library'] =='ARCH') {
-			$style = 'style="background-color: #ab82ff;"';
-		} else if ($item['temp_library'] =='MATHPHYS') {
-			$style = 'style="background-color: #ab82ff;"';
-		}
-	}
-
+if ($item['statistics_note_3'] !== 'HALE return') {
+	$pattern = '/(^)/';
+	$replace = 'Send to Problem Review Shelf';
+	$style = 'style="background-color: #cd5555;"';
+	$text = '';
 }
 
 	$holding = new Holding();
